0eeb896dda09b98e3a67505a7cbc033e6910ffe2
Lloyd Yeo <lloyd@kooki.co>
2017-03-22 19:29:41 +0800

    Lazy Commit!

--- resources/views/interactionsettings.blade.php
+++ resources/views/interactionsettings.blade.php
@@ -125,20 +125,20 @@
                 <div class="block-content">
                     <div class="row items-push">
                         <div class="form-group">
-                            <div class="col-xs-3">
+                            <div class="col-xs-12 col-lg-6">
                                 <div class="form-material floating">
                                     <label class="css-input css-input switch switch-square switch-primary">
                                         <input type="checkbox" checked><span></span> Unfollow users that don't folow me
                                     </label>
                                 </div>
                             </div>
-                            <div class="col-xs-3">
+                            <div class="col-xs-12 col-lg-3">
                                 <div class="form-material floating">
                                     <input class="form-control" type="text" id="material-gridf2" name="val-digits2">
                                     <label for="material-gridf2"><i class=""></i> Maximum Followers Filter</label>
                                 </div>
                             </div>
-                            <div class="col-xs-3">
+                            <div class="col-xs-12 col-lg-3">
                                 <div class="form-material floating">
                                     <input class="form-control" type="text" id="material-gridf3" name="val-digits2">
                                     <label for="material-gridf3"><i class=""></i> Minimum Followers Filter</label>
