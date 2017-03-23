97df14886e3590a9df0b6276661419aa02d88509
Lloyd Yeo <lloyd@kooki.co>
2017-03-23 10:46:13 +0800

    Lazy Commit!

--- resources/views/interactionsettings.blade.php
+++ resources/views/interactionsettings.blade.php
@@ -208,13 +208,11 @@
                             <form class="form-horizontal" onsubmit="return false;">
                                 <div class="form-group">
                                     <div class="col-md-12">
-                                        <input class="form-control" 
-                                               type="text" 
-                                               id="example-input2-group1"
-                                               data-emojiable="true"
-                                               placeholder="Add comments here..."
-                                               name="comments" aria-describedby="basic-addon1">
-                                        <!--<span class="input-group-addon"><i class="fa fa-envelope-o"></i></span>-->
+                                        <div class="input-group">
+                                            <input class="form-control" type="text" id="example-input2-group1" 
+                                                   name="example-input2-group1" placeholder="Enter your comments here...">
+                                            <span class="input-group-addon"><i class="fa fa-plus"></i></span>
+                                        </div>
                                     </div>
                                 </div>
                             </form>
