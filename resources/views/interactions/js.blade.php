e9abcb054d3737401fef635516ab3bb1e474fc64
Lloyd Yeo <lloyd@kooki.co>
2017-03-23 09:55:51 +0800

    Lazy Commit!

--- resources/views/interactions/js.blade.php
+++ resources/views/interactions/js.blade.php
@@ -130,6 +130,22 @@ jQuery(function () {
     
     initValidationMaterial();
     
+    // Initializes and creates emoji set from sprite sheet
+    window.emojiPicker = new EmojiPicker({
+        emojiable_selector: '[data-emojiable=true]',
+        assetsPath: 'emoji-picker/lib/img/',
+        popupButtonClasses: 'fa fa-smile-o'
+    });
+//    window.emojiPicker = new EmojiPicker({
+//        emojiable_selector: '[data-emojiable=true]',
+//        assetsPath: 'emoji-picker-gh-pages/lib/img/',
+//        popupButtonClasses: 'fa fa-smile-o'
+//    });
+    // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+    // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+    // It can be called as many times as necessary; previously converted input fields will not be converted again
+    window.emojiPicker.discover();
+    
 });
 
 $(".toggle-like-btn").on("click", function () {
