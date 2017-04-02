6d9cd5f68546b39da547f040bbf75f4e256733d8
Lloyd Yeo <lloyd@kooki.co>
2017-04-03 01:44:25 +0800

    Lazy Commit!

--- app/Console/Commands/GetNewDmJob.php
+++ app/Console/Commands/GetNewDmJob.php
@@ -89,7 +89,8 @@ class GetNewDmJob extends Command {
                             if ($newest_timestamp == 0) {
                                 $newest_timestamp = $story->args->timestamp;
                                 //update instagram profile's timestamp here.
-                                DB::connection('mysql_old')->update("UPDATE user_insta_profile SET recent_activity_timestamp = ? WHERE id = ?;", [$newest_timestamp, $ig_profile]);
+                                DB::connection('mysql_old')->update("UPDATE user_insta_profile SET recent_activity_timestamp = ? WHERE id = ?;", 
+                                        [$newest_timestamp, $ig_profile->id]);
                             }
                             
                             $new_follower_template = $ig_profile->insta_new_follower_template;
