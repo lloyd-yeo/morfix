6bd590f007add2ad7cf1783d224163d1f0ed9f81
Lloyd Yeo <lloyd@kooki.co>
2017-04-03 12:08:02 +0800

    Lazy Commit!

--- app/Console/Commands/RefreshInstagramProfile.php
+++ app/Console/Commands/RefreshInstagramProfile.php
@@ -3,30 +3,38 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use InstagramAPI\Instagram as Instagram;
+use InstagramAPI\SettingsAdapter as SettingsAdapter;
+use InstagramAPI\InstagramException as InstagramException;
+use App\InstagramProfile;
+use App\CreateInstagramProfileLog;
+use App\Proxy;
+use App\DmJob;
 
-class RefreshInstagramProfile extends Command
-{
+
+class RefreshInstagramProfile extends Command {
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'ig:refresh {offset : The position to start retrieving from.} {limit : The number of results to limit to.} {email?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Refresh the stats for a user\'s instagram profile.';
 
     /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
@@ -35,8 +43,58 @@ class RefreshInstagramProfile extends Command
      *
      * @return mixed
      */
-    public function handle()
-    {
-        //
+    public function handle() {
+        $offset = $this->argument('offset');
+        $limit = $this->argument('limit');
+        if (NULL !== $this->argument("email")) {
+            $users = DB::connection('mysql_old')->select("SELECT u.user_id, u.email FROM insta_affiliate.user u WHERE u.email = ?;", [$this->argument("email")]);
+        } else {
+            $users = DB::connection('mysql_old')->select("SELECT u.user_id, u.email FROM insta_affiliate.user u WHERE (u.user_tier > 1 OR u.trial_activation = 1) ORDER BY u.user_id ASC LIMIT ?,?;", [$offset, $limit]);
+        }
+
+        foreach ($users as $user) {
+            $instagram_profiles = DB::connection('mysql_old')->select("SELECT insta_username, insta_pw FROM user_insta_profile WHERE user_id = ?;", [$user->user_id]);
+            foreach ($instagram_profiles as $ig_profile) {
+                $this->line($ig_profile->insta_username . "\t" . $ig_profile->insta_pw);
+                $ig_username = $ig_profile->insta_username;
+                $ig_password = $ig_profile->insta_pw;
+
+                try {
+                    $instagram->setUser($ig_username, $ig_password);
+                    $instagram->setProxy($ig_profile->proxy);
+                    $explorer_response = $instagram->login();
+                    $user_response = $instagram->getUserInfoByName($ig_username);
+                    $instagram_user = $user_response->user;
+                    
+                    DB::connection('mysql_old')->
+                            update("UPDATE user_insta_profile SET follower_count = ?, num_posts = ? WHERE insta_username = ?;", 
+                                    [$instagram_user->follower_count, $instagram_user->media_count, $ig_username]);
+                    
+//                    $new_profile = new InstagramProfile;
+//                    $new_profile->user_id = Auth::user()->id;
+//                    $new_profile->email = Auth::user()->email;
+//                    $new_profile->insta_user_id = $instagram_user->pk;
+//                    $new_profile->insta_username = $ig_username;
+//                    $new_profile->insta_pw = $ig_password;
+//                    $new_profile->profile_pic_url = $instagram_user->profile_pic_url;
+//                    $new_profile->profile_full_name = $instagram_user->full_name;
+//                    $new_profile->follower_count = $instagram_user->follower_count;
+//                    $new_profile->num_posts = $instagram_user->media_count;
+//                    $new_profile->proxy = $proxy;
+//                    $new_profile->save();
+                    
+                } catch (\InstagramAPI\Exception\CheckpointRequiredException $checkpoint_ex) {
+                    $this->error($checkpoint_ex->getMessage());
+                    DB::connection('mysql_old')->update('update user_insta_profile set checkpoint_required = 1 where id = ?;', [$ig_profile->id]);
+                } catch (\InstagramAPI\Exception\NetworkException $network_ex) {
+                    $this->error($network_ex->getMessage());
+                    DB::connection('mysql_old')->update('update user_insta_profile set error_msg = ? where id = ?;', [$network_ex->getMessage(), $ig_profile->id]);
+                } catch (\InstagramAPI\Exception\EndpointException $endpoint_ex) {
+                    $this->error($endpoint_ex->getMessage());
+                    DB::connection('mysql_old')->update('update user_insta_profile set error_msg = ? where id = ?;', [$network_ex->getMessage(), $ig_profile->id]);
+                }
+            }
+        }
     }
+
 }
