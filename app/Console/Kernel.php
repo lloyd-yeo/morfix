f51c4e23f287d595df89153d09e543e70d8c08d7
Lloyd Yeo <lloyd@kooki.co>
2017-04-04 12:45:50 +0800

    Lazy Commit!

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,22 +40,53 @@ class Kernel extends ConsoleKernel {
             $schedule->command('dm:send ' . $counter . ' 10')->everyFiveMinutes();
         }
         
-        for ($i = 0; $i < 70; $i++) {
-            $counter = $i * 10;
-            $schedule->command('interaction:follow ' . $counter . ' 10')->everyFiveMinutes();
-        }
+//        for ($i = 0; $i < 70; $i++) {
+//            $counter = $i * 10;
+//            $schedule->command('interaction:follow ' . $counter . ' 10')->everyFiveMinutes();
+//        }
         
-        for ($i = 0; $i < 300; $i++) {
-            $counter = $i * 20;
-            $schedule->command('interaction:like ' . $counter . ' 20')->everyFiveMinutes();
-        }
+        $schedule->command('interaction:follow 0 100')->everyFiveMinutes();
+        $schedule->command('interaction:follow 100 100')->everyFiveMinutes();
+        $schedule->command('interaction:follow 200 100')->everyFiveMinutes();
+        $schedule->command('interaction:follow 300 100')->everyFiveMinutes();
+        $schedule->command('interaction:follow 400 100')->everyFiveMinutes();
+        $schedule->command('interaction:follow 500 100')->everyFiveMinutes();
+        $schedule->command('interaction:follow 600 100')->everyFiveMinutes();
+        $schedule->command('interaction:follow 700 100')->everyFiveMinutes();
+        
+//        for ($i = 0; $i < 300; $i++) {
+//            $counter = $i * 20;
+//            $schedule->command('interaction:like ' . $counter . ' 20')->everyFiveMinutes();
+//        }
+        $schedule->command('interaction:like 0 100')->everyFiveMinutes();
+        $schedule->command('interaction:like 100 100')->everyFiveMinutes();
+        $schedule->command('interaction:like 200 100')->everyFiveMinutes();
+        $schedule->command('interaction:like 300 100')->everyFiveMinutes();
+        $schedule->command('interaction:like 400 100')->everyFiveMinutes();
+        $schedule->command('interaction:like 500 100')->everyFiveMinutes();
+        $schedule->command('interaction:like 600 100')->everyFiveMinutes();
+        $schedule->command('interaction:like 700 100')->everyFiveMinutes();
+        
+//        for ($i = 0; $i < 300; $i++) {
+//            $counter = $i * 20;
+//            $schedule->command('interaction:comment ' . $counter . ' 20')->everyFiveMinutes();
+//        }
+        
+        $schedule->command('interaction:comment 0 100')->everyFiveMinutes();
+        $schedule->command('interaction:comment 100 100')->everyFiveMinutes();
+        $schedule->command('interaction:comment 200 100')->everyFiveMinutes();
+        $schedule->command('interaction:comment 300 100')->everyFiveMinutes();
+        $schedule->command('interaction:comment 400 100')->everyFiveMinutes();
+        $schedule->command('interaction:comment 500 100')->everyFiveMinutes();
+        $schedule->command('interaction:comment 600 100')->everyFiveMinutes();
+        $schedule->command('interaction:comment 700 100')->everyFiveMinutes();
         
 //        $schedule->command('interaction:like 0 3000')->everyFiveMinutes();
         
-        for ($i = 0; $i < 80; $i++) {
-            $counter = $i * 10;
-            $schedule->command('ig:refresh ' . $i . ' 1')->everyThirtyMinutes();
-        }
+//        for ($i = 0; $i < 80; $i++) {
+//            $counter = $i * 10;
+//            $schedule->command('ig:refresh ' . $i . ' 1')->everyThirtyMinutes();
+//        }
         
         //$schedule->command('analysis:follower')->daily();
         // $schedule->command('inspire')
