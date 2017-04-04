75f3b4a9e9348d2bbe033f85b53b5833abd2ef3f
Lloyd Yeo <lloyd@kooki.co>
2017-04-05 06:13:28 +0800

    Lazy Commit!

--- app/Console/Commands/InteractionFollow.php
+++ app/Console/Commands/InteractionFollow.php
@@ -131,7 +131,7 @@ class InteractionFollow extends Command {
                 $config["dbname"] = "morfix";
                 $config["dbtablename"] = "instagram_sessions";
 
-                $debug = false;
+                $debug = true;
                 $truncatedDebug = false;
                 $instagram = new \InstagramAPI\Instagram($debug, $truncatedDebug, $config);
 
