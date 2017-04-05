764b6f5aa3bcc1261faa9ff71de99d6804625264
Lloyd Yeo <lloyd@kooki.co>
2017-04-05 14:56:54 +0800

    Lazy Commit!

--- app/Console/Commands/EngagementGroup.php
+++ app/Console/Commands/EngagementGroup.php
@@ -19,7 +19,7 @@ class EngagementGroup extends Command {
      *
      * @var string
      */
-    protected $signature = 'engagement:start';
+    protected $signature = 'engagement:add';
 
     /**
      * The console command description.
@@ -44,11 +44,14 @@ class EngagementGroup extends Command {
      */
     public function handle() {
         $outstanding_engagements = DB::connection('mysql_old')
-                ->select("SELECT media_id FROM insta_affiliate.engagement_group_job WHERE engaged = 0 ORDER BY date_logged DESC LIMIT 1;");
+                ->select("SELECT media_id FROM insta_affiliate.engagement_group_job WHERE engaged = 0 ORDER BY date_logged DESC;");
         foreach ($outstanding_engagements as $outstanding_engagement) {
             $media_id = $outstanding_engagement->media_id;
+            $comment_count = 50;
+            
             DB::connection('mysql_old')
                     ->update("UPDATE engagement_group_job SET engaged = 1, date_worked_on = NOW() WHERE media_id = ?;", [$media_id]);
+            
             $engagement_group_users = DB::connection('mysql_old')
                     ->select("SELECT p.insta_username, p.insta_pw, p.proxy, p.auto_like, p.auto_comment, p.id
                                 FROM user_insta_profile p, user u
@@ -62,72 +65,17 @@ class EngagementGroup extends Command {
                                 AND (
                                 u.user_tier = 1 
                                 OR (u.user_tier > 1 AND p.auto_interaction = 1 AND (p.auto_like = 1 OR p.auto_comment = 1))
-                                )
-                                ORDER BY RAND()
-                                LIMIT 450;");
-
+                                );");
+            
             foreach ($engagement_group_users as $ig_profile) {
                 $ig_username = $ig_profile->insta_username;
-                $ig_password = $ig_profile->insta_pw;
-
-                $config = array();
-                $config["storage"] = "mysql";
-                $config["dbusername"] = "root";
-                $config["dbpassword"] = "inst@ffiliates123";
-                $config["dbhost"] = "52.221.60.235:3306";
-                $config["dbname"] = "morfix";
-                $config["dbtablename"] = "instagram_sessions";
-
-                $debug = true;
-                $truncatedDebug = false;
-                $instagram = new \InstagramAPI\Instagram($debug, $truncatedDebug, $config);
-                if ($ig_profile->proxy === NULL) {
-                    $proxies = DB::connection("mysql_old")->select("SELECT proxy, assigned FROM insta_affiliate.proxy WHERE assigned = 0 LIMIT 1;");
-                    foreach ($proxies as $proxy) {
-                        $rows_affected = DB::connection('mysql_old')->update('update user_insta_profile set proxy = ? where id = ?;', [$proxy->proxy, $ig_profile->id]);
-                        $instagram->setProxy($proxy->proxy);
-                        $rows_affected = DB::connection('mysql_old')->update('update proxy set assigned = 1 where proxy = ?;', [$proxy->proxy]);
-                    }
-                } else {
-                    $instagram->setProxy($ig_profile->proxy);
+                DB::connection('mysql_old')
+                            ->insert("INSERT INTO engagement_job_queue (media_id,insta_username,action) VALUES (?,?,?);", [$media_id, $ig_username,0]);
+                if ($ig_profile->auto_comment == 1 && $$comment_count > 0) {
+                    DB::connection('mysql_old')
+                            ->insert("INSERT INTO engagement_job_queue (media_id,insta_username,action) VALUES (?,?,?);", [$media_id, $ig_username,1]);
+                    $comment_count = $comment_count - 1;
                 }
-
-                try {
-                    $instagram->setUser($ig_username, $ig_password);
-                    $explorer_response = $instagram->login();
-                    $like_response = $instagram->like($media_id);
-                    $this->line($ig_username . "\t" . serialize($like_response));
-                } catch (\InstagramAPI\Exception\CheckpointRequiredException $checkpoint_ex) {
-                    $this->error("checkpt\t" . $checkpoint_ex->getMessage());
-                    DB::connection('mysql_old')->update('update user_insta_profile set checkpoint_required = 1 where id = ?;', [$ig_profile->id]);
-                    continue;
-                } catch (\InstagramAPI\Exception\NetworkException $network_ex) {
-                    $this->error("network\t" . $network_ex->getMessage());
-                    DB::connection('mysql_old')->update('update user_insta_profile set error_msg = ? where id = ?;', [$network_ex->getMessage(), $ig_profile->id]);
-                    continue;
-                } catch (\InstagramAPI\Exception\EndpointException $endpoint_ex) {
-                    $this->error("endpt\t" . $endpoint_ex->getMessage());
-                    DB::connection('mysql_old')->update('update user_insta_profile set invalid_proxy = 1, error_msg = ? where id = ?;', [$endpoint_ex->getMessage(), $ig_profile->id]);
-                    continue;
-                } catch (\InstagramAPI\Exception\IncorrectPasswordException $incorrectpw_ex) {
-                    $this->error("incorrectpw\t" . $incorrectpw_ex->getMessage());
-                    DB::connection('mysql_old')->update('update user_insta_profile set incorrect_pw = 1, error_msg = ? where id = ?;', [$incorrectpw_ex->getMessage(), $ig_profile->id]);
-                    continue;
-                } catch (\InstagramAPI\Exception\FeedbackRequiredException $feedback_ex) {
-                    $this->error("feedback\t" . $feedback_ex->getMessage());
-                    DB::connection('mysql_old')->update('update user_insta_profile set invalid_proxy = 1, error_msg = ? where id = ?;', [$feedback_ex->getMessage(), $ig_profile->id]);
-                    continue;
-                } catch (\InstagramAPI\Exception\EmptyResponseException $emptyresponse_ex) {
-                    continue;
-                } catch (\InstagramAPI\Exception\AccountDisabledException $acctdisabled_ex) {
-                    $this->error("acctdisabled\t" . $acctdisabled_ex->getMessage());
-                    DB::connection('mysql_old')->update('update user_insta_profile set invalid_user = 1, error_msg = ? where id = ?;', [$acctdisabled_ex->getMessage(), $ig_profile->id]);
-                    continue;
-                } catch (\InstagramAPI\Exception\ForcedPasswordResetException $forcedpwreset_ex) {
-                    $this->error("forcedpwreset\t" . $forcedpwreset_ex->getMessage());
-                    DB::connection('mysql_old')->update('update user_insta_profile set incorrect_pw = 1, error_msg = ? where id = ?;', [$forcedpwreset_ex->getMessage(), $ig_profile->id]);
-                    continue;
-                } 
             }
         }
     }
