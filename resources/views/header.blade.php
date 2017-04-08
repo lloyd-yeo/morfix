44c200b95f7b224d05d6785ec5d2660b11f475c6
Lloyd Yeo <lloyd@kooki.co>
2017-04-08 11:28:54 +0800

    Lazy Commit!

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -103,26 +103,6 @@
                 <i class="fa fa-ellipsis-v"></i>
             </button>
         </li>
-        <li>
-            <!-- Opens the Apps modal found at the bottom of the page, before including JS code -->
-            <button class="btn btn-default pull-right" data-toggle="modal" data-target="#apps-modal" type="button">
-                <i class="si si-grid"></i>
-            </button>
-        </li>
-        <li class="visible-xs">
-            <!-- Toggle class helper (for .js-header-search below), functionality initialized in App() -> uiToggleClass() -->
-            <button class="btn btn-default" data-toggle="class-toggle" data-target=".js-header-search" data-class="header-search-xs-visible" type="button">
-                <i class="fa fa-search"></i>
-            </button>
-        </li>
-        <li class="js-header-search header-search">
-            <form class="form-horizontal" action="base_pages_search.html" method="post">
-                <div class="form-material form-material-primary input-group remove-margin-t remove-margin-b">
-                    <input class="form-control" type="text" id="base-material-text" name="base-material-text" placeholder="Search..">
-                    <span class="input-group-addon"><i class="si si-magnifier"></i></span>
-                </div>
-            </form>
-        </li>
     </ul>
     <!-- END Header Navigation Left -->
 </header>
