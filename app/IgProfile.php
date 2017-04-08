6fba663846424d3c3fec6dda568eb17484667a05
Lloyd Yeo <lloyd@kooki.co>
2017-04-08 14:17:40 +0800

    Lazy Commit!

--- app/IgProfile.php
+++ app/IgProfile.php
@@ -9,4 +9,13 @@ class IgProfile extends Model
     protected $primaryKey = 'id'; 
     protected $table = "user_insta_profile";
     protected $connection = 'mysql_old';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'user_id', 'email', 'insta_user_id', 'insta_username', 'insta_pw', 'profile_pic_url', 'follower_count', 'profile_full_name'
+    ];
 }
