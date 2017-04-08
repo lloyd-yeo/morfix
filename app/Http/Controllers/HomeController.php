077ecea29307a3d0bb1318768fe6101fc37ecde7
Lloyd Yeo <lloyd@kooki.co>
2017-04-08 14:09:33 +0800

    Lazy Commit!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller {
             $ranking++;
         }
 
-        $instagram_profiles = IgProfile::where('insta_username', Auth::user()->email);
+        $instagram_profiles = IgProfile::where('insta_username', Auth::user()->email)->take(10)->get();
         
         $new_profile_follower_analysis = array();
         $new_profile_follower_analysis_label = array();
