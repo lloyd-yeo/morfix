b9808ca5cfe3b7468d86a8b391ab499f478cd299
Lloyd Yeo <lloyd@kooki.co>
2017-04-27 17:22:51 +0800

    Lazy Commit!

--- HalfHourJobs.php
+++ HalfHourJobs.php
@@ -1,11 +1,8 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/DB.php';
 $start = microtime(true);
-
 $conn = getConnection($servername, $username, $password, $dbname);
-#$conn->query("UPDATE user SET trial_activation = 2 WHERE user_tier > 1;");
 $conn->query("UPDATE user_insta_profile SET proxy = NULL, invalid_proxy = 0 WHERE invalid_proxy = 1;");
 
 $ig_profiles = array();
@@ -47,7 +44,6 @@ for ($i = 0; $i < count($ig_profiles); $i++) {
     
     $conn->close();
 }
-
 
 $time_elapsed_secs = microtime(true) - $start;
 echo "\n" . $time_elapsed_secs;
