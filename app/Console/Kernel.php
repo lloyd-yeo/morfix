4a44fe2abe137bced61681993b8de4e91fc4e10f
Lloyd Yeo <lloyd@kooki.co>
2017-04-30 10:53:43 +0800

    Lazy Commit!

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -46,19 +46,21 @@ class Kernel extends ConsoleKernel {
         $schedule->command('dm:get 0 0 julidasvierte@gmail.com')->everyFiveMinutes();
         $schedule->command('dm:get 0 0 ailyndigital@gmail.com')->everyFiveMinutes();
         $schedule->command('dm:get 0 0 julian.kussin@gmail.com')->everyFiveMinutes();
+        $schedule->command('dm:get 0 0 chuanian@hotmail.com')->everyFiveMinutes();
         
         for ($i = 0; $i < 50; $i++) {
             $counter = $i * 20;
             $schedule->command('interaction:like ' . $counter . ' 20')->everyFiveMinutes();
         }
 //        $schedule->command('interaction:like 0 0 annegreenfield323@gmail.com')->everyFiveMinutes();
-        $schedule->command('interaction:like 0 0 caleb@calebthetrainer.com')->everyFiveMinutes();
+//        $schedule->command('interaction:like 0 0 caleb@calebthetrainer.com')->everyFiveMinutes();
         $schedule->command('interaction:like 0 0 xtremewealth@gmail.com')->everyFiveMinutes();
         $schedule->command('interaction:like 0 0 Shawnjosiah.pd@gmail.com')->everyFiveMinutes();
         $schedule->command('interaction:like 0 0 nicolasmaton@gmail.com')->everyFiveMinutes();
         $schedule->command('interaction:like 0 0 raultraining@yahoo.com')->everyFiveMinutes();
         $schedule->command('interaction:like 0 0 peizhisim@gmail.com')->everyFiveMinutes();
         $schedule->command('interaction:like 0 0 julian.kussin@gmail.com')->everyFiveMinutes();
+        $schedule->command('interaction:like 0 0 chuanian@hotmail.com')->everyFiveMinutes();
         
         $schedule->command("engagement:add")->everyThirtyMinutes()->withoutOverlapping();
         $schedule->command("analysis:follower")->daily("00:00")->withoutOverlapping();
