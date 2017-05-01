f85aa142ee52a13fbbcb06d15d0e1b3c20479139
user <user@user-PC>
2017-05-01 18:18:21 +0800

    added log into db

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -12,6 +12,7 @@ class StripeWebhookController extends CashierController
     public function handleChargeRefunded(array $payload)
     {
         Log::info('Payment Refunded - StripeWebhook - handleChargeRefunded()', ['details' => json_encode($payload)]);
+        DB::connection('mysql_old')->insert("INSERT INTO stripe_webhook_log (log) VALUES (?)", [serialize($payload)]);
         return new Response('Webhook Handled', 200);
     }
 }
