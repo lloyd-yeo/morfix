944e50df1a1264aad0479e31ecf705dff50b6825
Lloyd Yeo <lloyd@kooki.co>
2017-05-02 00:50:05 +0800

    Lazy Commit!

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,7 +11,7 @@ class WebhookController extends BaseController {
 
     public function handleInvoiceCreated(array $payload) {
         Log::info('Invoice Created - StripeWebhook - handlewWebhook()', ['details' => json_encode($payload)]);
-        DB::connection('mysql_old')->insert("INSERT INTO stripe_webhook_log (log) VALUES (?)", [serialize($payload)]);
+        #DB::connection('mysql_old')->insert("INSERT INTO stripe_webhook_log (log) VALUES (?)", [serialize($payload)]);
         return response('Webhook Handled', 200);
     }
 
