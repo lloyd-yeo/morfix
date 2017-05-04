4eb4d0992962339947d137ed052b83922d8e334b
Lloyd Yeo <lloyd@kooki.co>
2017-05-05 03:30:31 +0800

    Lazy Commit!

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -1,12 +1,20 @@
 <!-- Stats -->
 <div class="content bg-white border-b">
     <div class="row items-push text-uppercase">
+        
         <div class="col-xs-6 col-sm-2">
+            <div class="block-content block-content-full bg-modern">
+                <i class="si si-crop fa-4x text-white"></i>
+                <div class="font-w600 text-white-op push-15-t">Crop</div>
+            </div>
+        </div>
+        
+<!--        <div class="col-xs-6 col-sm-2">
             <div class="font-w700 text-gray-darker animated fadeIn">Pending Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-calendar"></i> This month</small></div>
             <span class="h2 font-w700 text-moneygreen animated flipInX" data-subject="money" data-toggle="countTo"
                   data-decimals="2" data-to="{{ Auth::user()->pending_commission }}"></span>
-        </div>
+        </div>-->
         <div class="col-xs-6 col-sm-2">
             <div class="font-w700 text-gray-darker animated fadeIn">Pending Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-calendar"></i> Next Month</small></div>
