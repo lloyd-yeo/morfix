bfe260957cc6e500108621faddebe5f4439acef3
Lloyd Yeo <lloyd@kooki.co>
2017-05-05 16:18:16 +0800

    Lazy Commit!

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -2,7 +2,7 @@
 <div class="content bg-white border-b">
     <div class="row items-push text-uppercase">
         
-        <div class="col-xs-6 col-sm-2">
+        <div class="col-xs-6 col-sm-2 col-lg-4">
             <button class="btn btn-minw btn-rounded btn-primary" type="button"><i class="si si-plus"></i> Add Instagram Profile</button>
         </div>
         
@@ -12,27 +12,27 @@
             <span class="h2 font-w700 text-moneygreen animated flipInX" data-subject="money" data-toggle="countTo"
                   data-decimals="2" data-to="{{ Auth::user()->pending_commission }}"></span>
         </div>-->
-        <div class="col-xs-6 col-sm-2">
+<!--        <div class="col-xs-6 col-sm-2">
             <div class="font-w700 text-gray-darker animated fadeIn">Pending Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-calendar"></i> Next Month</small></div>
             <span class="h2 font-w700 text-moneygreen animated flipInX">$ 0.00</span>
-        </div>
-        <div class="col-xs-6 col-sm-3">
+        </div>-->
+<!--        <div class="col-xs-6 col-sm-3">
             <div class="font-w700 text-gray-darker animated fadeIn">Total Pending Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-calendar"></i> Consolidated</small></div>
             <span class="h2 font-w700 text-moneygreen animated flipInX"
                   data-subject="money" data-toggle="countTo"
                   data-decimals="2" data-to="{{ Auth::user()->pending_commission }}"></span>
-        </div>
-        <div class="col-xs-6 col-sm-2">
+        </div>-->
+<!--        <div class="col-xs-6 col-sm-2">
             <div class="font-w700 text-gray-darker animated fadeIn">Total Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-credit-card"></i> Paid out</small></div>
             <span class="h2 font-w700 text-moneygreen animated flipInX" 
                   data-subject="money" data-toggle="countTo"
                   data-decimals="2" data-to="{{ Auth::user()->total_commission }}"
                   >$ {{ number_format(Auth::user()->total_commission, 2, '.', ',')  }}</span>
-        </div>
-        <div class="col-xs-6 col-sm-3">
+        </div>-->
+        <div class="col-xs-6 col-sm-3 col-lg-8">
             <div class="font-w700 text-gray-darker animated fadeIn">Leaderboard Ranking</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-trophy"></i> All Time</small></div>
             <span class="h2 font-w300 text-modern animated flipInX">{{ $user_leaderboard_alltime_ranking }}</span>
