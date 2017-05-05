10bfd1f3e5d60a1d2576d6c8bb13d647cef22516
Lloyd Yeo <lloyd@kooki.co>
2017-05-05 21:12:38 +0800

    Lazy Commit!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,13 +28,14 @@
                     </div>
                     <div class="block-content">
                         <ul class="list list-timeline pull-t">
+                            
                             <!-- Twitter Notification -->
                             <li>
                                 <div class="list-timeline-time">12 hrs ago</div>
-                                <i class="fa fa-twitter list-timeline-icon bg-info"></i>
+                                <i class="fa fa-thumbs-o-up list-timeline-icon bg-info"></i>
                                 <div class="list-timeline-content">
-                                    <p class="font-w600">+ 1150 Followers</p>
-                                    <p class="font-s13">You’re getting more and more followers, keep it up!</p>
+                                    <p class="font-w600">NEW REFERRAL!</p>
+                                    <p class="font-s13"><a href="#">ywz.lloyd@gmail.com</a> just joined! You’re getting more and more referrals, keep it up!</p>
                                 </div>
                             </li>
                             <!-- END Twitter Notification -->
