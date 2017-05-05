51aa57eef67f0693071df83d0add0e5948b79e5c
Lloyd Yeo <lloyd@kooki.co>
2017-05-06 00:39:59 +0800

    Lazy Commit!

--- resources/views/dashboard/update.blade.php
+++ resources/views/dashboard/update.blade.php
@@ -5,6 +5,7 @@
     <i class="fa fa-thumbs-o-up list-timeline-icon bg-info"></i>
     @else
     <i class="fa fa-coffee list-timeline-icon bg-info"></i>
+    @endif
     <div class="list-timeline-content">
         <p class="font-w600">{{ $update->title }}</p>
         <p class="font-s13">{{ $update->content }}</p>
