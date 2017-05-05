820c0641724d660af656cd558f495e653f68dec1
Lloyd Yeo <lloyd@kooki.co>
2017-05-05 19:27:55 +0800

    Lazy Commit!

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -6,20 +6,20 @@
             <button class="btn btn-minw btn-rounded btn-primary" type="button"><i class="si si-plus"></i> Add Instagram Profile</button>
         </div>
         <div class="col-lg-6">
-            <div class="col-xs-6 col-sm-2 col-lg-2">
+<!--            <div class="col-xs-6 col-sm-2 col-lg-2">
                 <div class="font-w700 text-gray-darker animated fadeIn">Pending Commission</div>
                 <div class="text-muted animated fadeIn"><small><i class="si si-calendar"></i> This month</small></div>
                 <span class="h2 font-w700 text-moneygreen animated flipInX" data-subject="money" data-toggle="countTo"
                       data-decimals="2" data-to="{{ Auth::user()->pending_commission }}"></span>
-            </div>
-            <div class="col-xs-6 col-sm-2 col-lg-3">
+            </div>-->
+<!--            <div class="col-xs-6 col-sm-2 col-lg-3">
                 <div class="font-w700 text-gray-darker animated fadeIn">Total Commission</div>
                 <div class="text-muted animated fadeIn"><small><i class="si si-credit-card"></i> Paid out</small></div>
                 <span class="h2 font-w700 text-moneygreen animated flipInX" 
                       data-subject="money" data-toggle="countTo"
                       data-decimals="2" data-to="{{ Auth::user()->total_commission }}"
                       >$ {{ number_format(Auth::user()->total_commission, 2, '.', ',')  }}</span>
-            </div>
+            </div>-->
         </div>
         <!--        <div class="col-xs-6 col-sm-2 col-lg-2">
                     <div class="font-w700 text-gray-darker animated fadeIn">Pending Commission</div>
