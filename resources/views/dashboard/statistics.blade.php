4a005e567c1c93e35bb4809caa5273d4b1d71c96
Lloyd Yeo <lloyd@kooki.co>
2017-05-05 16:23:23 +0800

    Lazy Commit!

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -24,7 +24,7 @@
                   data-subject="money" data-toggle="countTo"
                   data-decimals="2" data-to="{{ Auth::user()->pending_commission }}"></span>
         </div>-->
-        <div class="col-xs-6 col-sm-2 col-lg-2">
+        <div class="col-xs-6 col-sm-2 col-lg-3">
             <div class="font-w700 text-gray-darker animated fadeIn">Total Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-credit-card"></i> Paid out</small></div>
             <span class="h2 font-w700 text-moneygreen animated flipInX" 
@@ -32,7 +32,7 @@
                   data-decimals="2" data-to="{{ Auth::user()->total_commission }}"
                   >$ {{ number_format(Auth::user()->total_commission, 2, '.', ',')  }}</span>
         </div>
-        <div class="col-xs-6 col-sm-3 col-lg-4">
+        <div class="col-xs-6 col-sm-3 col-lg-3">
             <div class="font-w700 text-gray-darker animated fadeIn">Leaderboard Ranking</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-trophy"></i> All Time</small></div>
             <span class="h2 font-w300 text-modern animated flipInX">{{ $user_leaderboard_alltime_ranking }}</span>
