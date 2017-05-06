db2eb558ebece11ae96778e6d71174748b41c0b3
Lloyd Yeo <lloyd@kooki.co>
2017-05-06 16:46:13 +0800

    Lazy Commit!

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -5,7 +5,7 @@
         <div class="col-xs-6 col-sm-6 col-lg-2">
             <div class="font-w700 text-gray-darker animated fadeIn">My Instagram Profiles</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-calendar"></i> <b>6</b> quota remaining 
-                    <i class="fa fa-question-circle text-info" style="margin-left: 5px;" data-toggle="tooltip" 
+                    <i class="fa fa-question-circle" style="margin-left: 5px;" data-toggle="tooltip" 
                        data-placement="top" title="Remaining instagram slots you can add in Morfix" type="button"></i></small>
             </div>
             <button class="btn btn-minw btn-rounded btn-primary" style="margin-top: 10px;" type="button"><i class="si si-plus"></i> Add Profile</button>
@@ -13,7 +13,7 @@
         <div class="col-xs-6 col-sm-2 col-lg-2">
             <div class="font-w700 text-gray-darker animated fadeIn">Pending Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-calendar"></i> Payable this month
-                <i class="fa fa-question-circle text-info" style="margin-left: 5px;" data-toggle="tooltip" 
+                <i class="fa fa-question-circle" style="margin-left: 5px;" data-toggle="tooltip" 
                        data-placement="top" title="Commissions that will be paid out the 25th of this month" type="button"></i>
                 </small>
             </div>
@@ -23,7 +23,7 @@
         <div class="col-xs-6 col-sm-2 col-lg-2">
             <div class="font-w700 text-gray-darker animated fadeIn">Pending Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-calendar"></i> Total
-                <i class="fa fa-question-circle text-info" style="margin-left: 5px;" data-toggle="tooltip" 
+                <i class="fa fa-question-circle" style="margin-left: 5px;" data-toggle="tooltip" 
                        data-placement="top" title="All commissions that hasn't been paid out" type="button"></i>
                 </small></div>
             <span class="h2 text-moneygreen animated flipInX" data-subject="money" data-toggle="countTo"
@@ -32,7 +32,7 @@
         <div class="col-xs-6 col-sm-2 col-lg-2">
             <div class="font-w700 text-gray-darker animated fadeIn">Total Commission</div>
             <div class="text-muted animated fadeIn"><small><i class="si si-credit-card"></i> Paid out
-                <i class="fa fa-question-circle text-info" style="margin-left: 5px;" data-toggle="tooltip" 
+                <i class="fa fa-question-circle" style="margin-left: 5px;" data-toggle="tooltip" 
                        data-placement="top" title="Total commissions that Morfix has paid out to date." type="button"></i>
                 </small></div>
             <span class="h2 text-moneygreen animated flipInX" 
