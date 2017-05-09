bccb28124f5ab2c5943c41dbc32995bcfdec2a04
Lloyd Yeo <lloyd@kooki.co>
2017-05-10 03:45:05 +0800

    Lazy Commit!

--- resources/views/affiliate/dashboard.blade.php
+++ resources/views/affiliate/dashboard.blade.php
@@ -17,6 +17,48 @@
     </div>
 
     <div class="content content-boxed">
+        
+        <!-- Header Tiles -->
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+                <a class="block block-link-hover3 text-center" href="javascript:void(0)">
+                    <div class="block-content block-content-full">
+                        <div class="h1 font-w700 text-modern" data-subject="money" data-toggle="countTo"
+                  data-decimals="2" data-to="{{ Auth::user()->pending_commission_payable }}"></div>
+                    </div>
+                    <div class="block-content block-content-full block-content-mini bg-gray-lighter text-muted font-w600">Pending Commission this Month</div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <a class="block block-link-hover3 text-center" href="javascript:void(0)">
+                    <div class="block-content block-content-full text-modern">
+                        <div class="h1 font-w700" data-subject="money" data-toggle="countTo"
+                  data-decimals="2" data-to="{{ Auth::user()->pending_commission }}"></div>
+                    </div>
+                    <div class="block-content block-content-full block-content-mini bg-gray-lighter text-muted font-w600">Total Pending Commission</div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <a class="block block-link-hover3 text-center" href="javascript:void(0)">
+                    <div class="block-content block-content-full text-modern">
+                        <div class="h1 font-w700" data-subject="money" data-toggle="countTo"
+                  data-decimals="2" data-to="{{ Auth::user()->all_time_commission }}"></div>
+                    </div>
+                    <div class="block-content block-content-full block-content-mini bg-gray-lighter text-muted font-w600">Total Commission</div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <a class="block block-link-hover3 text-center" href="javascript:void(0)">
+                    <div class="block-content block-content-full text-modern">
+                        <div class="h1 font-w700" data-toggle="countTo" data-to="57890"></div>
+                    </div>
+                    <div class="block-content block-content-full block-content-mini bg-gray-lighter text-muted font-w600">This Month</div>
+                </a>
+            </div>
+        </div>
+        <!-- END Header Tiles -->
+        
+        
         <div class="row font-s13">
             
         </div>
