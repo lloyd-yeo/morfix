085e6055ca8215a3e5e14dcbf178a68762e164c0
Lloyd Yeo <lloyd@kooki.co>
2017-05-09 14:01:31 +0800

    Lazy Commit!

--- app/InstagramProfileComment.php
+++ app/InstagramProfileComment.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class InstagramProfileComment extends Model
 {
+    protected $primaryKey = 'comment_id'; 
     protected $table = "user_insta_profile_comment";
     protected $connection = "mysql_old";
     public $timestamps = false;
