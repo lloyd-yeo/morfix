8f18640856a11a08ef3b2e956aaf8728632b7972
Lloyd Yeo <lloyd@kooki.co>
2017-05-10 03:37:11 +0800

    Lazy Commit!

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -47,6 +47,8 @@ class Kernel extends ConsoleKernel {
         $schedule->command('dm:get 0 0 ailyndigital@gmail.com')->everyFiveMinutes();
         $schedule->command('dm:get 0 0 julian.kussin@gmail.com')->everyFiveMinutes();
         $schedule->command('dm:get 0 0 chuanian@hotmail.com')->everyFiveMinutes();
+        $schedule->command('dm:get 0 0 levelslifestyleco@gmail.com')->everyFiveMinutes();
+        $schedule->command('dm:get 0 0 fabiopersonaltrainer@hotmail.com')->everyFiveMinutes();
         
         for ($i = 0; $i < 50; $i++) {
             $counter = $i * 20;
@@ -61,6 +63,7 @@ class Kernel extends ConsoleKernel {
         $schedule->command('interaction:like 0 0 peizhisim@gmail.com')->everyFiveMinutes();
         $schedule->command('interaction:like 0 0 julian.kussin@gmail.com')->everyFiveMinutes();
         $schedule->command('interaction:like 0 0 chuanian@hotmail.com')->everyFiveMinutes();
+        $schedule->command('interaction:like 0 0 fabiopersonaltrainer@hotmail.com')->everyFiveMinutes();
         
         $schedule->command("engagement:add")->everyThirtyMinutes()->withoutOverlapping();
         $schedule->command("analysis:follower")->daily("00:00")->withoutOverlapping();
