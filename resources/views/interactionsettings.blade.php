cba01fe701f8a8e8e115ac45c5d54d1f23d1e4a3
Lloyd Yeo <lloyd@kooki.co>
2017-05-10 23:51:14 +0800

    Lazy Commit!

--- resources/views/interactionsettings.blade.php
+++ resources/views/interactionsettings.blade.php
@@ -98,7 +98,7 @@
 
                     <div class="block-content" id="auto-interaction-settings-container">
                         <div class="row items-push">
-                            <div class="col-xs-12 col-sm-12 col-lg-6 col-lg-offset-3">
+                            <div class="col-xs-12 col-sm-12 col-lg-9 col-lg-offset-3">
                                 <label class="css-input css-input switch switch-square switch-lg switch-primary">
                                     @if ($ig_profile->auto_like == 1)
                                     <input class="toggle-like-btn" data-id="{{ $ig_profile->id }}" type="checkbox" checked><span></span> <span style='font-size: 15px; font-weight: 600;'><i class='fa fa-heart text-danger'></i> Like</span>
@@ -110,7 +110,7 @@
                                     
                                 </label>
                             </div>
-                            <div class="col-xs-12 col-sm-12 col-lg-6 col-lg-offset-3">
+                            <div class="col-xs-12 col-sm-12 col-lg-9 col-lg-offset-3">
                                 <label class="css-input css-input switch switch-square switch-lg switch-primary">
                                     @if ($ig_profile->auto_comment == 1)
                                     <input class="toggle-comment-btn" data-id="{{ $ig_profile->id }}" type="checkbox" checked><span></span> <span style='font-size: 15px; font-weight: 600;'><i class='fa fa-comments text-primary'></i> Comment</span>
@@ -121,7 +121,7 @@
                                     @endif
                                 </label>
                             </div>
-                            <div class="col-xs-12 col-sm-12 col-lg-6 col-lg-offset-3">
+                            <div class="col-xs-12 col-sm-12 col-lg-9 col-lg-offset-3">
                                 <label class="css-input css-input switch switch-square switch-lg switch-primary">
                                     @if ($ig_profile->auto_follow == 1)
                                     <input class="toggle-follow-btn" data-id="{{ $ig_profile->id }}" type="checkbox" checked><span></span> <span style='font-size: 15px; font-weight: 600;'><i class='si si-user-follow text-modern'></i> Follow</span>
@@ -132,7 +132,7 @@
                                     @endif
                                 </label>
                             </div>
-                            <div class="col-xs-12 col-sm-12 col-lg-6 col-lg-offset-3">
+                            <div class="col-xs-12 col-sm-12 col-lg-9 col-lg-offset-3">
                                 <label class="css-input css-input switch switch-square switch-lg switch-primary">
                                     @if ($ig_profile->auto_follow == 1)
                                     <input class="toggle-unfollow-btn" data-id="{{ $ig_profile->id }}" type="checkbox" checked><span></span> <span style='font-size: 15px; font-weight: 600;'><i class='si si-user-unfollow text-lightred'></i> Unfollow</span>
