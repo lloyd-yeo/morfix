80040be76a4c859eb416da51c8d4c0f7d4420e3f
Lloyd Yeo <lloyd@kooki.co>
2017-05-10 12:40:46 +0800

    Lazy Commit!

--- public/assets/js/pages/base_tables_datatables.js
+++ public/assets/js/pages/base_tables_datatables.js
@@ -18,8 +18,8 @@ var BaseTableDatatables = function() {
     var initDataTableFullPagination = function() {
         jQuery('.js-dataTable-full-pagination').dataTable({
             pagingType: "full_numbers",
-            columnDefs: [ { orderable: false, targets: [ 4 ] } ],
-            pageLength: 10,
+            columnDefs: [ { orderable: false, targets: [ 2 ] } ],
+            pageLength: 5,
             lengthMenu: [[5, 10, 15, 20], [5, 10, 15, 20]]
         });
     };
