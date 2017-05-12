106c70625ff8b03ba0a3ce65ac1b32406d1533fc
Lloyd Yeo <lloyd@kooki.co>
2017-05-12 15:10:32 +0800

    Lazy Commit!

--- resources/views/affiliate/dashboard.blade.php
+++ resources/views/affiliate/dashboard.blade.php
@@ -269,7 +269,8 @@
                                 <td><label class="label label-primary">Business</label></td>
                                 @endif
                                 
-                                <td>{{ \Carbon\Carbon::parse($invoice->start_date)->toFormattedDateString() }}</td>
+                                <!--<td>{{ \Carbon\Carbon::parse($invoice->start_date)->toFormattedDateString() }}</td>-->
+                                <td>{{ $invoice->start_date }}</td>
                                 @if ($invoice->charge_refunded == 1)
                                 <td><label class="label label-default">Refunded</label></td>
                                 @else
