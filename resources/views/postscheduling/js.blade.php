7e1609e9b6042a64a9803fd5db5c3e9275c0e0b7
Lloyd Yeo <lloyd@kooki.co>
2017-05-17 20:31:44 +0800

    Lazy Commit!

--- resources/views/postscheduling/js.blade.php
+++ resources/views/postscheduling/js.blade.php
@@ -31,7 +31,7 @@ jQuery(function () {
         dictDefaultMessage: '<span class="text-center"><span class="font-lg visible-xs-block visible-sm-block visible-lg-block"><span class="font-lg"><i class="fa fa-caret-right text-danger"></i> Drop files <span class="font-xs">to upload</span></span><span>&nbsp&nbsp<h4 class="display-inline"> (Or Click)</h4></span>',
         dictResponseError: 'Error uploading file!',
         headers: {
-            'X-CSRFToken': $('meta[name="token"]').attr('content')
+            'X-CSRFToken': $('meta[name="csrf-token"]').attr('content')
         }
     });
 });
