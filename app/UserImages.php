0a7bb6e866ad974af507f69c3b64b2af3f9d0190
Lloyd Yeo <lloyd@kooki.co>
2017-05-18 10:24:24 +0800

    Lazy Commit!

--- app/UserImages.php
+++ app/UserImages.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class UserImages extends Model {
-
+    public $timestamps = false;
     protected $table = 'user_images';
     protected $connection = 'mysql_old';
 }
