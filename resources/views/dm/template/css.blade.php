7498caf26267abaf8e741cedc6fe6f97f3a73960
Lloyd Yeo <lloyd@kooki.co>
2017-05-18 11:20:39 +0800

    Lazy Commit!

--- resources/views/dm/template/css.blade.php
+++ resources/views/dm/template/css.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="{{ asset('assets/js/plugins/slick/slick.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/js/plugins/slick/slick-theme.min.css') }}">
+<!--<link rel="stylesheet" href="{{ asset('assets/js/plugins/slick/slick.min.css') }}">-->
+<!--<link rel="stylesheet" href="{{ asset('assets/js/plugins/slick/slick-theme.min.css') }}">-->
 <link rel="stylesheet" href="{{ asset('assets/js/plugins/select2/select2.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/js/plugins/jquery-auto-complete/jquery.auto-complete.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/js/plugins/ion-rangeslider/css/ion.rangeSlider.min.css') }}">
