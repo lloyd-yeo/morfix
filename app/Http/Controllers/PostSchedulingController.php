5e7966744dd6ddf575f3808d08f2fd9c79b16dba
Lloyd Yeo <lloyd@kooki.co>
2017-05-18 08:27:12 +0800

    Lazy Commit!

--- app/Http/Controllers/PostSchedulingController.php
+++ app/Http/Controllers/PostSchedulingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\File;
-Illuminate\Http\UploadedFile;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
