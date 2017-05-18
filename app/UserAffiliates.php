ddd197bc97e3c93b48ccb4d4b0c9a0e350d62285
Lloyd Yeo <lloyd@kooki.co>
2017-05-18 10:21:27 +0800

    Lazy Commit!

--- app/UserAffiliates.php
+++ app/UserAffiliates.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class UserAffiliates extends Model {
 
     protected $table = 'user_affiliates';
-
+    protected $connection = 'mysql_old';
 }
--- app/UserImages.php
+++ app/UserImages.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class UserImages extends Model {
 
     protected $table = 'user_images';
-
+    protected $connection = 'mysql_old';
 }
