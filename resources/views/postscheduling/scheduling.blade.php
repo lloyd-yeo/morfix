2a1c60e1dd4b137e3ff3496c00f98f4854797654
Lloyd Yeo <lloyd@kooki.co>
2017-05-18 11:41:00 +0800

    Lazy Commit!

--- resources/views/postscheduling/scheduling.blade.php
+++ resources/views/postscheduling/scheduling.blade.php
@@ -200,17 +200,9 @@
                                 </div>
                             </div>
                             @endif
-<!--                            <div class="form-group">
-                                <div class="col-md-8">
-                                    <div class="form-material">
-                                        <input class="js-datetimepicker form-control" type="text" 
-                                               id="example-datetimepicker5" name="example-datetimepicker5" placeholder="Choose a date..">
-                                        <label for="example-datetimepicker5">Schedule Date</label>
-                                    </div>
-                                </div>
-                            </div>-->
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Inline</label>
+                                <label class="col-md-4 control-label">Schedule Date</label>
+                                <br/>
                                 <div class="col-md-6">
                                     <div class="js-datetimepicker" data-show-today-button="true" data-inline="true"></div>
                                 </div>
