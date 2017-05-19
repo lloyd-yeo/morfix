788fdc5874cc220221f9b3116c9d73f3af67b7d0
Lloyd Yeo <lloyd@kooki.co>
2017-05-19 19:07:02 +0800

    Lazy Commit!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,81 +139,9 @@ window.Laravel = {!! json_encode([
                         <div class="block-content">
                             
                             <div class="row text-center">
-                                
-                                <div class="col-lg-6">
-                                    <!-- PREMIUM Plan -->
-                                    <div class="block block-bordered block-link-hover3 text-center" href="javascript:void(0)">
-                                        <div class="block-header" style='padding-top: 10px; 
-                                             padding-left: 10px; padding-right: 10px; padding-bottom: 5px;
-                                             border-bottom: none;'>
-                                            <div style='padding:5px; background-color:rgb(30, 115, 190); height: 274px;'>
-                                                <h3 class="push-20-t block-title h1 font-w700 text-white" 
-                                                    style="font-family: 'Montserrat', sans-serif; font-size: 25px;">PREMIUM</h3>
-                                                <div class="push-10-t block-title h6 text-white" 
-                                                     style="font-size: 14px; text-transform: none; font-weight: 100;
-                                                     font-style: italic; font-family: 'Lora', serif;">&nbsp;</div>
-                                                <div class="push-20-t text-white" 
-                                                     style="font-family: 'Montserrat', sans-serif; 
-                                                     font-style: normal; font-weight: normal; height: 200px;">
-                                                    <span style="font-size: 30px; top: -20px; position:relative;">$</span>
-                                                    <span style="font-size: 60px; top: -20px; padding-left: 10px;">37/mth</span>
-                                                    <div class="push-30-t block-title h6 text-white" 
-                                                         style="font-size: 14px; text-transform: none; font-weight: 100;
-                                                         font-style: italic; font-family: 'Lora', serif;">Make Affiliate Commission</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="block-content" style='padding-top: 0px;
-                                             padding-left: 10px; padding-right: 10px; padding-bottom: 10px;'>
-                                            <div style='background-color:#f5f5f5;'>
-                                                <div style="font-family: 'Raleway'; padding-top: 35px; font-weight: 700; 
-                                                     padding-bottom: 20px; padding-left: 30px; padding-right: 30px;">
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        No Morfix.co branding
-                                                    </p>
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        Full Speed
-                                                    </p>
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        High Priority Support
-                                                    </p>
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        Instagram Affiliate Training
-                                                    </p>
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        Auto Interaction (Like, Comment Follow, Unfollow)
-                                                    </p>
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        Greet New Followers With Direct Message
-                                                    </p>
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        Unlimited Scheduled Posts
-                                                    </p>
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        First Comment Function
-                                                    </p>
-                                                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                                                        Private Facebook Group
-                                                    </p>
-                                                </div>
-                                                <div style='padding-top: 0px; padding-bottom: 40px;'>
-                                                    <a href='/app/register.php' class="text-white" style='
-                                                       background-color:rgb(30, 115, 190); 
-                                                       padding: 10px 50px;
-                                                       font-weight: 600; 
-                                                       font-size: 15px; 
-                                                       border: solid 1px #D3D3D3;
-                                                       text-align: center;
-                                                       text-transform: uppercase;'>
-                                                        Try Now
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- END PREMIUM Plan -->
-                                </div>
-                                
+                                @if (Auth::user()->tier == 1)
+                                @include('payment.table.premium')
+                                @endif
                             </div>
                         </div>
                     </div>
