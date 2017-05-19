77721bfcc53f1c26dfe0dcff99191f2faadb5889
Lloyd Yeo <lloyd@kooki.co>
2017-05-19 22:20:45 +0800

    Lazy Commit!

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Response;
+use App\User;
+use App\InstagramProfile;
+use App\Niche;
+use App\InstagramProfileComment;
+use App\InstagramProfileTargetHashtag;
+use App\InstagramProfileTargetUsername;
+use App\DefaultImageGallery;
+use App\UserImages;
+use App\InstagramProfilePhotoPostSchedule;
 
 class PaymentController extends Controller
 {
     function upgrade(Request $request, $plan) {
-        return $plan;
+        return response()->json(["plan" => $plan]);
         #return view('payment.index', [
         #]);
     }
