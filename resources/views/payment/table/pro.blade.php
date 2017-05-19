313ffb8603c54ad23c1deddd2354c9ab2861212a
Lloyd Yeo <lloyd@kooki.co>
2017-05-19 19:11:41 +0800

    Lazy Commit!

--- resources/views/payment/table/pro.blade.php
+++ resources/views/payment/table/pro.blade.php
@@ -6,7 +6,7 @@
              border-bottom: none;'>
             <div style='padding:5px; background-color:rgb(30, 115, 190); height: 274px;'>
                 <h3 class="push-20-t block-title h1 font-w700 text-white" 
-                    style="font-family: 'Montserrat', sans-serif; font-size: 25px;">PREMIUM</h3>
+                    style="font-family: 'Montserrat', sans-serif; font-size: 25px;">Pro</h3>
                 <div class="push-10-t block-title h6 text-white" 
                      style="font-size: 14px; text-transform: none; font-weight: 100;
                      font-style: italic; font-family: 'Lora', serif;">&nbsp;</div>
@@ -14,10 +14,10 @@
                      style="font-family: 'Montserrat', sans-serif; 
                      font-style: normal; font-weight: normal; height: 200px;">
                     <span style="font-size: 30px; top: -20px; position:relative;">$</span>
-                    <span style="font-size: 60px; top: -20px; padding-left: 10px;">37/mth</span>
+                    <span style="font-size: 60px; top: -20px; padding-left: 10px;">370/mth</span>
                     <div class="push-30-t block-title h6 text-white" 
                          style="font-size: 14px; text-transform: none; font-weight: 100;
-                         font-style: italic; font-family: 'Lora', serif;">Make Affiliate Commission!</div>
+                         font-style: italic; font-family: 'Lora', serif;">Save & learn!</div>
                 </div>
             </div>
         </div>
@@ -27,31 +27,13 @@
                 <div style="font-family: 'Raleway'; padding-top: 35px; font-weight: 700; 
                      padding-bottom: 20px; padding-left: 30px; padding-right: 30px;">
                     <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        No Morfix.co branding
+                        Everything from Premium & more!
                     </p>
                     <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        Full Speed
+                        2 months of <b>FREE</b> premium functions
                     </p>
                     <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        High Priority Support
-                    </p>
-                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        Instagram Affiliate Training
-                    </p>
-                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        Auto Interaction (Like, Comment Follow, Unfollow)
-                    </p>
-                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        Greet New Followers With Direct Message
-                    </p>
-                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        Unlimited Scheduled Posts
-                    </p>
-                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        First Comment Function
-                    </p>
-                    <p style="border-bottom: 1px dotted #bdbdbd; line-height: 50px; color: rgb(0, 0, 0); text-align:center; font-size: 14px;">
-                        Private Facebook Group
+                        EXCLUSIVE access to our 6 figures Instagram System training videos!
                     </p>
                 </div>
                 <div style='padding-top: 0px; padding-bottom: 40px;'>
@@ -63,7 +45,7 @@
                        border: solid 1px #D3D3D3;
                        text-align: center;
                        text-transform: uppercase;'>
-                        UPGRADE
+                        Try Now
                     </a>
                 </div>
             </div>
