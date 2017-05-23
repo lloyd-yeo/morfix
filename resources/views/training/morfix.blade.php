e8765528846df0201b077e4113bc726bd557b858
Lloyd Yeo <lloyd@kooki.co>
2017-05-23 14:24:23 +0800

    Lazy Commit!

--- resources/views/training/morfix.blade.php
+++ resources/views/training/morfix.blade.php
@@ -21,33 +21,61 @@
     </div>
     
     <div class="content content-boxed">
-            <div class='row'>
-                <div class="col-lg-4">
-                    <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#upgrade-dm-modal">
-                        <div class="block-content block-content-full bg-primary clearfix">
-                            <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
-                            <span class="h4 font-w700 text-white">Part 1</span> - <span class="h4 text-white-op">Dashboard</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4">
-                    <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#upgrade-dm-modal">
-                        <div class="block-content block-content-full bg-primary clearfix">
-                            <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
-                            <span class="h4 font-w700 text-white">Part 2</span> - <span class="h4 text-white-op">Profile</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4">
-                    <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#upgrade-dm-modal">
-                        <div class="block-content block-content-full bg-primary clearfix">
-                            <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
-                            <span class="h4 font-w700 text-white">Part 3</span> - <span class="h4 text-white-op">Auto Interaction</span>
-                        </div>
-                    </a>
-                </div>
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#dashboard">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 1</span> - <span class="h4 text-white-op">Dashboard</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#profile">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 2</span> - <span class="h4 text-white-op">Profile</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#autointeraction">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 3</span> - <span class="h4 text-white-op">Auto Interaction</span>
+                    </div>
+                </a>
             </div>
         </div>
+        
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#directmessage">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 4</span> - <span class="h4 text-white-op">Direct Messages</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#photoscheduling">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 5</span> - <span class="h4 text-white-op">Post Scheduling</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#dmalgorithm">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 6</span> - <span class="h4 text-white-op">Beating the DM Algorithm</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        
+    </div>
 </main>
 @endsection
 
