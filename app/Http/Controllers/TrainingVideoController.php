d985f39de9a900cd5c52ac2126509d01595c3a72
Lloyd Yeo <lloyd@kooki.co>
2017-05-23 14:46:02 +0800

    Lazy Commit!

--- app/Http/Controllers/TrainingVideoController.php
+++ app/Http/Controllers/TrainingVideoController.php
@@ -34,9 +34,33 @@ class TrainingVideoController extends Controller
             ]);
             
         } else if ($type == "affiliate") {
+            
+            $iframe_welcome = "<iframe src=\"https://player.vimeo.com/video/199810006\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_comms = "<iframe src=\"https://player.vimeo.com/video/199810127\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_affiliatetrg = "<iframe src=\"https://player.vimeo.com/video/199810177\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_affiliatearea = "<iframe src=\"https://player.vimeo.com/video/201139129\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_optin = "<iframe src=\"https://player.vimeo.com/video/201139252\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_autoresponder = "<iframe src=\"https://player.vimeo.com/video/201139168\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_ebook = "<iframe src=\"https://player.vimeo.com/video/216167875\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+
+            $morfix_training_video = array("i-welcome" => $iframe_welcome, "i-comms" => $iframe_comms,
+                "i-affiliatetrg" => $iframe_affiliatetrg, "i-affiliatearea" => $iframe_affiliatearea,
+                "i-optin" => $iframe_optin, "i-autoresponder" => $iframe_autoresponder, "i-ebook" => $iframe_ebook);
+
+            $morfix_training_video_header = array(
+                "i-welcome" => "Part 1 - Welcome Message", 
+                "i-comms" => "Part 2 - Commission Plan",
+                "i-affiliatetrg" => "Part 3 - Affiliate Training", 
+                "i-affiliatearea" => "Part 4 - Affiliate Area",
+                "i-optin" => "Part 5 - Opt In Page", 
+                "i-autoresponder" => "Part 6 - Auto Responder", 
+                "i-ebook" => "Part 7 - Bonus Ebook");
+            
             return view('training.affiliate', [
-                
+                'morfix_training_video' => $morfix_training_video,
+                'morfix_training_video_header' => $morfix_training_video_header,
             ]);
+            
         } else if ($type == "6figureprofile") {
             return view('training.6figure', [
                 
--- resources/views/training/affiliate.blade.php
+++ resources/views/training/affiliate.blade.php
@@ -19,6 +19,105 @@
             </div>
         </div>
     </div>
+    
+    <div class="content content-boxed">
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#dashboard">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 1</span> - <span class="h4 text-white-op">Welcome Message</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#profile">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 2</span> - <span class="h4 text-white-op">Commission Plan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#autointeraction">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 3</span> - <span class="h4 text-white-op">Affiliate Training</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#directmessage">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 4</span> - <span class="h4 text-white-op">Affiliate Area</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#photoscheduling">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 5</span> - <span class="h4 text-white-op">Opt In Page</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#dmalgorithm">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 6</span> - <span class="h4 text-white-op">Auto Responder</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#directmessage">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 7</span> - <span class="h4 text-white-op">Bonus EBook</span>
+                    </div>
+                </a>
+            </div>
+        </div>        
+    </div>
+    
+    @foreach($morfix_training_video as $training => $training_video_iframe)
+    <!-- Apps Modal -->
+    <!-- Opens from the button in the header -->
+    <div class="modal fade" id="{{ $training }}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-lg modal-dialog modal-dialog-top">
+            <div class="modal-content">
+                <!-- Apps Block -->
+                <div class="block block-themed block-transparent">
+                    <div class="block-header bg-modern">
+                        <ul class="block-options">
+                            <li>
+                                <button data-dismiss="modal" type="button"><i class="si si-close"></i></button>
+                            </li>
+                        </ul>
+                        <h3 class="block-title">{{ $morfix_training_video_header[$training] }}</h3>
+                    </div>
+                    <div class="block-content">
+                        <div class='row'>
+                            <div class="embed-responsive embed-responsive-16by9">
+                                {!! $training_video_iframe !!}
+                            </div>
+                            <div class="modal-footer">
+                                <button id="closetutorial-btn" data-dismiss="modal" class="btn blue lighten-1" type="button">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END Apps Block -->
+            </div>
+        </div>
+    </div>
+    <!-- END Apps Modal -->
+    @endforeach
 </main>
 @endsection
 
