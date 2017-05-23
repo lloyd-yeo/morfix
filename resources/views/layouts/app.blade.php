014579c8cf3ca363c8cf2357ee4d04529852d5bf
Lloyd Yeo <lloyd@kooki.co>
2017-05-23 10:29:36 +0800

    Lazy Commit!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -144,7 +144,7 @@
                                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                                     <h3 class="font-w300 push-15">Free Trial</h3>
                                     <p>You are currently on the <a class="alert-link" href="javascript:void(0)">free trial</a> version of Morfix!</p>
-                                    <p>Upgrade below to enjoy more functions!</p>
+                                    <p>Upgrade below to enjoy more features!</p>
                                 </div>
                                 <!-- END Warning Alert -->
                             </div>
