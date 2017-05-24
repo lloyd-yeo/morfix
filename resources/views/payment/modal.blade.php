602113d00dc48a23e93a69b3ee294bca044151ec
Lloyd Yeo <lloyd@kooki.co>
2017-05-24 10:30:48 +0800

    Lazy Commit!

--- resources/views/payment/modal.blade.php
+++ resources/views/payment/modal.blade.php
@@ -233,7 +233,102 @@
                                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                                 <h3 class="font-w300 push-15"><i class='fa fa-info-circle'></i> Unlock follow-up messaging!</h3>
                                 <p>You are currently on the <a class="alert-link" href="javascript:void(0)">free trial</a> version of Morfix!</p>
-                                <p>Upgrade now to become an affiliate of Morfix & learn how to run your very own affiliate marketing business!</p>
+                                <p>Upgrade now to unlock follow-up messaging that helps to increase responsiveness of leads & put your conversations on auto-pilot!</p>
+                            </div>
+                        </div>
+                        <!-- END Warning Alert -->
+                        @elseif (Auth::user()->tier == 2)
+                        <!-- Warning Alert -->
+                        <div class='col-lg-12'>
+                            <div class="alert alert-warning alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <h3 class="font-w300 push-15"><i class='fa fa-info-circle'></i> Unlock follow-up messaging!</h3>
+                                <p>You are currently on the <a class="alert-link" href="javascript:void(0)">Premium</a> version of Morfix!</p>
+                                <p>Upgrade now to unlock follow-up messaging that helps to increase responsiveness of leads & put your conversations on auto-pilot!</p>
+                            </div>
+                        </div>
+                        <!-- END Warning Alert -->
+                        @elseif (Auth::user()->tier == 3)
+                        <!-- Warning Alert -->
+                        <div class='col-lg-12'>
+                            <div class="alert alert-warning alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <h3 class="font-w300 push-15"><i class='fa fa-info-circle'></i> Unlock follow-up messaging!</h3>
+                                <p>You are currently on the <a class="alert-link" href="javascript:void(0)">Pro</a> version of Morfix!</p>
+                                <p>Upgrade now to unlock follow-up messaging that helps to increase responsiveness of leads & put your conversations on auto-pilot!</p>
+                            </div>
+                        </div>
+                        <!-- END Warning Alert -->
+                        @endif
+                    </div>
+                    <div class="row text-center">
+                        @if (Auth::user()->tier == 1)
+                        <div class='col-lg-3'></div>
+                        @include('payment.table.premium-business')
+                        @elseif (Auth::user()->tier == 2)
+                        @include('payment.table.business')
+                        @include('payment.table.mastermind')
+                        @elseif (Auth::user()->tier == 3)
+                        @include('payment.table.business')
+                        @include('payment.table.mastermind')
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <!-- END Apps Block -->
+        </div>
+    </div>
+</div>
+<!-- END Apps Modal -->
+
+
+<!-- Apps Modal -->
+<!-- Opens from the button in the header -->
+<div class="modal fade" id="upgrade-engagement-group-modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-lg modal-dialog modal-dialog-top">
+        <div class="modal-content">
+            <!-- Apps Block -->
+            <div class="block block-themed block-transparent">
+                <div class="block-header bg-modern">
+                    <ul class="block-options">
+                        <li>
+                            <button data-dismiss="modal" type="button"><i class="si si-close"></i></button>
+                        </li>
+                    </ul>
+                    <h3 class="block-title">Upgrade your account now!</h3>
+                </div>
+                <div class="block-content">
+                    <div class='row'>
+                        @if (Auth::user()->tier == 1)
+                        <!-- Warning Alert -->
+                        <div class='col-lg-12'>
+                            <div class="alert alert-warning alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <h3 class="font-w300 push-15"><i class='fa fa-info-circle'></i> Unlock the Engagement Group function!</h3>
+                                <p>You are currently on the <a class="alert-link" href="javascript:void(0)">free trial</a> version of Morfix!</p>
+                                <p>Upgrade now to enjoy our engagement group function to let your post receive influencer-level exposure!</p>
+                            </div>
+                        </div>
+                        <!-- END Warning Alert -->
+                        @elseif (Auth::user()->tier == 2)
+                        <!-- Warning Alert -->
+                        <div class='col-lg-12'>
+                            <div class="alert alert-warning alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <h3 class="font-w300 push-15"><i class='fa fa-info-circle'></i> Unlock the Engagement Group function!</h3>
+                                <p>You are currently on the <a class="alert-link" href="javascript:void(0)">Premium</a> version of Morfix!</p>
+                                <p>Upgrade now to enjoy our engagement group function to let your post receive influencer-level exposure!</p>
+                            </div>
+                        </div>
+                        <!-- END Warning Alert -->
+                        @elseif (Auth::user()->tier == 3)
+                        <!-- Warning Alert -->
+                        <div class='col-lg-12'>
+                            <div class="alert alert-warning alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <h3 class="font-w300 push-15"><i class='fa fa-info-circle'></i> Unlock the Engagement Group function!</h3>
+                                <p>You are currently on the <a class="alert-link" href="javascript:void(0)">Pro</a> version of Morfix!</p>
+                                <p>Upgrade now to enjoy our engagement group function to let your post receive influencer-level exposure!</p>
                             </div>
                         </div>
                         <!-- END Warning Alert -->
