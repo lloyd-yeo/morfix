9677e3843abc28daec319e474c76f7f78ed96927
Lloyd Yeo <lloyd@kooki.co>
2017-05-24 11:31:33 +0800

    Lazy Commit!

--- resources/views/settings/css.blade.php
+++ resources/views/settings/css.blade.php
@@ -1,8 +1,1 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<link rel="stylesheet" href="{{ asset('assets/js/plugins/datatables/jquery.dataTables.min.css') }}">
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -1,8 +1,74 @@
-<?php
+@extends('layouts.app')
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+@section('css')
+@include('settings.css')
+@endsection
 
+@section('sidebar')
+@include('sidebar', ['page' => 'settings'])
+@endsection
+
+@section('content')
+<main id="main-container">
+    <div class="content bg-gray-lighter">
+        <div class="row items-push">
+            <div class="col-sm-7">
+                <h1 class="page-heading">
+                    <i class="si si-settings"></i> Settings <small> Manage your account settings here.</small>
+                </h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="content content-boxed">
+        <div class="row font-s13">
+            <!-- DataTables init on table by adding .js-dataTable-full class, functionality initialized in js/pages/base_tables_datatables.js -->
+            <table class="table table-bordered table-striped js-dataTable-subscription">
+                <thead>
+                    <tr>
+                        <th class="text-center"></th>
+                        <th>Subscription</th>
+                        <th class="hidden-xs">Email</th>
+                        <th class="hidden-xs" style="width: 15%;">Access</th>
+                        <th class="text-center" style="width: 10%;">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-center">1</td>
+                        <td class="font-w600">Roger Hart</td>
+                        <td class="hidden-xs">client1@example.com</td>
+                        <td class="hidden-xs">
+                            <span class="label label-info">Business</span>
+                        </td>
+                        <td class="text-center">
+                            <div class="btn-group">
+                                <button class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="Edit Client"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="Remove Client"><i class="fa fa-times"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">1</td>
+                        <td class="font-w600">Roger Hart</td>
+                        <td class="hidden-xs">client1@example.com</td>
+                        <td class="hidden-xs">
+                            <span class="label label-info">Business</span>
+                        </td>
+                        <td class="text-center">
+                            <div class="btn-group">
+                                <button class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="Edit Client"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-xs btn-default" type="button" data-toggle="tooltip" title="Remove Client"><i class="fa fa-times"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</main>
+@endsection
+
+@section('js')
+@include('settings.js')
+@endsection
