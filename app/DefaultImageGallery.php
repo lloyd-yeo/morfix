3c8e81f5783c51f8ab31556cfd9e3be4bfc6461d
Lloyd Yeo <lloyd@kooki.co>
2017-05-29 17:16:25 +0800

    Lazy Commit!

--- app/DefaultImageGallery.php
+++ app/DefaultImageGallery.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class DefaultImageGallery extends Model {
+    protected $primaryKey = 'image_id'; 
     protected $table = 'default_image_gallery';
     protected $connection = 'mysql_old';
 }
