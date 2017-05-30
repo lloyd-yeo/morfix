034d080b72315d9fa714dd8edab060936319da89
Lloyd Yeo <lloyd@kooki.co>
2017-05-30 21:39:08 +0800

    Lazy Commit!

--- app/InstagramProfileTargetUsername.php
+++ app/InstagramProfileTargetUsername.php
@@ -8,5 +8,6 @@ class InstagramProfileTargetUsername extends Model {
 
     protected $table = 'user_insta_target_username';
     protected $connection = 'mysql_old';
+    protected $primaryKey = 'target_id'; 
     public $timestamps = false;
 }
