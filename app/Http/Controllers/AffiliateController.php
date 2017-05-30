83824197417f91edc0d16053fc5af15f415a4d6f
Lloyd Yeo <lloyd@kooki.co>
2017-05-30 17:46:08 +0800

    Lazy Commit!

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\YourlsUrl;
+use App\User;
 
 class AffiliateController extends Controller {
 
