b26aa14b274f0fcfba9593478358faaeee3000c9
Lloyd Yeo <lloyd@kooki.co>
2017-05-30 17:19:24 +0800

    Lazy Commit!

--- app/Http/Controllers/DirectMessageTemplatesController.php
+++ app/Http/Controllers/DirectMessageTemplatesController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\InstagramProfile;
+use App\DmJob;
 use Response;
 
 class DirectMessageTemplatesController extends Controller
