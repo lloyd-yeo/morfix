1c9a5e6d7071aafa090cafb411f7e5797e2acdff
Lloyd Yeo <lloyd@kooki.co>
2017-05-30 19:06:45 +0800

    Lazy Commit!

--- app/Http/Controllers/TrainingVideoController.php
+++ app/Http/Controllers/TrainingVideoController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TrainingVideoController extends Controller
-{
+class TrainingVideoController extends Controller {
+
     public function __construct() {
         $this->middleware('auth');
     }
-    
+
     function index(Request $request, $type) {
         if ($type == "morfix") {
-            
+
             $iframe_dashboard = "<iframe src=\"https://player.vimeo.com/video/192896299\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             $iframe_profile = "<iframe src=\"https://player.vimeo.com/video/192896315\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             $iframe_autointeraction = "<iframe src=\"https://player.vimeo.com/video/192896261\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
@@ -32,9 +32,8 @@ class TrainingVideoController extends Controller
                 'morfix_training_video' => $morfix_training_video,
                 'morfix_training_video_header' => $morfix_training_video_header,
             ]);
-            
         } else if ($type == "affiliate") {
-            
+
             $iframe_welcome = "<iframe src=\"https://player.vimeo.com/video/199810006\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             $iframe_comms = "<iframe src=\"https://player.vimeo.com/video/199810127\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             $iframe_affiliatetrg = "<iframe src=\"https://player.vimeo.com/video/199810177\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
@@ -48,23 +47,82 @@ class TrainingVideoController extends Controller
                 "i-optin" => $iframe_optin, "i-autoresponder" => $iframe_autoresponder, "i-ebook" => $iframe_ebook);
 
             $morfix_training_video_header = array(
-                "i-welcome" => "Part 1 - Welcome Message", 
+                "i-welcome" => "Part 1 - Welcome Message",
                 "i-comms" => "Part 2 - Commission Plan",
-                "i-affiliatetrg" => "Part 3 - Affiliate Training", 
+                "i-affiliatetrg" => "Part 3 - Affiliate Training",
                 "i-affiliatearea" => "Part 4 - Affiliate Area",
-                "i-optin" => "Part 5 - Opt In Page", 
-                "i-autoresponder" => "Part 6 - Auto Responder", 
+                "i-optin" => "Part 5 - Opt In Page",
+                "i-autoresponder" => "Part 6 - Auto Responder",
                 "i-ebook" => "Part 7 - Bonus Ebook");
-            
+
             return view('training.affiliate', [
                 'morfix_training_video' => $morfix_training_video,
                 'morfix_training_video_header' => $morfix_training_video_header,
             ]);
-            
         } else if ($type == "6figureprofile") {
+
+            $iframe_setup = "<iframe src=\"https://player.vimeo.com/video/214766168\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_bio = "<iframe src=\"https://player.vimeo.com/video/215135861\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_secure = "<iframe src=\"https://player.vimeo.com/video/214766196\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_accttype = "<iframe src=\"https://player.vimeo.com/video/214766217\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_content = "<iframe src=\"https://player.vimeo.com/video/214766262\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_content2 = "<iframe src=\"https://player.vimeo.com/video/214766286\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_content3 = "<iframe src=\"https://player.vimeo.com/video/214765973\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_createcontent = "<iframe src=\"https://player.vimeo.com/video/215135912\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_posttiming = "<iframe src=\"https://player.vimeo.com/video/214766000\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_repost = "<iframe src=\"https://player.vimeo.com/video/214766014\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_whotofollow = "<iframe src=\"https://player.vimeo.com/video/215135955\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_hashtag = "<iframe src=\"https://player.vimeo.com/video/214766033\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_engagementgroup = "<iframe src=\"https://player.vimeo.com/video/215135978\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_analytics = "<iframe src=\"https://player.vimeo.com/video/214766068\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_shoutout = "<iframe src=\"https://player.vimeo.com/video/214766094\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_influencer = "<iframe src=\"https://player.vimeo.com/video/214766125\" width=\"640\" height=\"1138\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+            $iframe_rates = "<iframe src=\"https://player.vimeo.com/video/214766149\" width=\"640\" height=\"400\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+
+            $morfix_training_video = array(
+                "ig-setup" => $iframe_setup,
+                "ig-bio" => $iframe_bio,
+                "ig-secure" => $iframe_secure,
+                "ig-accttype" => $iframe_accttype,
+                "ig-content" => $iframe_content,
+                "ig-content2" => $iframe_content2,
+                "ig-content3" => $iframe_content3,
+                "ig-createcontent" => $iframe_createcontent,
+                "ig-posttiming" => $iframe_posttiming,
+                "ig-repost" => $iframe_repost,
+                "ig-whotofollow" => $iframe_whotofollow,
+                "ig-hashtag" => $iframe_hashtag,
+                "ig-engagementgroup" => $iframe_engagementgroup,
+                "ig-analytics" => $iframe_analytics,
+                "ig-shoutout" => $iframe_shoutout,
+                "ig-influencer" => $iframe_influencer,
+                "ig-rates" => $iframe_rates
+            );
+
+            $morfix_training_video_header = array(
+                "ig-setup" => "Setting up Instagram to Win",
+                "ig-bio" => "Bio & Link in Bio",
+                "ig-secure" => "Securing your Account",
+                "ig-accttype" => "Types of Accounts",
+                "ig-content" => "Finding Great Content (Part 1)",
+                "ig-content2" => "Finding Great Content (Part 2)",
+                "ig-content3" => "Finding Great Content (Part 3)",
+                "ig-createcontent" => "Creating Instagram Content",
+                "ig-posttiming" => "Timing to Post",
+                "ig-repost" => "How to Repost",
+                "ig-whotofollow" => "Who to Follow",
+                "ig-hashtag" => "Finding Hashtags",
+                "ig-engagementgroup" => "Engagement Group",
+                "ig-analytics" => "Instagram Analytics",
+                "ig-shoutout" => "Shoutout",
+                "ig-influencer" => "Reaching Out to Influencers",
+                "ig-rates" => "Negotiate Rates");
+
             return view('training.6figure', [
-                
+                'morfix_training_video' => $morfix_training_video,
+                'morfix_training_video_header' => $morfix_training_video_header,
             ]);
         }
     }
+
 }
--- resources/views/training/6figure.blade.php
+++ resources/views/training/6figure.blade.php
@@ -19,6 +19,144 @@
             </div>
         </div>
     </div>
+    
+    <div class="content content-boxed">
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-setup">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 1</span> - <span class="h4 text-white-op">Setting up Instagram to Win</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-bio">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 2</span> - <span class="h4 text-white-op">Bio & Link in Bio</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-secure">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 3</span> - <span class="h4 text-white-op">Securing your Account</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-accttype">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 4</span> - <span class="h4 text-white-op">Types of Accounts</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-content">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 5</span> - <span class="h4 text-white-op">Finding Great Content (Part 1)</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-content2">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w700 text-white">Part 6</span> - <span class="h4 text-white-op">Finding Great Content (Part 2)</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-content3">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Finding Great Content (Part 3)</span>
+                    </div>
+                </a>
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-createcontent">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Creating Instagram Content</span>
+                    </div>
+                </a>
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-posttiming">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Timing to Post</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-repost">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">How to Repost</span>
+                    </div>
+                </a>
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-whotofollow">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Who to Follow</span>
+                    </div>
+                </a>
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-hashtag">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Finding Hashtags</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-engagementgroup">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Engagement Group</span>
+                    </div>
+                </a>
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-analytics">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Instagram Analytics</span>
+                    </div>
+                </a>
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-shoutout">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Shoutout</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-lg-4">
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-influencer">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Reaching Out to Influencers</span>
+                    </div>
+                </a>
+                <a class="block block-link-hover2" href="#" data-toggle="modal" data-target="#ig-rates">
+                    <div class="block-content block-content-full bg-primary clearfix">
+                        <i class="fa fa-2x fa-play-circle fa-2x text-white pull-left"></i>
+                        <span class="h4 font-w500 text-white">Part 7</span> - <span class="h4 text-white-op">Negotiate Rates</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    
 </main>
 @endsection
 
