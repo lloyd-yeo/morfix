80519d10b8cbd1dd199cd833227847181f816fdc
Lloyd Yeo <lloyd@kooki.co>
2017-05-30 17:51:27 +0800

    Lazy Commit!

--- resources/views/affiliate/js.blade.php
+++ resources/views/affiliate/js.blade.php
@@ -1,6 +1,6 @@
 <!-- Page JS Plugins -->
 <script src="{{ asset('assets/js/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('assets/js/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+<!--<script src="{{ asset('assets/js/plugins/sweetalert2/sweetalert2.min.js') }}"></script>-->
 
 <script>
 $.ajaxSetup({
