80d674e5eb6d69b007b3f0bad199bebae1637c04
Lloyd Yeo <lloyd@kooki.co>
2017-05-30 11:49:43 +0800

    Lazy Commit!

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
-class AffiliateController extends Controller
-{
+class AffiliateController extends Controller {
+
     /**
      * @param Request $request
      * @param null $user_id
      * @return View
      */
     public function index(Request $request) {
-        
+
         $active_users = DB::connection('mysql_old')->select('SELECT * 
                                 FROM insta_affiliate.get_referral_charges_of_user 
                                 WHERE referrer_email = ?
@@ -23,10 +23,15 @@ class AffiliateController extends Controller
                                 AND charge_refunded = 0
                                 AND NOW() >= start_date
                                 AND NOW() <= expiry_date;', [Auth::user()->email]);
+        
+        
+        
         $user_id = Auth::user()->user_id;
+        
         $referral_links = DB::connection('mysql_old')->select('SELECT * FROM yourls_url WHERE url LIKE "%referrer=' . $user_id . '&%";');
         $referrals = DB::connection('mysql_old')->select('SELECT u.email, u.user_tier, u.created_at, a.refunded_premium, a.refunded_pro, a.refunded_business, a.refunded_mastermind 
             FROM user_affiliate a, user u WHERE a.referrer = ? AND a.referred = u.user_id AND u.user_tier > 1 ORDER BY u.created_at DESC;', [Auth::user()->user_id]);
+        
         $invoices = DB::connection('mysql_old')->select('SELECT r.vip AS referrer_vip, r.user_tier AS referrer_user_tier, r.email AS referrer_email, c.charge_id, 
                                                                 i.invoice_id, i.subscription_id, i.start_date, i.expiry_date, u.email AS referred_email,
                                                                 c.paid AS charge_paid, i.paid AS invoice_paid, c.refunded AS charge_refunded
@@ -46,4 +51,5 @@ class AffiliateController extends Controller
             'invoices' => $invoices,
         ]);
     }
+
 }
