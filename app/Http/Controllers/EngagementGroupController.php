5ab07b4d87e623574522d8cfce320a92be5b5c79
Lloyd Yeo <lloyd@kooki.co>
2017-05-31 13:25:13 +0800

    Lazy Commit!

--- app/Http/Controllers/EngagementGroupController.php
+++ app/Http/Controllers/EngagementGroupController.php
@@ -22,11 +22,10 @@ use App\InstagramProfileLikeLog;
 use App\InstagramProfileMedia;
 use Unicodeveloper\Emoji\Emoji;
 use Carbon\Carbon;
-
 use Response;
 
-class EngagementGroupController extends Controller
-{
+class EngagementGroupController extends Controller {
+
     public function __construct() {
         $this->middleware('auth');
     }
@@ -37,19 +36,19 @@ class EngagementGroupController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        
+
 //        $exit_code = Artisan::call('ig:refresh', [
 //            'offset' => 0,
 //            'limit' => 0,
 //            'email' => Auth::user()->email,
 //        ]);
-        
+
         $instagram_profiles = InstagramProfile::where('email', Auth::user()->email)->take(11)->get();
         return view('engagement-group.index', [
             'user_ig_profiles' => $instagram_profiles,
         ]);
     }
-    
+
     public function profile(Request $request, $id) {
         $ig_profile = InstagramProfile::find($id);
         $medias = InstagramProfileMedia::where('insta_username', $ig_profile->insta_username)->orderBy('created_at', 'desc')->get();
@@ -58,22 +57,28 @@ class EngagementGroupController extends Controller
             'medias' => $medias,
         ]);
     }
-    
+
     public function schedule(Request $request, $media_id) {
         $user = User::where('email', Auth::user()->email)->first();
         if ($user->engagement_quota > 0) {
-            $engagement_group_job = EngagementGroupJob::firstOrNew();
-            $engagement_group_job = new EngagementGroupJob;
-            $engagement_group_job->media_id = $media_id;
-            $engagement_group_job->engaged = 0;
-            if ($engagement_group_job->save()) {
-                $user->engagement_quota = $user->engagement_quota-1;
-                $user->save();
+            $engagement_group_job = EngagementGroupJob::where('media_id', '=', $media_id)->first();
+            if ($engagement_group_job === null) {
+                $engagement_group_job = new EngagementGroupJob;
+                $engagement_group_job->media_id = $media_id;
+                $engagement_group_job->engaged = 0;
+                if ($engagement_group_job->save()) {
+                    $user->engagement_quota = $user->engagement_quota - 1;
+                    $user->save();
+                }
+                return Response::json(array("success" => true, 'message' => "Your photo has been sent for engagement group. Expect a increase in engagement."));
+            } else {
+                $engagement_group_job->engaged = 0;
+                $engagement_group_job->save();
+                return Response::json(array("success" => false, 'message' => "Your image has already been sent for engagement before."));
             }
-            return Response::json(array("success" => true, 'message' => "Your photo has been sent for engagement group. Expect a increase in engagement."));
         } else {
             return Response::json(array("success" => false, 'message' => "You've ran out of engagement credits. Do try again tomorrow."));
         }
-        
     }
+
 }
