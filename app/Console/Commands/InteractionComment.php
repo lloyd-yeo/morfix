14a118b67f9655baefd3137f6e5f88a88c925fad
Lloyd Yeo <lloyd@kooki.co>
2017-06-02 16:09:23 +0800

    Lazy Commit!

--- app/Console/Commands/InteractionComment.php
+++ app/Console/Commands/InteractionComment.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Builder;
 use InstagramAPI\Instagram as Instagram;
 use InstagramAPI\SettingsAdapter as SettingsAdapter;
 use InstagramAPI\InstagramException as InstagramException;
