6c14f8fc5693b6db504259a568746b0304d512ab
Lloyd Yeo <lloyd@kooki.co>
2017-06-05 18:35:21 +0800

    Lazy Commit!

--- app/BlacklistedUsername.php
+++ app/BlacklistedUsername.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class BlacklistedUsername extends Model
 {
     protected $table = 'blacklisted_username';
+    protected $primaryKey = "username";
+    protected $connection = "mysql_old";
+    public $timestamps = false;
+    public $incrementing = false;
 }
