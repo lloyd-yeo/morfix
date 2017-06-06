bcaff033c6823e8a3e91a961677d5d1fa374ec50
Lloyd Yeo <lloyd@kooki.co>
2017-06-06 17:28:44 +0800

    Lazy Commit!

--- app/InstagramProfilePhotoPostSchedule.php
+++ app/InstagramProfilePhotoPostSchedule.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class InstagramProfilePhotoPostSchedule extends Model {
 
     protected $table = 'user_insta_photo_post_schedule';
+    protected $primaryKey = 'schedule_id';
     public $timestamps = false;
 }
