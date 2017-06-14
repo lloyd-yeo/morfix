8f247c1c0a25d93ad7ae54fc7497b6ea2cd4f65d
Lloyd Yeo <lloyd@kooki.co>
2017-06-14 13:09:59 +0800

    Lazy Commit!

--- app/YourlsUrl.php
+++ app/YourlsUrl.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class YourlsUrl extends Model {
     protected $table = 'yourls_url';
-    protected $connection = 'mysql_old';
     public $timestamps = false;
+    protected $primaryKey = "keyword";
 }
