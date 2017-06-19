021200895b4989180761e2b4a81be62223f806aa
Lloyd Yeo <lloyd@kooki.co>
2017-06-19 16:59:29 +0800

    Lazy Commit!

--- app/Jobs/GetDm.php
+++ app/Jobs/GetDm.php
@@ -54,7 +54,7 @@ class GetDm implements ShouldQueue {
      * @return void
      */
     public function handle() {
-        
+        DB::reconnect();
         $ig_profile = $this->profile;
         #echo($ig_profile->insta_username . "\t" . $ig_profile->insta_pw);
         $ig_username = $ig_profile->insta_username;
