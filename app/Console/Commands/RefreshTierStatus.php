fc413f508e0096c20a91aa4d5308d4063ba0921a
Lloyd Yeo <lloyd@kooki.co>
2017-06-20 19:16:57 +0800

    Lazy Commit!

--- app/Console/Commands/RefreshTierStatus.php
+++ app/Console/Commands/RefreshTierStatus.php
@@ -47,7 +47,7 @@ class RefreshTierStatus extends Command
 //        }
         \Stripe\Stripe::setApiKey("sk_live_HeS5nnfJ5qARMPsANoGw32c2");
         
-        $subscriptions = \Stripe\Subscription::all();
+        $subscriptions = \Stripe\Subscription::all(array('limit'=>100));
         
         foreach ($subscriptions->autoPagingIterator() as $subscription) {
             $plan = $subscription->plan;
