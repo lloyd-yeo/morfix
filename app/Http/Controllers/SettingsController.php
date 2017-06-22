480d01df0b5e9ecf4f678f746d16a25d436aee27
Lloyd Yeo <lloyd@kooki.co>
2017-06-22 14:48:00 +0800

    Lazy Commit!

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -25,34 +25,36 @@ class SettingsController extends Controller {
         //get recent subscription
         \Stripe\Stripe::setApiKey("sk_live_HeS5nnfJ5qARMPsANoGw32c2");
         $subscriptions = array();
-
+        $invoices = array();
+        
         if (Auth::user()->stripe_id === NULL) {
+            
             $customer = \Stripe\Customer::create(array(
-                        "email" => Auth::user()->email,
+                "email" => Auth::user()->email,
             ));
-
             $user = User::where('email', Auth::user()->email)->first();
             $user->stripe_id = $customer->id;
             $user->save();
+            
         } else {
             $subscriptions_listings = \Stripe\Subscription::all(array('customer' => Auth::user()->stripe_id));
             $subscriptions = $subscriptions_listings->data;
-            
+
             //Remove al active subscription
             $saved_subscriptions = StripeActiveSubscription::where('stripe_id', Auth::user()->stripe_id)->get();
             foreach ($saved_subscriptions as $sub) {
                 $sub->delete();
             }
+
             
-            $invoices = array();
-            
+
             foreach ($subscriptions as $subscription) {
                 //The Invoices under this subscription
                 $invoice_listings = \Stripe\Invoice::all(array("subscription" => $subscription->id));
                 $stripe_id = $subscription->customer;
-                
+
                 $invoices[$subscription->id] = $invoice_listings->data[0];
-                
+
                 $items = $subscription->items->data;
                 foreach ($items as $item) {
                     $plan = $item->plan;
@@ -65,6 +67,7 @@ class SettingsController extends Controller {
                     $active_subscription->end_date = \Carbon\Carbon::createFromTimestamp($subscription->current_period_end);
                     $active_subscription->stripe_subscription_id = $subscription->id;
                     if ($active_subscription->save()) {
+                        
                     }
                 }
             }
@@ -94,6 +97,61 @@ class SettingsController extends Controller {
         return view('settings.index', [
             'updateCreditCardMessage' => "Your card has been updated",
         ]);
+    }
+
+    public function attemptInvoice($invoice_id) {
+
+        try {
+            \Stripe\Stripe::setApiKey("sk_live_HeS5nnfJ5qARMPsANoGw32c2");
+            $invoice = \Stripe\Invoice::retrieve($invoice_id);
+            
+            if ($invoice->pay()->paid == true) {
+                
+                
+                
+                return Response::json(array("success" => true, 'message' => "Your invoice has been paid."));
+            } else {
+                return Response::json(array("success" => false, 'message' => "Our attempt to charge your invoice has failed."));
+            }
+            
+            
+        } catch (\Stripe\Error\Card $e) {
+            // Since it's a decline, \Stripe\Error\Card will be caught
+            $payment_log->log = $e->getMessage();
+            $payment_log->save();
+            $body = $e->getJsonBody();
+            $err = $body['error'];
+
+            return response()->json([
+                        "success" => false,
+                        "status" => $e->getHttpStatus(),
+                        "type" => $err['type'],
+                        "code" => $err['code'],
+                        "message" => $err['message']]);
+        } catch (\Stripe\Error\InvalidRequest $e) {
+            // Invalid parameters were supplied to Stripe's API
+            $payment_log->log = $e->getMessage();
+            $payment_log->save();
+        } catch (\Stripe\Error\Authentication $e) {
+            // Authentication with Stripe's API failed
+            // (maybe you changed API keys recently)
+            $payment_log->log = $e->getMessage();
+            $payment_log->save();
+        } catch (\Stripe\Error\ApiConnection $e) {
+            // Network communication with Stripe failed
+            $payment_log->log = $e->getMessage();
+            $payment_log->save();
+        } catch (\Stripe\Error\Base $e) {
+            // Display a very generic error to the user, and maybe send
+            // yourself an email
+            $payment_log->log = $e->getMessage();
+            $payment_log->save();
+        } catch (Exception $e) {
+            // Something else happened, completely unrelated to Stripe
+            $payment_log->log = $e->getMessage();
+            $payment_log->save();
+        }
+        
     }
 
 }
--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Cashier\Billable as Billable;
+use App\StripeActiveSubscription;
 
 class User extends Authenticatable {
     
@@ -34,6 +35,45 @@ class User extends Authenticatable {
     
     public function stripeDetails() {
         return StripeDetail::where('email', $this->email)->get();
+    }
+    
+    public function deleteStripeSubscriptions() {
+        //Remove al active subscription
+        $saved_subscriptions = StripeActiveSubscription::where('stripe_id', Auth::user()->stripe_id)->get();
+        foreach ($saved_subscriptions as $sub) {
+            $sub->delete();
+        }
+    }
+    
+    public function updateStripeSubscriptions() {
+        $subscriptions_listings = \Stripe\Subscription::all(array('customer' => Auth::user()->stripe_id));
+        $subscriptions = $subscriptions_listings->data;
+
+        $this->deleteStripeSubscriptions();
+
+        $invoices = array();
+
+        foreach ($subscriptions as $subscription) {
+            //The Invoices under this subscription
+            $invoice_listings = \Stripe\Invoice::all(array("subscription" => $subscription->id));
+            $stripe_id = $subscription->customer;
+
+            $invoices[$subscription->id] = $invoice_listings->data[0];
+
+            $items = $subscription->items->data;
+            foreach ($items as $item) {
+                $plan = $item->plan;
+                $plan_id = $plan->id;
+                $active_subscription = new StripeActiveSubscription;
+                $active_subscription->stripe_id = $stripe_id;
+                $active_subscription->subscription_id = $plan_id;
+                $active_subscription->status = $subscription->status;
+                $active_subscription->start_date = \Carbon\Carbon::createFromTimestamp($subscription->current_period_start);
+                $active_subscription->end_date = \Carbon\Carbon::createFromTimestamp($subscription->current_period_end);
+                $active_subscription->stripe_subscription_id = $subscription->id;
+                $active_subscription->save();
+            }
+        }
     }
 
 }
