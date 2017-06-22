5bae9996c03f9def10fed4e867fa07958ff675c7
Lloyd Yeo <lloyd@kooki.co>
2017-06-22 17:05:42 +0800

    Lazy Commit!

--- resources/views/settings/js.blade.php
+++ resources/views/settings/js.blade.php
@@ -76,4 +76,9 @@ $(".btn-pay-invoice").on("click", function(){
     });
     
 });
+
+
+@if ($update_credit_card_response !== NULL)
+    swal('Credit Card Changed!', {{ $update_credit_card_response }}, 'success');
+@endif
 </script>
