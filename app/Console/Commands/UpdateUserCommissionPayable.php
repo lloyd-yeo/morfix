5113fec10c4748f8303e6e71716681ed073cbeaa
Lloyd Yeo <lloyd@kooki.co>
2017-06-26 17:09:52 +0800

    Lazy Commit!

--- app/Console/Commands/UpdateUserCommissionPayable.php
+++ app/Console/Commands/UpdateUserCommissionPayable.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\User;
 
 class UpdateUserCommissionPayable extends Command
 {
@@ -11,14 +12,14 @@ class UpdateUserCommissionPayable extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'update:payablecsv';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Update a user\'s payable commission from a csv.';
 
     /**
      * Create a new command instance.
@@ -37,6 +38,19 @@ class UpdateUserCommissionPayable extends Command
      */
     public function handle()
     {
-        //
+        $path = storage_path('app/may-payout-final.csv');
+        $file = fopen($path, "r");
+        while (($data = fgetcsv($file, 200, ",")) !== FALSE) {
+            if ($data[4] == "No") {
+                $pending_comms = $data[2];
+                $user = User::where($data[0])->first();
+                if ($user !== NULL) {
+                    $user->pending_commission_payable = $pending_comms;
+                    if ($user->save()) {
+                        echo "Updated [" . $user->email . "] pending comms to " . $pending_comms . "\n";
+                    }
+                }
+            }
+        }
     }
 }
