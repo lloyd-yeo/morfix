c0bdbd586241715fc8a3453fdcbc35820dea43d0
Lloyd Yeo <lloyd@kooki.co>
2017-06-27 15:34:30 +0800

    Lazy Commit!

--- app/Jobs/SendDm.php
+++ app/Jobs/SendDm.php
@@ -7,19 +7,45 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\DB;
+use InstagramAPI\Instagram as Instagram;
+use InstagramAPI\SettingsAdapter as SettingsAdapter;
+use InstagramAPI\InstagramException as InstagramException;
+use App\InstagramProfile;
+use App\CreateInstagramProfileLog;
+use App\Proxy;
+use App\DmJob;
 
 class SendDm implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable,
+        InteractsWithQueue,
+        Queueable,
+        SerializesModels;
+
+    protected $profile;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 1;
+
+    /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
+    public $timeout = 120;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
-    {
-        //
+    public function __construct(\App\InstagramProfile $profile) {
+        $this->profile = $profile;
     }
 
     /**
@@ -29,6 +55,8 @@ class SendDm implements ShouldQueue
      */
     public function handle()
     {
-        //
+        DB::reconnect();
+        
+        
     }
 }
