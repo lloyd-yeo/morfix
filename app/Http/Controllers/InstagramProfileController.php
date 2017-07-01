ff92b5948ccfd02b0a42ff0b30f3f1e23062b54e
Lloyd Yeo <lloyd@kooki.co>
2017-07-02 00:21:14 +0800

    Lazy Commit!

--- app/Http/Controllers/InstagramProfileController.php
+++ app/Http/Controllers/InstagramProfileController.php
@@ -284,7 +284,7 @@ class InstagramProfileController extends Controller {
             $ig_profile->checkpoint_required = 0;
             $ig_profile->save();
             return Response::json(array("success" => true, 'response' => 'Your profile has restored connectivity.'));
-        } catch (InstagramAPI\Exception\RequestException $ig_ex) {
+        } catch (\InstagramAPI\Exception\InstagramException $ig_ex) {
             return Response::json(array("success" => false, 'response' => 'Unable to connect to your profile, please retry. ' . $ig_ex->getMessage()));
         }
     }
