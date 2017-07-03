a0c93f3640700e133570a10819acd7db2aef47e6
Lloyd Yeo <lloyd@kooki.co>
2017-07-04 07:36:19 +0800

    Lazy Commit!

--- resources/views/faq/topic.blade.php
+++ resources/views/faq/topic.blade.php
@@ -22,7 +22,7 @@
             <div class="col-sm-12">
                 <a class="block block-link-hover2 text-center" href="javascript:void(0)">
                     <div class="block-content block-content-full border-b" style="overflow-y:auto;">
-                        <span class="pull-left"><i class="si si-direction fa-5x text-info"></i></span>
+                        <span class="pull-left"><i class="si si-direction fa-500px text-info"></i></span>
                     </div>
                 </a>
             </div>
