09d6ae47c3d66ced1464a640052c6f4eaad338fb
Lloyd Yeo <lloyd@kooki.co>
2017-07-04 01:43:53 +0800

    Lazy Commit!

--- resources/views/payment/table/mastermind.blade.php
+++ resources/views/payment/table/mastermind.blade.php
@@ -65,11 +65,12 @@
                     <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
                 </form>-->
                 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="X3G8EGMRC7FHS">
-<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribe_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.sandbox.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-</form>
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="EQZVT8JU3RKME">
+                <input type="image" src="https://www.sandbox.paypal.com/en_GB/i/btn/btn_subscribe_SM.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.sandbox.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+                </form>
+
 
 
 
