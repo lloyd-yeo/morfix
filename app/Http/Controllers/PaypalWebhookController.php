ca9999232529d519206a49ee2326ead4346a0676
Lloyd Yeo <lloyd@kooki.co>
2017-07-04 00:11:51 +0800

    Lazy Commit!

--- app/Http/Controllers/PaypalWebhookController.php
+++ app/Http/Controllers/PaypalWebhookController.php
@@ -10,8 +10,13 @@ use App\PaypalWebhookLog;
 class PaypalWebhookController extends Controller
 {
     public function listen(Request $request) {
+        
         $log = new PaypalWebhookLog;
-        $log->message = $request;
+        $log->message = "NEW WEBHOOK HERE!";
+        $log->save();
+        
+        $log = new PaypalWebhookLog;
+        $log->message = serialize($request);
         $log->save();
     }
 }
