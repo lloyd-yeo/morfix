a65b51c44aba267a943b6721c3f49ab21f97d859
Lloyd Yeo <lloyd@kooki.co>
2017-07-11 23:55:50 +0800

    Lazy Commit!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\InstagramProfile;
 use App\UserUpdate;
+use App\StripeActiveSubscription;
 
 class HomeController extends Controller {
 
@@ -26,11 +27,42 @@ class HomeController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        
+
         $current_user = User::find(Auth::user()->user_id);
         
+        if ($current_user->vip == 0 && $current_user->admin == 0 && $current_user->stripe_id !== NULL) {
+            $tier = 1;
+            \Stripe\Stripe::setApiKey("sk_live_HeS5nnfJ5qARMPsANoGw32c2");
+            $stripe_id = $current_user->stripe_id;
+            $subscriptions = \Stripe\Subscription::all(array("customer" => $stripe_id));
+            foreach ($subscriptions->autoPagingIterator() as $subscription) {
+                echo $subscription . "\n\n\n\n";
+                $stripe_id = $subscription->customer;
+                $items = $subscription->items->data;
+                foreach ($items as $item) {
+                    $plan = $item->plan;
+                    $plan_id = $plan->id;
+
+                    if ($subscription->status == "active" || $subscription->status == "trialing") {
+                        if ($plan_id == "0137") {
+                            $tier = $tier + 1;
+                        } else if ($plan_id == "0297") {
+                            $tier = $tier + 10;
+                        } else if ($plan_id == "MX370") {
+                            $tier = $tier + 2;
+                        } else if ($plan_id == "MX970") {
+                            $tier = $tier + 20;
+                        } else if ($plan_id == "0197" || $plan_id == "0167") {
+                            $tier = $tier + 11;
+                        }
+                    }
+                }
+            }
+            $current_user->tier = $tier;
+            $current_user->save();
+        }
+
 //        if ($current_user->user_tier == 4 )
-        
 //        if ($current_user->user_tier == 2) {
 //            $current_user->tier = 2;
 //            $current_user->save();
@@ -44,15 +76,15 @@ class HomeController extends Controller {
 //            $current_user->save();
 //            $current_user = User::find(Auth::user()->user_id);
 //        }
-        
+
         DB::update("UPDATE user_insta_profile SET profile_pic_url = REPLACE(profile_pic_url, 'http://', 'https://');");
-        
+
         $leaderboard_alltime = DB::select("SELECT email, name, (SUM(pending_commission)+SUM(all_time_commission)) AS total_comms FROM user
                         GROUP BY email, name
                         ORDER BY total_comms DESC LIMIT 10;");
 
         $leaderboard_weekly = User::orderBy('pending_commission', 'desc')->take(10)->get();
-        
+
         $leaderboard_alltime_ranking = "UNRANKED";
 
         $ranking = 1;
@@ -64,13 +96,13 @@ class HomeController extends Controller {
         }
 
         $instagram_profiles = InstagramProfile::where('email', Auth::user()->email)->take($current_user->num_acct)->get();
-        
+
         $new_profile_follower_analysis = array();
         $new_profile_follower_analysis_label = array();
         $new_follower_count = array();
 
         foreach ($instagram_profiles as $ig_profile) {
-            
+
             $follower_analysis = DB::select("SELECT follower_count, date FROM insta_affiliate.user_insta_follower_analysis WHERE insta_username = ? ORDER BY date DESC LIMIT 10;", [$ig_profile->insta_username]);
             $analysis_csv = "";
             $analysis_date_csv = "";
@@ -106,9 +138,9 @@ class HomeController extends Controller {
             $new_profile_follower_analysis_label[$ig_profile->insta_username] = $analysis_date_csv;
             $new_follower_count[$ig_profile->insta_username] = $new_follower_diff;
         }
-        
+
         $user_updates = UserUpdate::where('email', Auth::user()->email)->orderBy('id', 'desc')->take(5)->get();
-        
+
 //        $user_updates = DB::select("SELECT `user_updates`.`id`,
 //                            `user_updates`.`email`,
 //                            `user_updates`.`title`,
@@ -116,13 +148,13 @@ class HomeController extends Controller {
 //                            `user_updates`.`type`,
 //                            `user_updates`.`created_at`
 //                        FROM `insta_affiliate`.`user_updates` WHERE `user_updates`.`email` = ? ORDER BY id DESC;", [Auth::user()->email]);
-        
+
         $remaining_quota = DB::select("SELECT COUNT(email) AS email_count FROM user_insta_profile WHERE email = \"" . Auth::user()->email . "\";");
-        
+
         $user = User::where('email', Auth::user()->email)->first();
-        
+
         $remaining_quota = $user->num_acct - $remaining_quota[0]->email_count;
-        
+
         return view('home', [
             'leaderboard_alltime' => $leaderboard_alltime,
             'leaderboard_weekly' => $leaderboard_weekly,
