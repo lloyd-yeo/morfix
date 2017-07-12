3ed7b9048147bc9a87754ff92f852839bb747a99
Lloyd Yeo <lloyd@kooki.co>
2017-07-13 05:38:19 +0800

    Lazy Commit!

--- resources/views/vsl/signup.blade.php
+++ resources/views/vsl/signup.blade.php
@@ -65,7 +65,7 @@
                             <img class='center-block' src="../assets/img/promo/vsl-instaffiliates-logo.png" style='width:180px; height:180px;'>
                         </a>
                     </p>
-                    <h1 class='center-block text-center font-w700 push text-white push-30-t' style='font-size: 62px; line-height: 0.9; max-width:910px;'>
+                    <h1 class='center-block text-center font-w700 push text-white push-30-t' style='text-shadow: 2px 4px 3px rgba(0,0,0,0.3); font-size: 62px; line-height: 0.9; max-width:910px;'>
                         DISCOVER HOW TO CREATE<br/>HUGE AUTOMATED INCOME ON<br/>SOCIAL MEDIA EVEN IF YOU HAVE NO EXPERIENCE
                     </h1>
                 </div>
