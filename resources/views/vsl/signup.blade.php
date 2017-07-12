02eb5d2a89a38e534164fd89f836f1e4da58e6d6
Lloyd Yeo <lloyd@kooki.co>
2017-07-13 05:46:57 +0800

    Lazy Commit!

--- resources/views/vsl/signup.blade.php
+++ resources/views/vsl/signup.blade.php
@@ -80,16 +80,16 @@
     </body>
 
     <!-- OneUI Core JS: jQuery, Bootstrap, slimScroll, scrollLock, Appear, CountTo, Placeholder, Cookie and App.js -->
-    <script src="assets/js/core/jquery.min.js"></script>
-    <script src="assets/js/core/bootstrap.min.js"></script>
-    <script src="assets/js/core/jquery.slimscroll.min.js"></script>
-    <script src="assets/js/core/jquery.scrollLock.min.js"></script>
-    <script src="assets/js/core/jquery.appear.min.js"></script>
-    <script src="assets/js/core/jquery.countTo.min.js"></script>
-    <script src="assets/js/core/jquery.placeholder.min.js"></script>
-    <script src="assets/js/core/js.cookie.min.js"></script>
-    <script src="assets/js/app.js"></script>
-    <script src="assets/js/plugins/jquery-vide/jquery.vide.min.js"></script>
+    <script src="../assets/js/core/jquery.min.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
+    <script src="../assets/js/core/jquery.slimscroll.min.js"></script>
+    <script src="../assets/js/core/jquery.scrollLock.min.js"></script>
+    <script src="../assets/js/core/jquery.appear.min.js"></script>
+    <script src="../assets/js/core/jquery.countTo.min.js"></script>
+    <script src="../assets/js/core/jquery.placeholder.min.js"></script>
+    <script src="../assets/js/core/js.cookie.min.js"></script>
+    <script src="../assets/js/app.js"></script>
+    <script src="../assets/js/plugins/jquery-vide/jquery.vide.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/1.1.4/typed.min.js"></script>
     <!-- Page JS Code -->
     <script>
