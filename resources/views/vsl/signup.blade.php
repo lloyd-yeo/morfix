16cf93a2eab06589afd29e57e314a052b16da000
Lloyd Yeo <lloyd@kooki.co>
2017-07-13 06:15:27 +0800

    Lazy Commit!

--- resources/views/vsl/signup.blade.php
+++ resources/views/vsl/signup.blade.php
@@ -154,7 +154,7 @@
             </div>
             <div class='col-lg-12 push-50-t' style="padding-left: 0px; padding-right: 0px;">
                 <div class='center-block' style='background-color:black;'>
-                    <p class="text-center font-s48 text-white push-50-t push-50" style='font-weight:900;'>
+                    <p class="text-center font-s48 text-white" style='font-weight:900; padding-top: 80px; padding-bottom: 80px;'>
                         Spy on what our users think!
                     </p>
                 </div>
