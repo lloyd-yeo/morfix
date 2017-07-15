a69c7a95ea317ad11ce2243de47381082e4780b8
Lloyd Yeo <lloyd@kooki.co>
2017-07-16 00:38:05 +0800

    Lazy Commit!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller {
 
         $current_user = Auth::user();
         
-        if ($current_user -> paypal == 1) {
+        if ($current_user->paypal == 1) {
         } else if ($current_user->email == "maychengmt@yahoo.com") {
             $current_user->tier = 12;
             $current_user->save();
