3a277be621f96389c52e1bf40fa9b960ea1c4f12
Lloyd Yeo <lloyd@kooki.co>
2017-07-16 18:27:46 +0800

    Lazy Commit!

--- public/assets/js/pages/base_tables_datatables.js
+++ public/assets/js/pages/base_tables_datatables.js
@@ -28,13 +28,6 @@ var BaseTableDatatables = function() {
         jQuery('.js-dataTable-schedule').dataTable({
             columnDefs: [ { orderable: false, targets: [ 4 ] } ],
             pageLength: 10,
-            columns: [
-                    { width: "10%" },
-                    { width: "25%" },
-                    { width: "20%" },
-                    { width: "30%" },
-                    { width: "10%" }
-                  ],
             order: [[ 0, "desc" ]],
             lengthMenu: [[5, 10, 15, 20], [5, 10, 15, 20]]
         });
