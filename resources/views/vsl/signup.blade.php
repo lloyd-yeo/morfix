1e5ffeaf5555bac75472914772cfb56640b3bdbb
Lloyd Yeo <lloyd@kooki.co>
2017-07-18 23:28:29 +0800

    Lazy Commit!

--- resources/views/vsl/signup.blade.php
+++ resources/views/vsl/signup.blade.php
@@ -548,6 +548,7 @@
                             }
                         });
                     } else { //pay via Paypal
+                        $form.submit();
                     }
                 });
 
