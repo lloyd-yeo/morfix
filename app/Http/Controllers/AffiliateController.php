7c19e9fd1df8f23a0a23d5cdf57870e49872b244
Lloyd Yeo <lloyd@kooki.co>
2017-07-18 23:13:10 +0800

    Lazy Commit!

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -267,7 +267,7 @@ class AffiliateController extends Controller {
         return $username;
     }
     
-    function oldClean($string) {
+    public function oldClean($string) {
         $string = str_replace(' ', '#', $string); // Replaces all spaces with hyphens.
         return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
     }
