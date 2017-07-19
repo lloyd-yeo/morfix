396da24ff119dfe42a77f874f2865516e44aea67
Lloyd Yeo <lloyd@kooki.co>
2017-07-19 20:37:07 +0800

    Lazy Commit!

--- app/Console/Commands/InteractionLike.php
+++ app/Console/Commands/InteractionLike.php
@@ -66,6 +66,10 @@ class InteractionLike extends Command {
 
         foreach ($users as $user) {
             
+            if ($user->email != "theoilybabes@yahoo.com") {
+                continue;
+            }
+            
             if (NULL === $this->argument("email")) {
                 #dispatch(new \App\Jobs\InteractionLike(\App\User::find($user->user_id)))->onQueue('like');
                 
