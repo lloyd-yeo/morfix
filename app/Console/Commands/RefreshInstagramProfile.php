9f31c2d49cb63a3f9a68d311c03615e1e8dbe258
Lloyd Yeo <lloyd@kooki.co>
2017-07-23 00:55:54 +0800

    Lazy Commit!

--- app/Console/Commands/RefreshInstagramProfile.php
+++ app/Console/Commands/RefreshInstagramProfile.php
@@ -101,7 +101,7 @@ class RefreshInstagramProfile extends Command {
                     $instagram_user = $user_response->user;
                     var_dump($instagram_user);
                     
-                    DB::update("UPDATE user_insta_profile SET name = ?, updated_at = NOW(), follower_count = ?, num_posts = ?, insta_user_id = ?, profile_pic_url = ? WHERE insta_username = ?;", 
+                    DB::update("UPDATE user_insta_profile SET profile_full_name = ?, updated_at = NOW(), follower_count = ?, num_posts = ?, insta_user_id = ?, profile_pic_url = ? WHERE insta_username = ?;", 
                                     [$instagram_user->full_name, $instagram_user->follower_count, $instagram_user->media_count, $instagram_user->pk, $instagram_user->profile_pic_url, $ig_username]);
                     
                     $items = $instagram->timeline->getSelfUserFeed()->items;
