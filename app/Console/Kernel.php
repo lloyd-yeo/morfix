f420670f7e660a9b1f6ba89dd925065b4eb452ea
Lloyd Yeo <lloyd@kooki.co>
2017-07-30 19:50:51 +0800

    Lazy Commit!

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends ConsoleKernel {
      */
     protected function schedule(Schedule $schedule) {
         $schedule->command('interaction:like')->everyFiveMinutes();
-        #$schedule->command('interaction:comment')->everyFiveMinutes();
+        $schedule->command('interaction:comment')->everyFiveMinutes();
         $schedule->command('interaction:follow')->everyMinute();
         #$schedule->command('dm:get')->everyThirtyMinutes();
         #$schedule->command('dm:send')->everyTenMinutes();
