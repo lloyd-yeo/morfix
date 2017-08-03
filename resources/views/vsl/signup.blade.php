a17c2d67360ceae1264a90a6e012d4aa548438e0
Lloyd Yeo <lloyd@kooki.co>
2017-08-03 14:49:57 +0800

    Lazy Commit!

--- resources/views/vsl/signup.blade.php
+++ resources/views/vsl/signup.blade.php
@@ -163,7 +163,14 @@
         </style>
         <!-- Scripts -->
         <script>
-        </script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-88362519-1', 'auto');
+            ga('send', 'pageview');
+          </script>
     </head>
     <body style='margin:0px;'>
         <main id="main-container" style='min-height: 100%; background-image: url("../assets/img/promo/vsl-bg.jpg"); background-color: rgba(0,0,0,1); background-size:cover;'>
