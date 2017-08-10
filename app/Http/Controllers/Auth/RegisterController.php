8943449a5563cb595e96fdaa56b37298bdf85c89
Lloyd Yeo <lloyd@kooki.co>
2017-08-11 02:04:11 +0800

    Lazy Commit!

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ use RegistersUsers;
     protected function validator(array $data) {
         return Validator::make($data, [
                     'name' => 'required|max:255',
-                    'email' => 'required|email|max:255|unique:users',
+                    'email' => 'required|email|max:255|unique:user',
                     'password' => 'required|min:6|confirmed',
         ]);
     }
