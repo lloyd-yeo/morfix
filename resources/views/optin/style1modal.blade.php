439020ee9d4ec3bb7896eb3def4929d66bec5a48
Lloyd Yeo <lloyd@kooki.co>
2017-08-15 13:27:26 +0800

    Lazy Commit!

--- resources/views/optin/style1modal.blade.php
+++ resources/views/optin/style1modal.blade.php
@@ -14,7 +14,10 @@
                         <div class="wpb_wrapper">
                             <div class='block'>
                                 <div class='block-header'>
-                                    <h3 class="block-title" style='font-size: 24px;'>Your account details:</h3>
+                                    <h3 class="block-title" style='font-size: 24px;'>
+                                        HOW I MADE $678.25 PER DAY<br/>
+                                        FROM THIS INSTAGRAM<br/>LOOPHOLE INCOME SYSTEM
+                                    </h3>
                                 </div>
                                 <div class="block-content block-content-full">
                                     <form id="payment-form" class="form-horizontal push-10-t" onsubmit="event.preventDefault();" method="post" action="{{ url('vsl/signup/cc') }}">
@@ -27,79 +30,12 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <div class="form-material form-material-primary ">
-                                                    <input class="form-control" type="text" 
-                                                           id="signup-name"  name="name">
-                                                    <label for="signup-name">Name:</label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <div class="form-material form-material-primary ">
-                                                    <input class="form-control" type="password" 
-                                                           id="signup-pw"  name="pw">
-                                                    <label for="signup-pw">Password:</label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <div class="form-material form-material-primary ">
-                                                    <input class="form-control" type="password" 
-                                                           id="signup-pw2"  name="pw2">
-                                                    <label for="signup-pw2">Confirm Password:</label>
-                                                </div>
-                                            </div>
-                                        </div>
                                         
-                                        <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <div class="form-material form-material-primary ">
-                                                    <select id="payment-method-dropdown" name="payment_method_dropdown" class="payment-method-dropdown form-control">
-                                                        <option value="1">Pay by Debit/Credit Card</option>
-                                                        <option value="2">Pay by Paypal</option>
-                                                    </select>
-                                                    <label for="payment-method-dropdown">Select Payment Method</label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        
-                                        <div id="stripe-card-group" class="form-group">
-                                            <div class="col-sm-12">
-                                                <label for="card-element">
-                                                    Credit or Debit Card Number
-                                                </label>
-                                                <div id="card-element">
-                                                    <!-- a Stripe Element will be inserted here. -->
-                                                </div>
-
-                                                <!-- Used to display form errors -->
-                                                <div id="card-errors"></div>
-                                            </div>
-                                        </div>
-                                        <div id="stripe-logo" class="form-group" style="margin-bottom:0px;">
-                                            <div class='col-sm-12'>
-                                                <div class='pull-right' style="margin-top: 5px;"><img style='height:30px;' src="../assets/img/logo/powered-by-stripe-dark.png" /></div>
-                                            </div>
-                                        </div>
                                         <div id="stripe-card-btn" class='form-group'>
                                             <div class='col-sm-12'>
                                                 <div class="form-material form-material-primary">
                                                     <button class="pull-right btn btn-sm btn-primary" type="submit">Make Payment</button>
                                                 </div>
-                                            </div>
-                                        </div>
-                                        
-                                        <div id="paypal-group" class="form-group" style="display:none;">
-                                            <div class="col-sm-12">
-                                                <!-- PayPal Logo -->
-                                                <a id="paypal-btn" href="{{ url('vsl/signup/paypal') }}">
-                                                    <img src="https://www.paypalobjects.com/webstatic/en_AU/i/buttons/btn_paywith_primary_s.png" alt="Pay with PayPal" />
-                                                </a>
-                                                <!-- PayPal Logo -->
                                             </div>
                                         </div>
                                         
