0a98f98242ecc79cfac013b8de8ce630bf28b841
Lloyd Yeo <lloyd@kooki.co>
2017-08-15 13:30:23 +0800

    Lazy Commit!

--- resources/views/optin/style1.blade.php
+++ resources/views/optin/style1.blade.php
@@ -206,6 +206,14 @@
                 </center>
             </div>
             
+            <div class='col-lg-10 col-lg-offset-1 push-50-t'>
+                <div class='center-block' style='max-width:520px;'>
+                    <button class="signup-btn start-stop-btn btn btn-block btn-lg" style="width: 100%; margin-left: auto; margin-right: auto; margin-top: 20px;">
+                        <span id="signup-btn-text">GET INSTANT ACCESS</span>
+                    </button>
+                </div>
+            </div>
+            
             <div class='col-lg-12 push-50-t' style="padding-left: 0px; padding-right: 0px;">
             </div>
             
