d05183b49ebaf6f8b95a3f16048f460842772f98
Lloyd Yeo <lloyd@kooki.co>
2017-08-15 12:56:45 +0800

    Lazy Commit!

--- resources/views/optin/style1.blade.php
+++ resources/views/optin/style1.blade.php
@@ -180,7 +180,9 @@
         </script>
     </head>
     <body style='margin:0px;'>
-        <main id="main-container" style='min-height: 100%; background-image: url("../assets/img/promo/vsl-bg.jpg"); background-color: rgba(0,0,0,1); background-size:cover;'>
+        <main id="main-container" style='min-height: 100%; 
+              background-image: url("../assets/img/promo/vsl-bg.jpg"); 
+              background-color: rgba(0,0,0,0.4); background-size:cover;'>
             <div class='col-lg-12 push-50-t'>
                 <div class='col-lg-10 col-lg-offset-1'>
                     <p>
