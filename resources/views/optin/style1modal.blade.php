b9d4e7be5c787a80bbe0eb13f41ae878882800a5
Lloyd Yeo <lloyd@kooki.co>
2017-08-15 14:04:18 +0800

    Lazy Commit!

--- resources/views/optin/style1modal.blade.php
+++ resources/views/optin/style1modal.blade.php
@@ -11,7 +11,9 @@
                      style="margin-left: auto; margin-right: auto; padding-left: 0px; padding-right: 0px;">
                     <div class="dfd_col-tablet-6 columns three dfd_col-tabletop-6 dfd_col-laptop-6 dfd_col-mobile-12">
                         <div class='block' style="margin-bottom: 0px;">
-                            <img src="../assets/img/promo/style1optin-popup.png" >
+                            <center>
+                                <img src="../assets/img/promo/style1optin-popup.png" >
+                            </center>
                         </div>
                     </div>
                     <div id="payment-detail-div" class="dfd_col-tablet-6 columns three dfd_col-tabletop-6 dfd_col-laptop-6 dfd_col-mobile-12">
