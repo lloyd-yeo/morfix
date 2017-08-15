e910ea1420884a48ea0a11bf73031df864d4770b
Lloyd Yeo <lloyd@kooki.co>
2017-08-15 12:57:56 +0800

    Lazy Commit!

--- resources/views/optin/style1.blade.php
+++ resources/views/optin/style1.blade.php
@@ -203,6 +203,17 @@
                             webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                 </div>
             </div>
+            
+            <div class='col-lg-10 col-lg-offset-1 push-50-t'>
+                <div class='center-block' style='max-width:910px;'>
+                    Enter your email here
+                    <input type="email">
+                    <button class="signup-btn start-stop-btn btn btn-block btn-lg" style="width: 100%; margin-left: auto; margin-right: auto; margin-top: 20px;">
+                        <span id="signup-btn-text">GET INSTANT ACCESS</span>
+                    </button>
+                </div>
+            </div>
+            
         </main>
     </body>
 
