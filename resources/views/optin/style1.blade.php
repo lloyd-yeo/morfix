619db4f0c955e9f359e90157776de5dcab3b65c7
Lloyd Yeo <lloyd@kooki.co>
2017-08-15 13:13:42 +0800

    Lazy Commit!

--- resources/views/optin/style1.blade.php
+++ resources/views/optin/style1.blade.php
@@ -206,14 +206,14 @@
                 </center>
             </div>
 
-            <div class='col-lg-10 col-lg-offset-1 push-30-t'>
+<!--            <div class='col-lg-10 col-lg-offset-1 push-30-t'>
                 <div class='center-block' style='max-width:520px;'>
                     <input class="form-control" type="email" placeholder="EMAIL">
                     <button class="signup-btn start-stop-btn btn btn-block btn-lg" style="width: 100%; margin-left: auto; margin-right: auto; margin-top: 20px;">
                         <span id="signup-btn-text">GET INSTANT ACCESS</span>
                     </button>
                 </div>
-            </div>
+            </div>-->
 
             <div class='col-lg-12 push-50-t' style="padding-left: 0px; padding-right: 0px;">
 
