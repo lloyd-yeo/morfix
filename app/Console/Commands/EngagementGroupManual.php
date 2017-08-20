7a8be3ca8a125a41fa3710c2a8425799b9d4403b
Lloyd Yeo <lloyd@kooki.co>
2017-08-21 01:11:26 +0800

    Lazy Commit!

---
+++ app/Console/Commands/EngagementGroupManual.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+use App\InstagramProfile;
+
+class EngagementGroupManual extends Command {
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'engagementgroup:like {media_id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run engagement group on a media_id.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        $ig_profiles = InstagramProfile::where('checkpoint_required', 0)
+                ->where('account_disabled', 0)
+                ->where('invalid_user', 0)
+                ->where('incorrect_pw', 0)
+                ->where('invalid_proxy', 0)
+                ->get();
+        
+        $mediaId = $this->argument('media_id');
+        
+        foreach ($ig_profiles as $ig_profile) {
+
+            $ig_username = $ig_profile->insta_username;
+            $ig_password = $ig_profile->insta_pw;
+
+            $config = array();
+            $config["storage"] = "mysql";
+            $config["pdo"] = DB::connection('mysql_igsession')->getPdo();
+            $config["dbtablename"] = "instagram_sessions";
+
+            $debug = false;
+            $truncatedDebug = false;
+            $instagram = new \InstagramAPI\Instagram($debug, $truncatedDebug, $config);
+
+            if ($ig_profile->proxy === NULL) {
+                $proxy = Proxy::inRandomOrder()->first();
+                $ig_profile->proxy = $proxy->proxy;
+                $ig_profile->save();
+                $proxy->assigned = $proxy->assigned + 1;
+                $proxy->save();
+            }
+
+            $instagram->setProxy($ig_profile->proxy);
+            $instagram->setUser($ig_username, $ig_password);
+            $explorer_response = $instagram->login();
+            
+            $response = $instagram->media->like($mediaId);
+            
+            var_dump ($response);
+            
+        }
+    }
+
+}
