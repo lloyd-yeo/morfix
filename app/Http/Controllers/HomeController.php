d50be84c53f5140dae5b948a08608e243202d1dc
Lloyd Yeo <lloyd@kooki.co>
2017-08-21 11:45:02 +0800

    Lazy Commit!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Cookie;
 use App\User;
 use App\InstagramProfile;
 use App\UserUpdate;
