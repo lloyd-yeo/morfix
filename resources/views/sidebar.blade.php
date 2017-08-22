292d4a1a9686ca22f0fda58e6afeb05e08846e56
rong <rong@192.168.2.97>
2017-08-22 15:55:57 +0800

    added the username in red text

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -172,18 +172,6 @@
                         @endif
                     </li>
 
-                    <li>
-                        @if ($page == 'admin')
-                        <a class="active" href="/training/affiliate"><i class="si si-layers"></i><span class="sidebar-mini-hide">Affiliate Training</span></a>
-                        @else
-                        @if (Auth::user()->tier == 10)
-                        <a href="/training/affiliate"><i class="si si-layers"></i><span class="sidebar-mini-hide">Affiliate Training</span></a>
-                        @else
-                        <a href="#" data-toggle="modal" data-target="#upgrade-training-affiliate-modal"><i class="si si-lock"></i><span class="sidebar-mini-hide">Affiliate Training</span></a>
-                        @endif
-                        @endif
-                    </li>
-
                 </ul>
             </div>
             <!-- END Side Content -->
