c064bdb370b761bded0d13a2d1632b400ea269b9
Lloyd Yeo <lloyd@kooki.co>
2017-08-24 15:30:37 +0800

    Lazy Commit!

--- resources/views/affiliate/dashboard.blade.php
+++ resources/views/affiliate/dashboard.blade.php
@@ -214,9 +214,9 @@
                                     @endif
                                     <td>{{ $referral->created_at }}</td>
                                     @if ($referral->paypal == 1)
-                                    <td><img style="height: 30px;" src="{{ asset('assets/img/logo/credit-card.png') }}" /></td>
+                                    <td><img style="height: 20px;" src="{{ asset('assets/img/logo/credit-card.png') }}" /></td>
                                     @else
-                                    <td><img style="height: 30px;" src="{{ asset('assets/img/logo/paypal-logo.png') }}" /></td>
+                                    <td><img style="height: 20px;" src="{{ asset('assets/img/logo/paypal-logo.png') }}" /></td>
                                     @endif
                                 </tr>
                             @endif
