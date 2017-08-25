abbe14430605bb1a6330c6a439e69f18b8820205
Lloyd Yeo <lloyd@kooki.co>
2017-08-25 20:37:34 +0800

    Lazy Commit!

--- app/Console/Commands/ReadCommissionList.php
+++ app/Console/Commands/ReadCommissionList.php
@@ -5,8 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\User;
 
-class ReadCommissionList extends Command
-{
+class ReadCommissionList extends Command {
+
     /**
      * The name and signature of the console command.
      *
@@ -26,8 +26,7 @@ class ReadCommissionList extends Command
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
@@ -36,8 +35,7 @@ class ReadCommissionList extends Command
      *
      * @return mixed
      */
-    public function handle()
-    {
+    public function handle() {
         $path = storage_path('app/august-commission.csv');
         $file = fopen($path, "r");
         $all_data = array();
@@ -47,20 +45,29 @@ class ReadCommissionList extends Command
         $referrer_eligiblity = array();
         $referrer_paypal_email = array();
         while (($data = fgetcsv($file, 200, ",")) !== FALSE) {
-            
+
             if ($row_count == 0) {
                 $row_count++;
                 continue;
             }
-            
+
             $referrer_email = $data[0];
-            
+
             if ($referrer_email == "gabriel@instaffiliates.com") {
                 continue;
             }
-            
+
             if (key_exists($referrer_email, $referrers)) {
                 $current_comms = $referrers[$referrer_email];
+
+                if ($data[2] == "137") {
+                    $current_comms += 20;
+                } else if ($data[2] == "297") {
+                    $current_comms += 50;
+                }
+                
+                $referrers[$referrer_email] = $current_comms;
+                
             } else {
                 $user = User::where('email', $referrer_email)->first();
                 if ($user !== NULL) {
@@ -68,12 +75,21 @@ class ReadCommissionList extends Command
                     $referrers[$referrer_email] = $current_comms;
                     $referrer_eligiblity[$referrer_email] = $data[7];
                     $referrer_paypal_email[$referrer_email] = $user->paypal_email;
+
+                    if ($data[2] == "137") {
+                        $current_comms = 20;
+                    } else if ($data[2] == "297") {
+                        $current_comms = 50;
+                    }
+
+                    $referrers[$referrer_email] = $current_comms;
                 }
             }
         }
-        
+
         foreach ($referrers as $referrer_email => $comms) {
             echo $referrer_email . "," . $comms . "," . $referrer_eligiblity[$referrer_email] . "\n";
         }
     }
+
 }
