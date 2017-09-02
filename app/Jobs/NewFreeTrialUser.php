f29e4d99a04229539275193625eda53e5183bd67
Lloyd Yeo <lloyd@kooki.co>
2017-09-02 21:21:00 +0800

    Lazy Commit!

--- app/Jobs/NewFreeTrialUser.php
+++ app/Jobs/NewFreeTrialUser.php
@@ -12,8 +12,8 @@ use AWeberAPI;
 use App\User;
 use App\Mail\NewPassword;
 
-class NewFreeTrialUser implements ShouldQueue
-{
+class NewFreeTrialUser implements ShouldQueue {
+
     use Dispatchable,
         InteractsWithQueue,
         Queueable,
@@ -32,18 +32,16 @@ class NewFreeTrialUser implements ShouldQueue
      * @var int
      */
     public $timeout = 60;
-    
     protected $email;
     protected $name;
     protected $ip;
-    
+
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($email, $name, $ip)
-    {
+    public function __construct($email, $name, $ip) {
         $this->email = $email;
         $this->name = $name;
         $this->ip = $ip;
@@ -54,59 +52,62 @@ class NewFreeTrialUser implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
-    {
-        $user = new User;
-        $user->email = $this->email;
-        $user->name = $this->name;
-        $user->trial_activation = 1;
-        $user->trial_end_date = \Carbon\Carbon::now()->addWeek();
-        $user->password = str_random(8);
-        $user->num_acct = 1;
-        $user->active = 1;
-        $user->verification_token = str_random(20);
-        $user->user_tier = 1;
-        $user->tier = 1;
-        
-        if ($user->save()) {
+    public function handle() {
+        if (User::where('email', $this->email)->count() === 0) {
             
-            echo $user;
-            
-            Mail::to($user->email)->send(new NewPassword($user, "free_trial"));
-            
-            $consumerKey = "AkAxBcK3kI1q0yEfgw4R4c77";
-            $consumerSecret = "DEchWOGoptnjNSqtwPz3fgZg6wkMpOTWTYCJcgBF";
+            $user = new User;
+            $user->email = $this->email;
+            $user->name = $this->name;
+            $user->trial_activation = 1;
+            $user->trial_end_date = \Carbon\Carbon::now()->addWeek();
+            $user->password = str_random(8);
+            $user->num_acct = 1;
+            $user->active = 1;
+            $user->verification_token = str_random(20);
+            $user->user_tier = 1;
+            $user->tier = 1;
 
-            $aweber = new AWeberAPI($consumerKey, $consumerSecret);
-            $account = $aweber->getAccount("AgI2J88WjcAhUkFlCn3OwzLx", "wdX1JHuuhIFm9AEiJt3SVUdM5S7Z8lAE7UKmP29P");
+            if ($user->save()) {
 
-            foreach ($account->lists as $offset => $list) {
-                $list_id = $list->id;
-                
-                if ($list_id != 4485376) {
-                    continue;
+                echo $user;
+
+                Mail::to($user->email)->send(new NewPassword($user, "free_trial"));
+
+                $consumerKey = "AkAxBcK3kI1q0yEfgw4R4c77";
+                $consumerSecret = "DEchWOGoptnjNSqtwPz3fgZg6wkMpOTWTYCJcgBF";
+
+                $aweber = new AWeberAPI($consumerKey, $consumerSecret);
+                $account = $aweber->getAccount("AgI2J88WjcAhUkFlCn3OwzLx", "wdX1JHuuhIFm9AEiJt3SVUdM5S7Z8lAE7UKmP29P");
+
+                foreach ($account->lists as $offset => $list) {
+                    $list_id = $list->id;
+
+                    if ($list_id != 4485376) {
+                        continue;
+                    }
+
+                    # create a subscriber
+                    $params = array(
+                        'email' => $this->email,
+                        'name' => $this->name,
+                        'ip_address' => $this->ip,
+                        'ad_tracking' => 'morfix_registration',
+                        'last_followup_message_number_sent' => 1,
+                        'misc_notes' => 'MorifX Registration Page'
+                    );
+
+                    try {
+                        $subscribers = $list->subscribers;
+                        $new_subscriber = $subscribers->create($params);
+                    } catch (\AWeberAPIException $ex) {
+                        echo $ex->getMessage();
+                        #return response('[' . $request->input('contact.email') . '] Free Trial Customer Updated & Registered Before in the List!', 200);
+                    }
                 }
-
-                # create a subscriber
-                $params = array(
-                    'email' => $this->email,
-                    'name' => $this->name,
-                    'ip_address' => $this->ip,
-                    'ad_tracking' => 'morfix_registration',
-                    'last_followup_message_number_sent' => 1,
-                    'misc_notes' => 'MorifX Registration Page'
-                );
-
-                try {
-                    $subscribers = $list->subscribers;
-                    $new_subscriber = $subscribers->create($params);
-                } catch (\AWeberAPIException $ex) {
-                    echo $ex->getMessage();
-                    #return response('[' . $request->input('contact.email') . '] Free Trial Customer Updated & Registered Before in the List!', 200);
-                }
+            } else {
+                echo "Unable to save user: " . $this->email . "\n\n";
             }
-        } else {
-            echo "Unable to save user: " . $this->email . "\n\n";
         }
     }
+
 }
