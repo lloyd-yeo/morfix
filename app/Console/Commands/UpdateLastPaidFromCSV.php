9079d9be1105c80d1017c41ec164bab3b859d0ca
Lloyd Yeo <lloyd@kooki.co>
2017-09-13 23:53:58 +0800

    Lazy Commit!

--- app/Console/Commands/UpdateLastPaidFromCSV.php
+++ app/Console/Commands/UpdateLastPaidFromCSV.php
@@ -49,15 +49,10 @@ class UpdateLastPaidFromCSV extends Command {
             $current_email = $data[0];
             $user = User::where('email', $current_email)->first();
             if ($user !== NULL) {
-  
-                if (($data[2] > 50) && ($data[1] != empty) && ($data[3] == 'Eligible') ) {
-
-                if ($data[2] > 0) {
+                if ($data[2] > 50 && $data[1] != empty && $data[3] == 'Eligible') {
                     $user->last_pay_out_date = $last_pay_out_coms_date;
-                    $user->save;
+                    #$user->save;
                     echo "Updated [$current_email] last pay out coms to [$last_pay_out_coms_date]\n";
-                } else {
-                    $user->last_pay_out_date = NULL;
                 }
             }
         }
