12969f61a2975d2d982c91120598f2367136309f
Lloyd Yeo <lloyd@kooki.co>
2017-09-15 00:40:04 +0800

    Lazy Commit!

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -27,7 +27,7 @@
             <div class="block-content block-content-full block-content-narrow">
                 <h2 class="content-heading">User Administration</h2>
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-lg-12">
                         <!-- Material Lock -->
                         <div class="block block-themed">
                             <div id="upgrade-tier-block" class="block-header bg-danger">
