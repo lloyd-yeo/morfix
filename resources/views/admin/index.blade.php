79f932a3e0d5fcda3174da0a571f078d64b15fcc
Lloyd Yeo <lloyd@kooki.co>
2017-09-15 00:32:21 +0800

    Lazy Commit!

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -21,31 +21,39 @@
         <!-- Modules -->
         <div class="block">
             <div class="block-content block-content-full block-content-narrow">
-                <h2 class="content-heading">Lock Forms</h2>
+                <h2 class="content-heading">User Administration</h2>
                 <div class="row">
                     <div class="col-lg-4">
-                        <!-- Bootstrap Lock -->
+                        <!-- Material Lock -->
                         <div class="block block-themed">
-                            <div class="block-header bg-danger">
-                                <ul class="block-options">
-                                    <li>
-                                        <button type="button" data-toggle="block-option" data-action="refresh_toggle" data-action-mode="demo"><i class="si si-refresh"></i></button>
-                                    </li>
-                                    <li>
-                                        <button type="button" data-toggle="block-option" data-action="content_toggle"></button>
-                                    </li>
-                                </ul>
-                                <h3 class="block-title">Bootstrap</h3>
+                            <div id="upgrade-tier-block" class="block-header bg-danger">
+                                <h3 class="block-title">Upgrade User Tier</h3>
                             </div>
                             <div class="block-content">
                                 <div class="text-center push-10-t push-30">
-                                    <img class="img-avatar img-avatar96" src="assets/img/avatars/avatar4.jpg" alt="">
+                                    <img class="img-avatar img-avatar96" src="assets/img/avatars/avatar10.jpg" alt="">
                                 </div>
-                                <form class="form-horizontal" action="base_forms_premade.html" method="post" onsubmit="return false;">
+                                <form class="form-horizontal push-10" action="base_forms_premade.html" method="post" onsubmit="return false;">
                                     <div class="form-group">
-                                        <label class="col-xs-12" for="lock1-password">Password</label>
                                         <div class="col-xs-12">
-                                            <input class="form-control" type="password" id="lock1-password" name="lock1-password" placeholder="Enter your password..">
+                                            <div class="form-material">
+                                                <input class="form-control" type="email" id="upgrade-tier-email" name="upgrade-tier-email" 
+                                                       placeholder="Enter user's email...">
+                                                <label for="upgrade-tier-email">Email</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-xs-12">
+                                            <div class="form-material">
+                                                <select class="form-control" id="upgrade-tier" name="upgrade-tier" size="1">
+                                                    <option value="2">Premium</option>
+                                                    <option value="12">Business + Premium</option>
+                                                    <option value="3">Pro</option>
+                                                    <option value="13">Business + Pro</option>
+                                                </select>
+                                                <label for="upgrade-tier">Tier?</label>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="form-group">
@@ -56,7 +64,36 @@
                                 </form>
                             </div>
                         </div>
-                        <!-- END Bootstrap Lock -->
+                        <!-- END Material Lock -->
+                    </div>
+                    <div class="col-lg-4">
+                        <!-- Material Lock -->
+                        <div class="block block-themed">
+                            <div class="block-header bg-danger">
+                                <h3 class="block-title">Material</h3>
+                            </div>
+                            <div class="block-content">
+                                <div class="text-center push-10-t push-30">
+                                    <img class="img-avatar img-avatar96" src="assets/img/avatars/avatar10.jpg" alt="">
+                                </div>
+                                <form class="form-horizontal push-10" action="base_forms_premade.html" method="post" onsubmit="return false;">
+                                    <div class="form-group">
+                                        <div class="col-xs-12">
+                                            <div class="form-material">
+                                                <input class="form-control" type="password" id="lock2-password" name="lock2-password" placeholder="Enter your password..">
+                                                <label for="lock2-password">Password</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-xs-12">
+                                            <button class="btn btn-sm btn-danger" type="submit"><i class="fa fa-unlock push-5-r"></i> Unlock</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- END Material Lock -->
                     </div>
                     <div class="col-lg-4">
                         <!-- Material Lock -->
@@ -94,43 +131,6 @@
                             </div>
                         </div>
                         <!-- END Material Lock -->
-                    </div>
-                    <div class="col-lg-4">
-                        <!-- Material (floating) Lock -->
-                        <div class="block block-themed">
-                            <div class="block-header bg-danger">
-                                <ul class="block-options">
-                                    <li>
-                                        <button type="button" data-toggle="block-option" data-action="refresh_toggle" data-action-mode="demo"><i class="si si-refresh"></i></button>
-                                    </li>
-                                    <li>
-                                        <button type="button" data-toggle="block-option" data-action="content_toggle"></button>
-                                    </li>
-                                </ul>
-                                <h3 class="block-title">Material (floating)</h3>
-                            </div>
-                            <div class="block-content">
-                                <div class="text-center push-10-t push-30">
-                                    <img class="img-avatar img-avatar96" src="assets/img/avatars/avatar1.jpg" alt="">
-                                </div>
-                                <form class="form-horizontal push-10" action="base_forms_premade.html" method="post" onsubmit="return false;">
-                                    <div class="form-group">
-                                        <div class="col-xs-12">
-                                            <div class="form-material floating">
-                                                <input class="form-control" type="password" id="lock3-password" name="lock3-password">
-                                                <label for="lock3-password">Password</label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="col-xs-12">
-                                            <button class="btn btn-sm btn-danger" type="submit"><i class="fa fa-unlock push-5-r"></i> Unlock</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <!-- END Material (floating) Lock -->
                     </div>
                 </div>
             </div>
