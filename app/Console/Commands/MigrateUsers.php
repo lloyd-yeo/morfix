7f53798d7f4a169a2159cc7645370c5a61aac848
Lloyd Yeo <lloyd@kooki.co>
2017-09-15 13:27:34 +0800

    Lazy Commit!

---
+++ app/Console/Commands/MigrateUsers.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use DB;
+use App\User;
+use App\InstagramProfile;
+
+
+class MigrateUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:user {partition}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate User, InstagramProfile to the target table.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $master_users = DB::connection('mysql_master')
+                ->table('user')
+                ->where('partition', $this->argument('partition'))
+                ->get();
+        foreach ($master_users as $master_user) {
+            $this->line($master_user);
+        }
+    }
+}
