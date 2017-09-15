580a4c99c566530624b92c5aa16efd7e39e08abc
Lloyd Yeo <lloyd@kooki.co>
2017-09-16 05:26:06 +0800

    Lazy Commit!

--- app/Console/Commands/UpdateUserTotalInteractionStatistics.php
+++ app/Console/Commands/UpdateUserTotalInteractionStatistics.php
@@ -9,9 +9,10 @@ use App\LikeLogsArchive;
 use App\InstagramProfileLikeLog;
 use App\InstagramProfileCommentLog;
 use App\InstagramProfileFollowLog;
+use Carbon\Carbon;
 
-class UpdateUserTotalInteractionStatistics extends Command
-{
+class UpdateUserTotalInteractionStatistics extends Command {
+
     /**
      * The name and signature of the console command.
      *
@@ -31,8 +32,7 @@ class UpdateUserTotalInteractionStatistics extends Command
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
@@ -41,8 +41,7 @@ class UpdateUserTotalInteractionStatistics extends Command
      *
      * @return mixed
      */
-    public function handle()
-    {
+    public function handle() {
         $partition = 0;
         if ($this->argument('partition') !== NULL) {
             $partition = $this->argument('partition');
@@ -50,12 +49,65 @@ class UpdateUserTotalInteractionStatistics extends Command
             //Global update;
             $ig_profiles = InstagramProfile::all();
             foreach ($ig_profiles as $ig_profile) {
-                if ($ig_profile->auto_like == 1 && $ig_profile->total_likes == 0) {
-                    $total_likes = InstagramProfileLikeLog::where('insta_username', $ig_profile->insta_username)->count();
-                    $ig_profile->total_likes = $total_likes;
-                    $ig_profile->save();
-                }
+                $this->initialUpdateOfStats($ig_profile);
             }
         }
     }
+
+    public function initialUpdateOfTotalStats($ig_profile) {
+            $total_likes = InstagramProfileLikeLog::where('insta_username', $ig_profile->insta_username)->count();
+            $ig_profile->total_likes = $total_likes;
+            $ig_profile->save();
+            
+            $total_likes_archived = LikeLogsArchive::where('insta_username', $ig_profile->insta_username)->count();
+            $ig_profile->total_likes = $ig_profile->total_likes + $total_likes_archived;
+            $ig_profile->save();
+            
+            $daily_likes = InstagramProfileLikeLog::where('insta_username', $ig_profile->insta_username)
+                    ->whereBetween('date_liked', Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+                            Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s'))
+                    ->count();
+            $ig_profile->daily_likes = $daily_likes;
+            $ig_profile->save();
+            
+            $total_comments = InstagramProfileCommentLog::where('insta_username', $ig_profile->insta_username)->count();
+            $ig_profile->total_comments = $total_comments;
+            $ig_profile->save();
+            
+            $daily_comments = InstagramProfileCommentLog::where('insta_username', $ig_profile->insta_username)
+                    ->whereBetween('date_commented', Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+                            Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s'))
+                    ->count();
+            $ig_profile->daily_comments = $daily_comments;
+            $ig_profile->save();
+            
+            $total_follows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)
+                    ->where('follow', 1)
+                    ->count();
+            
+            $ig_profile->total_follows = $total_follows;
+            $ig_profile->save();
+            
+            $daily_follows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)
+                    ->whereBetween('date_inserted', Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+                            Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s'))
+                    ->count();
+            $ig_profile->daily_follows = $daily_follows;
+            $ig_profile->save();
+            
+            $total_unfollows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)
+                    ->where('unfollowed', 1)
+                    ->count();
+            $ig_profile->total_unfollows = $total_unfollows;
+            $ig_profile->save();
+            
+            $daily_unfollows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)
+                    ->whereBetween('date_unfollowed', Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+                            Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s'))
+                    ->count();
+            $ig_profile->daily_unfollows = $daily_unfollows;
+            $ig_profile->save();
+            
+    }
+
 }
