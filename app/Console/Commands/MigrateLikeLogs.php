08f2f59b833bf14e94c404b0f294453f9dadb7e2
Lloyd Yeo <lloyd@kooki.co>
2017-09-19 05:48:27 +0800

    Lazy Commit!

--- app/Console/Commands/MigrateLikeLogs.php
+++ app/Console/Commands/MigrateLikeLogs.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\InstagramProfileLikeLog;
+use App\InstagramProfile;
 
 class MigrateLikeLogs extends Command
 {
@@ -11,14 +13,14 @@ class MigrateLikeLogs extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'migrate:like';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Migrate like logs for this slave.';
 
     /**
      * Create a new command instance.
@@ -37,6 +39,23 @@ class MigrateLikeLogs extends Command
      */
     public function handle()
     {
-        //
+        $ig_profiles = InstagramProfile::all();
+        foreach ($ig_profiles as $ig_profile) {
+            $master_like_logs = DB::connection('mysql_master')
+                    ->table('user_insta_profile_like_log')
+                    ->where('insta_username', $ig_profile->insta_username)
+                    ->get();
+            foreach ($master_like_logs as $master_like_log) {
+                $like_log = new InstagramProfileLikeLog;
+                $like_log->log_id = $master_like_log->log_id;
+                $like_log->insta_username = $master_like_log->insta_username;
+                $like_log->target_username = $master_like_log->target_username;
+                $like_log->target_media = $master_like_log->target_media;
+                $like_log->target_media_code = $master_like_log->target_media_code;
+                $like_log->log = $master_like_log->log;
+                $like_log->date_liked = $master_like_log->date_liked;
+                $like_log->save();
+            }
+        }
     }
 }
