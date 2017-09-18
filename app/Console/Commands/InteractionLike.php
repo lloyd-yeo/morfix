92c4bfc99846b90054020af957f4da344dcb6251
Lloyd Yeo <lloyd@kooki.co>
2017-09-18 10:27:16 +0800

    Lazy Commit!

--- app/Console/Commands/InteractionLike.php
+++ app/Console/Commands/InteractionLike.php
@@ -110,7 +110,6 @@ class InteractionLike extends Command {
                     ->where('invalid_user', false)
                     ->where('incorrect_pw', false)
                     ->where('user_id', $user->user_id)
-                    ->where('next_like_time', '<=', \Carbon\Carbon::now()->toDateTimeString())
                     ->get();
 
             try {
