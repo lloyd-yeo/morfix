4a017250a1b949cdec5904ef529d2acbcf881df2
Lloyd Yeo <lloyd@kooki.co>
2017-09-19 05:53:38 +0800

    Lazy Commit!

--- app/Console/Commands/MigrateProxyList.php
+++ app/Console/Commands/MigrateProxyList.php
@@ -46,7 +46,7 @@ class MigrateProxyList extends Command
         foreach ($proxies as $proxy) {
             $new_proxy = new Proxy;
             $new_proxy->proxy = $proxy->proxy;
-            $new_proxy->assigned = $proxy->proxy;
+            $new_proxy->assigned = $proxy->assigned;
             $new_proxy->error = $proxy->error;
             if ($new_proxy->save()) {
                 $this->line("Migrated [" . $new_proxy->proxy . "]");
