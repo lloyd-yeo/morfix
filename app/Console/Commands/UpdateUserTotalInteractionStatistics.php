da0f6f707a550ba497f506fd8574ebe7b6cb8eb7
Lloyd Yeo <lloyd@kooki.co>
2017-09-20 04:16:07 +0800

    Lazy Commit!

--- app/Console/Commands/UpdateUserTotalInteractionStatistics.php
+++ app/Console/Commands/UpdateUserTotalInteractionStatistics.php
@@ -95,30 +95,42 @@ class UpdateUserTotalInteractionStatistics extends Command {
     
     private function refreshDailyStats($ig_profile) {
         
+//        $daily_likes = InstagramProfileLikeLog::where('insta_username', $ig_profile->insta_username)
+//                ->whereBetween('date_liked', [Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+//                    Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s')])
+//                ->count();
         $daily_likes = InstagramProfileLikeLog::where('insta_username', $ig_profile->insta_username)
-                ->whereBetween('date_liked', [Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
-                    Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s')])
+                ->whereDate('date_liked', '=', Carbon::today()->toDateString())
                 ->count();
         $ig_profile->daily_likes = $daily_likes;
 //        $ig_profile->save();
 
+//        $daily_comments = InstagramProfileCommentLog::where('insta_username', $ig_profile->insta_username)
+//                ->whereBetween('date_commented', [Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+//                    Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s')])
+//                ->count();
         $daily_comments = InstagramProfileCommentLog::where('insta_username', $ig_profile->insta_username)
-                ->whereBetween('date_commented', [Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
-                    Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s')])
+                ->whereDate('date_commented', '=', Carbon::today()->toDateString())
                 ->count();
         $ig_profile->daily_comments = $daily_comments;
 //        $ig_profile->save();
 
-        $daily_follows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)
-                ->whereBetween('date_inserted', [Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
-                    Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s')])
+//        $daily_follows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)
+//                ->whereBetween('date_inserted', [Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+//                    Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s')])
+//                ->count();
+        $daily_follows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)->where('follow', 1)
+                ->whereDate('date_inserted', '=', Carbon::today()->toDateString())
                 ->count();
         $ig_profile->daily_follows = $daily_follows;
 //        $ig_profile->save();
 
-        $daily_unfollows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)
-                ->whereBetween('date_unfollowed', [Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
-                    Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s')])
+//        $daily_unfollows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)
+//                ->whereBetween('date_unfollowed', [Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+//                    Carbon::now()->setTime(23, 59, 59)->format('Y-m-d H:i:s')])
+//                ->count();
+        $daily_unfollows = InstagramProfileFollowLog::where('insta_username', $ig_profile->insta_username)->where('unfollowed', 1)
+                ->whereDate('date_unfollowed', '=', Carbon::today()->toDateString())
                 ->count();
         $ig_profile->daily_unfollows = $daily_unfollows;
         
