543054d921332c778116a8935b54e17c14b96e2a
Lloyd Yeo <lloyd@kooki.co>
2017-09-20 02:51:22 +0800

    Lazy Commit!

--- resources/views/payment/table/business.blade.php
+++ resources/views/payment/table/business.blade.php
@@ -46,7 +46,7 @@
                     </p>
                 </div>
                 <div style='padding-top: 0px; padding-bottom: 20px;'>
-                    <a id="business-btn" href='#' target="_blank" class="text-white" style='
+                    <a id="business-btn" href='https://upgrade.morfix.co/business' target="_blank" class="text-white" style='
                        background-color:rgb(30, 115, 190); 
                        padding: 10px 50px;
                        font-weight: 600; 
