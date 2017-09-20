653d22de3f76bd92a8afedb2a052c7e6a7aab668
kennCK <kennettecanales@gmail.com>
2017-09-20 21:58:34 +0800

    update

--- app/Console/Commands/InteractionComment.php
+++ app/Console/Commands/InteractionComment.php
@@ -184,17 +184,18 @@ class InteractionComment extends Command {
 
                     echo "[$ig_username] real unengaged followings count = $real_unengaged_followings_count \n";
 
-                    // if (count($unengaged_followings) < 1 || $real_unengaged_followings_count == 0) {
-                    //     /*
-                    //       - Call Unengaged Likings Method
-                    //      */
-                    //     $this->unengagedLikings($ig_username, $ig_profile);
-                    // } else {
-                    //     /*
-                    //       - Call unengaged followings method
-                    //      */
-                    //     $this->unengagedFollowings($unengaged_followings, $ig_profile);
-                    // }
+                    if (count($unengaged_followings) < 1 || $real_unengaged_followings_count == 0) {
+                        /*
+                          - Call Unengaged Likings Method
+                         */
+                        $this->unengagedLikings($ig_username, $ig_profile);
+                    } else {
+                        /*
+                          - Call unengaged followings method
+                         */
+                        echo '196';
+                        $this->unengagedFollowings($unengaged_followings, $ig_profile);
+                    }
 
 
 
