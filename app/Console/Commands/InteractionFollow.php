7fa77d7f77cd32b782ab4389098b8ff11d7e7008
Lloyd Yeo <lloyd@kooki.co>
2017-09-20 09:36:12 +0800

    Lazy Commit!

--- app/Console/Commands/InteractionFollow.php
+++ app/Console/Commands/InteractionFollow.php
@@ -121,6 +121,7 @@ class InteractionFollow extends Command {
                             . 'AND account_disabled = 0 '
                             . 'AND invalid_user = 0 '
                             . 'AND incorrect_pw = 0 '
+                            . 'AND feedback_required = 0 '
                             . 'AND (NOW() >= next_follow_time OR next_follow_time IS NULL) '
                             . 'AND user_id = ' . $user->user_id)->get();
             
