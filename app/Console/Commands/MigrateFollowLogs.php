cb00bb33f0522da982a22b841de6a26503e2cfb9
Lloyd Yeo <lloyd@kooki.co>
2017-09-22 09:07:46 +0800

    Lazy Commit!

--- app/Console/Commands/MigrateFollowLogs.php
+++ app/Console/Commands/MigrateFollowLogs.php
@@ -49,26 +49,29 @@ class MigrateFollowLogs extends Command {
                         ->where('insta_username', $ig_profile->insta_username)
                         ->get();
 
-                foreach ($master_follow_logs as $master_follow_log) {
-                    $follow_log = new InstagramProfileFollowLog;
-                    $follow_log->log_id = $master_follow_log->log_id;
-                    $follow_log->insta_username = $master_follow_log->insta_username;
-                    $follow_log->follower_username = $master_follow_log->follower_username;
-                    $follow_log->follower_id = $master_follow_log->follower_id;
-                    $follow_log->log = $master_follow_log->log;
-                    $follow_log->date_inserted = $master_follow_log->date_inserted;
-                    $follow_log->added_delay = $master_follow_log->added_delay;
-                    $follow_log->follow = $master_follow_log->follow;
-                    $follow_log->follow_success = $master_follow_log->follow_success;
-                    $follow_log->unfollowed = $master_follow_log->unfollowed;
-                    $follow_log->unfollow_log = $master_follow_log->unfollow_log;
-                    $follow_log->date_unfollowed = $master_follow_log->date_unfollowed;
-                    try {
-                        $follow_log->save();
-                    } catch (QueryException $ex) {
-                        $this->line($ex->getMessage());
+                DB::connection('mysql_master')->table('user_insta_profile_follow_log')
+                        ->where('insta_username', $ig_profile->insta_username)->chunk(1000, function ($master_follow_logs) {
+                    foreach ($master_follow_logs as $master_follow_log) {
+                        $follow_log = new InstagramProfileFollowLog;
+                        $follow_log->log_id = $master_follow_log->log_id;
+                        $follow_log->insta_username = $master_follow_log->insta_username;
+                        $follow_log->follower_username = $master_follow_log->follower_username;
+                        $follow_log->follower_id = $master_follow_log->follower_id;
+                        $follow_log->log = $master_follow_log->log;
+                        $follow_log->date_inserted = $master_follow_log->date_inserted;
+                        $follow_log->added_delay = $master_follow_log->added_delay;
+                        $follow_log->follow = $master_follow_log->follow;
+                        $follow_log->follow_success = $master_follow_log->follow_success;
+                        $follow_log->unfollowed = $master_follow_log->unfollowed;
+                        $follow_log->unfollow_log = $master_follow_log->unfollow_log;
+                        $follow_log->date_unfollowed = $master_follow_log->date_unfollowed;
+                        try {
+                            $follow_log->save();
+                        } catch (QueryException $ex) {
+                            $this->line($ex->getMessage());
+                        }
                     }
-                }
+                });
             }
         } else {
             $ig_profile = InstagramProfile::where('insta_username', $this->argument('insta_username'))->first();
