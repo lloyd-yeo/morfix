ba057048495c543fca830e961a019c811d382727
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-10-01 06:48:16 +0800

    Lazy Commit!

--- app/Console/Commands/MigrateCommentLogs.php
+++ app/Console/Commands/MigrateCommentLogs.php
@@ -61,7 +61,7 @@ class MigrateCommentLogs extends Command {
                     $comment_log->date_commented = $master_comment_log->date_commented;
                     try {
                         $comment_log->save();
-                    } catch (QueryException $ex) {
+                    } catch (Illuminate\Database\QueryException $ex) {
                         continue;
                     }
                 }
@@ -85,7 +85,7 @@ class MigrateCommentLogs extends Command {
                 $comment_log->date_commented = $master_comment_log->date_commented;
                 try {
                     $comment_log->save();
-                } catch (QueryException $ex) {
+                } catch (Illuminate\Database\QueryException $ex) {
                     continue;
                 }
             }
