bc82f8e0be77782d07398fb8b6d6b94874f4fefb
kennCK <kennettecanales@gmail.com>
2017-10-03 10:46:57 +0800

    update

--- app/Jobs/InteractionFollow.php
+++ app/Jobs/InteractionFollow.php
@@ -120,7 +120,7 @@ class InteractionFollow implements ShouldQueue {
                 echo "[" . $this->profile->insta_username . "] beginning following sequence.\n";
                 $this->initInstagramAPI($this->profile);
                 $use_hashtags = InstagramHelper::randomizeUseHashtags($this->instagram, $this->profile, $this->targeted_hashtags, $this->targeted_usernames);
-
+                
                 if ($use_hashtags == 0) {
                     //use targeted usernames
                     foreach ($this->targeted_usernames as $target_username) {
