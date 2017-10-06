2b9cad5e669306d8086227511e1060a0c91b7513
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-10-06 11:09:25 +0800

    Lazy Commit!

--- app/Jobs/InteractionFollow.php
+++ app/Jobs/InteractionFollow.php
@@ -145,7 +145,7 @@ class InteractionFollow implements ShouldQueue {
                             $valid_user = InteractionFollowHelper::isProfileValidForFollow($this->instagram, $this->profile, $user_to_follow);
                             
                             if ($valid_user == 2) {
-                                echo "\n[" . $ig_profile->insta_username . "] encountered an exception.\n";
+                                echo "\n[" . $this->profile->insta_username . "] encountered an exception.\n";
                                 return;
                             } else if ($valid_user) {
                                 $this->followed = InteractionFollowHelper::follow($this->instagram, $this->profile, $user_to_follow);
