1d59264ef87072e261b14391209aa3be42cb0e35
rong <rong@192.168.2.97>
2017-10-09 14:48:52 +0800

    route for stripe webhook3

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\StripeWebhookLog;
+use Carbon\Carbon;
 
-class StripeWebhookController extends Controller
-{
+class StripeWebhookController extends Controller {
+
     public function chargeRefunded(Request $request) {
+
+        \Stripe\Stripe::setApiKey("sk_test_dAO7D2WkkUOHnuHgXBeti0KM");
         
+        $webhook_log = new StripeWebhookLog;
+        $webhook_log->log = $request->json();
+        $webhook_log->date_logged = Carbon::now();
+        $webhook_log->save();
     }
-    
+
     public function invoicePaymentFailed(Request $request) {
-        
+
+        \Stripe\Stripe::setApiKey("sk_test_dAO7D2WkkUOHnuHgXBeti0KM");
     }
+
 }
--- app/StripeWebhookLog.php
+++ app/StripeWebhookLog.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class StripeWebhookLog extends Model {
 
     protected $table = 'stripe_webhook_logs';
+    protected $primaryKey = 'stripe_log_id';
+    public $timestamp = false;
+    public $incrementing = false;
 
 }
