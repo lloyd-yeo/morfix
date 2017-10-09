e148a4646ae8738b7bab22a46dddd219862ccb9b
rong <rong@192.168.2.97>
2017-10-09 15:34:25 +0800

    route for stripe webhook5

--- app/StripeWebhookLog.php
+++ app/StripeWebhookLog.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class StripeWebhookLog extends Model {
 
-    protected $table = 'stripe_webhook_logs';
+    protected $table = 'stripe_webhook_log';
     protected $primaryKey = 'stripe_log_id';
     public $timestamp = false;
     public $incrementing = false;
