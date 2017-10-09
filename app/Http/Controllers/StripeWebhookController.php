d3788cb82124910ee3fe205014e680906fa65975
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-10-09 15:25:39 +0800

    Lazy Commit!

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -19,8 +19,14 @@ class StripeWebhookController extends Controller {
     }
 
     public function invoicePaymentFailed(Request $request) {
-
+        
         \Stripe\Stripe::setApiKey("sk_test_dAO7D2WkkUOHnuHgXBeti0KM");
+        
+        $webhook_log = new StripeWebhookLog;
+        $webhook_log->log = serialize($request->all());
+        $webhook_log->date_logged = Carbon::now();
+        $webhook_log->save();
+        
     }
 
 }
