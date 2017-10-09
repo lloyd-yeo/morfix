771c9070e7f97e35d1ddd2d50c0e0afb7eac5ba3
rong <rong@192.168.2.97>
2017-10-09 14:37:28 +0800

    route for stripe webhook2

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\StripeWebhookLog;
 
 class StripeWebhookController extends Controller
 {
-    //
+    public function chargeRefunded(Request $request){
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -142,7 +142,7 @@ Route::post('/admin/getstripedetails', 'AdminController@getStripeDetails');
 Route::post('/admin/interaction/like', 'AdminController@runInteractionLike');
 Route::post('/admin/interaction/comment', 'AdminController@runInteractionComment');
 
-Route::post('/stripe/charge/refunded');
+Route::post('/stripe/charge/refunded', 'StripeWebhookController@chargeRefunded');
 Route::post('/stripe/invoice/paymentfailed');
 
 /**
