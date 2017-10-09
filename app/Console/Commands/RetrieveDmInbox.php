52968fb3a01af806b8762926ae1a52c6c74be0dd
kennCK <kennettecanales@gmail.com>
2017-10-09 16:20:39 +0800

    update

--- app/Console/Commands/RetrieveDmInbox.php
+++ app/Console/Commands/RetrieveDmInbox.php
@@ -146,7 +146,7 @@ class RetrieveDmInbox extends Command
                 $subArray = (array)$subObject;
                 foreach ($subObject as $k1 => $v1) {
                     echo "\t\t".$k1."\n";
-                    if(sizeof($subArray[$k1]) > 1){
+                    if(sizeof($subArray[$k1]) > 0){
                         $subObject1 = (object) $subArray[$k1];
                         foreach ($subObject1 as $k2 => $v2) {
                             echo "\t\t\t".$k2."\n";
