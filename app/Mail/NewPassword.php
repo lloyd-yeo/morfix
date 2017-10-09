11ba91a69765435b1988a75706be94d92f2d274f
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-10-09 11:21:14 +0800

    Lazy Commit!

--- app/Mail/NewPassword.php
+++ app/Mail/NewPassword.php
@@ -36,16 +36,23 @@ class NewPassword extends Mailable
         $subject = "";
         if ($this->mode == "free_trial") {
             $subject = '[Morfix] Your Free Trial account is ready!';
-        } else if ($this->mode == "premium") {
-            $subject = '[Morfix] Your Premium account is ready!';
-        }
-        
-        return $this->view('email.signup.password')
+            return $this->view('email.signup.password')
                     ->subject($subject)
                     ->with(['name' => $this->user->name, 
                         'email' => $this->user->email, 
                         'password' => $this->user->password,
                         'mode' => $this->mode]);
+        } else if ($this->mode == "premium") {
+            $subject = '[Morfix] Your Premium account is ready!';
+            return $this->view('email.signup.premium')
+                    ->subject($subject)
+                    ->with(['name' => $this->user->name, 
+                        'email' => $this->user->email, 
+                        'password' => $this->user->password,
+                        'mode' => $this->mode]);
+        }
+        
+        
         
     }
 }
