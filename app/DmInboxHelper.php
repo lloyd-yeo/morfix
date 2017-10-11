f56ae87ac7bc82aa5b96cebe20213d779c9e0311
kennCK <kennettecanales@gmail.com>
2017-10-11 23:43:05 +0800

    update

--- app/DmInboxHelper.php
+++ app/DmInboxHelper.php
@@ -8,65 +8,41 @@ use LazyJsonMapper\LazyJsonMapper;
 
 class DmInboxHelper extends LazyJsonMapper{
 
-  public static function retrieve(Instagram $instagram){
-    try{
-         $response = $instagram->direct->getInbox();
-         if($response->getInbox()->getThreads()){
-            $threads = (array)$response->getInbox()->getThreads();
-            $i = 0;
-            foreach ($threads as $thread) {
-                $i++;
-                echo "\t Thread => $i\n";
-                $threadResponse = $instagram->direct->getThread($thread->getThreadId());
-                DmInboxHelper::manage($threadResponse->getThread());
-            }
-          return $response->getInbox();
-         }
-         else{ 
-          echo "\t Inbox is Empty.\n\n All Resuts => ".json_encode($responseArray);
-          return null;
-         }
-    }catch (CheckpointRequiredException $ex) {
-      DmInboxHelper::handleInstagramException($ex);
-      return false;
-    } catch (IncorrectPasswordException $ex) {
-      DmInboxHelper::handleInstagramException($ex);
-      return false;
-    } catch (EndpointException $ex) {
-      DmInboxHelper::handleInstagramException($ex);
-      return false;
-    } catch (NetworkException $ex) {
-      DmInboxHelper::handleInstagramException($ex);
-      return false;
-    } catch (AccountDisabledException $ex) {
-      DmInboxHelper::handleInstagramException($ex);
-      return false;
-    } catch (RequestException $ex) {
-      DmInboxHelper::handleInstagramException($ex);
-      return false;
-    } catch (\Exception $ex) {
-      DmInboxHelper::handleInstagramException($ex);
-      return false;
-    }
+public static function retrieve(Instagram $instagram){
+   $response = $instagram->direct->getInbox();
+   if($response->getInbox()->getThreads()){
+      $threads = (array)$response->getInbox()->getThreads();
+      $i = 0;
+      foreach ($threads as $thread) {
+          $i++;
+          echo "\t Thread => $i\n";
+          $threadResponse = $instagram->direct->getThread($thread->getThreadId());
+          DmInboxHelper::extractItems((array)$threadResponse->getThread());
+      }
+    return $response->getInbox();
+   }
+   else{ 
+    echo "\t Inbox is Empty.\n\n All Resuts => ".json_encode($responseArray);
+    return null;
+   }
   }
 
-  public static function manage($thread){
-    $items = $thread->getItems();
-    echo "\t\t Items\n".json_encode((array)$items[0]);
+  public static function extractThreads($inbox){
+    return (array)$inbox->getThreads();
+  }
+
+  public static function extractUsers($thread){
+    return (array)$thread->getUsers();
+  }
+
+  public static function extractItems($thread){
+    echo json_encode($thread)."\n";
+    return (array)$thread->getItems();
   }
 
   public static function getThread(Instagram $instagram, $threadId){
     $threadResponse = $instagram->direct->getThread($thread_id);
-    return $threadResponse->getThread();
-  }
-
-  public static function displayIndexes($objects){
-      foreach ((array)$objects as $object) {
-        echo json_encode($object)."\n";
-        foreach ((array)$object as $key => $value) {
-          echo "\t\t\t $key => ".json_encode($value)."\n";
-        } 
-      }
+    return (array)$threadResponse->getThread();
   }
 
   public static function handleInstagramException($ex){
