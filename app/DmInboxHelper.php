d901ef9b9cba14e81e014fb60037fa7f262a54b7
kennCK <kennettecanales@gmail.com>
2017-10-11 13:22:52 +0800

    update

--- app/DmInboxHelper.php
+++ app/DmInboxHelper.php
@@ -49,7 +49,7 @@ class DmInboxHelper{
   }
 
   public static function manage($thread){
-    $thread->printJson();
+    $thread->getItems()->printJson();
     // $threadArray = (array)$thread->objectData();
     // foreach ($threadArray as $key => $value) {
     //   if($key == 'users' || $key == "inviter" || $key == "last_seen_at"){
