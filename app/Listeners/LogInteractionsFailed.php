f8589ae0d8edcfa7714494bbe00606b7ac26906e
rong <rong@192.168.2.97>
2017-10-16 02:31:59 +0800

    checkinteractions22

--- app/Listeners/LogInteractionsFailed.php
+++ app/Listeners/LogInteractionsFailed.php
@@ -6,7 +6,6 @@ use App\Events\UserInteractionsFailed;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\UserInteractionFailed;
-use Carbon\Carbon;
 
 class LogInteractionsFailed
 {
@@ -32,8 +31,13 @@ class LogInteractionsFailed
         if (!empty($event->failed_profiles))
         {
             foreach($event->failed_profiles as $failed_profile){
-                $failed_profile->notified = 1;
-                $failed_profile->save();
+                $from = Carbon::now()->subMinute(15)->toDateTimeString();
+                $update = UserInteractionFailed::where('insta_username' , $failed_profile->insta_username)
+                    ->first();
+                if($update !== NULL){
+                    $update->notified = 1;
+                    $update->save();
+                }
             }
         }
 
