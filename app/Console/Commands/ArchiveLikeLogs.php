c31c4f8f1fd1fe4f7dd2263559bcac2aca04a290
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-10-17 16:53:28 +0800

    Lazy Commit!

--- app/Console/Commands/ArchiveLikeLogs.php
+++ app/Console/Commands/ArchiveLikeLogs.php
@@ -18,71 +18,70 @@ use App\CreateInstagramProfileLog;
 use App\Proxy;
 use App\DmJob;
 
-class ArchiveLikeLogs extends Command {
+class ArchiveLikeLogs extends Command
+{
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'archive:like {size}';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'archive:like {size}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Do up an archive of the like logs.';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Do up an archive of the like logs.';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct() {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle() {
-        $size = $this->argument('size');
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$size = $this->argument('size');
 
-//        while ($size < 8038619) {
-//            $size = $size + 10000;
-            
-            echo "Archiving results with log_id <= $size...\n";
-            DB::insert('INSERT IGNORE INTO user_insta_profile_like_log_archive (insta_username, target_username, 
+		echo "Archiving results with log_id <= $size...\n";
+		DB::insert('INSERT IGNORE INTO user_insta_profile_like_log_archive (insta_username, target_username, 
                 target_media, target_media_code, log, date_liked) 
                 SELECT insta_username, target_username, target_media, target_media_code, log, date_liked 
                 FROM user_insta_profile_like_log 
-                WHERE log_id <= ?;', [$size]);
-            echo "Archiving success!\n";
-            
-            echo "Deleting from current table...\n";
-            InstagramProfileLikeLog::where('log_id', '<=', $size)->chunk(200, function ($like_logs) {
+                WHERE log_id <= ?;', [ $size ]);
+		echo "Archiving success!\n";
 
-                foreach ($like_logs as $like_log) {
-//                $archive = new LikeLogsArchive;
-//                $archive->insta_username = $like_log->insta_username;
-//                $archive->target_username = $like_log->target_username;
-//                $archive->target_media = $like_log->target_media;
-//                $archive->target_media_code = $like_log->target_media_code;
-//                $archive->log = $like_log->log;
-//                $archive->date_liked = $like_log->date_liked;
-//                    if ($archive->save()) {
-//                        echo "Saving archive #" . $archive->log_id . "\n";
-//                    }
+		echo "Deleting from current table...\n";
+		InstagramProfileLikeLog::where('log_id', '<=', $size)->chunk(3000, function ($like_logs) {
 
-                    echo "Removing: " . $like_log->log_id . "\n";
-                    $like_log->delete();
-                }
-            });
-            echo "Deleting complete!\n";
-//        }
-    }
+			foreach ($like_logs as $like_log) {
+//				$archive = new LikeLogsArchive;
+//				$archive->insta_username = $like_log->insta_username;
+//				$archive->target_username = $like_log->target_username;
+//				$archive->target_media = $like_log->target_media;
+//				$archive->target_media_code = $like_log->target_media_code;
+//				$archive->log = $like_log->log;
+//				$archive->date_liked = $like_log->date_liked;
+//				if ($archive->save()) {
+//					echo "Saving archive #" . $archive->log_id . "\n";
+//				}
+
+				echo "Removing: " . $like_log->log_id . "\n";
+				$like_log->delete();
+			}
+		});
+		echo "Deleting complete!\n";
+	}
 
 }
