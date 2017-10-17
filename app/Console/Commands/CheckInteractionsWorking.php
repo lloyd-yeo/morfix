061d634bff85f262e942bc98227c091eecd5f48e
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-10-17 16:27:20 +0800

    Lazy Commit!

--- app/Console/Commands/CheckInteractionsWorking.php
+++ app/Console/Commands/CheckInteractionsWorking.php
@@ -85,7 +85,7 @@ class CheckInteractionsWorking extends Command
     public function checkIgProfile($ig_profile, $tier, $partition)
     {
         $updated = false;
-        $from = Carbon::now()->subHours(3)->toDateTimeString();
+        $from = Carbon::now()->subHours(2)->toDateTimeString();
         $to = Carbon::now()->toDateTimeString();
         $user_like = NULL;
         $user_comment = NULL;
