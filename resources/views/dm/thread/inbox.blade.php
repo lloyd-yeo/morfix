c8d8aa0943e027b790d57073a51cc501e0fd001b
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-10-24 13:43:19 +0800

    Lazy Commit!

--- resources/views/dm/thread/inbox.blade.php
+++ resources/views/dm/thread/inbox.blade.php
@@ -28,40 +28,9 @@
                     <!-- Friends Widget -->
                     <div class="block block-bordered">
                         <div class="block-header">
-                            <ul class="block-options">
-                                <li>
-                                    <button type="button" data-toggle="block-option"
-                                            data-action="fullscreen_toggle"></button>
-                                </li>
-                                <li>
-                                    <button type="button" data-toggle="block-option" data-action="refresh_toggle"
-                                            data-action-mode="demo"><i class="si si-refresh"></i></button>
-                                </li>
-                                <li>
-                                    <button type="button" data-toggle="block-option"
-                                            data-action="content_toggle"></button>
-                                </li>
-                            </ul>
-                            <h3 class="block-title">Friends</h3>
+                            <h3 class="block-title"><i></i>DM Inbox</h3>
                         </div>
                         <div class="block-content">
-                            <div class="pull-r-l pull-t push">
-                                <table class="block-table text-center bg-gray-lighter border-b">
-                                    <tbody>
-                                    <tr>
-                                        <td class="border-r" style="width: 50%;">
-                                            <div class="h1 font-w700">3</div>
-                                            <div class="h5 text-muted text-uppercase push-5-t">New Friends</div>
-                                        </td>
-                                        <td>
-                                            <div class="push-30 push-30-t">
-                                                <i class="si si-users fa-3x text-black-op"></i>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
                             <ul class="nav-users push">
                                 <li>
                                     <a href="base_pages_profile.html">
