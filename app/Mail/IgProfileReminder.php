eb1f99679a779d76e7102f263990b57184b3df54
Lim Choon Rong <rong@MacBook-Pro.local>
2017-10-27 12:09:24 +0800

    IgprofileReminder

---
+++ app/Mail/IgProfileReminder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\User;
+
+class IgProfileReminder extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $subject = "Have you added your Instagram Profile with us yet?";
+
+        return $this->view('email.signup.profilereminder')
+            ->subject($subject)
+            ->with(['name' => $this->user->name]);
+    }
+}
