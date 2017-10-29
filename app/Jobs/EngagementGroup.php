7d64af527392ff9cd362b2548ef2e13f9344fbe9
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-10-30 01:50:27 +0800

    Lazy Commit!

--- app/Jobs/EngagementGroup.php
+++ app/Jobs/EngagementGroup.php
@@ -108,7 +108,7 @@ class EngagementGroup implements ShouldQueue
 				} else {
 					echo "[" . $ig_profile->insta_username . "] owner is NOT on Free-Trial\n";
 
-					if ($this->comment === 1) {
+					if ($this->comment == 1) {
 						echo ("[" . $ig_profile->insta_username . "] has turned on Auto-Comment\n");
 
 						if ($ig_profile->auto_comment === 1) {
