ddf87f501c3a84e00dc89c8607db9301d0c0e688
rong <rongronggg@gmail.com>
2017-11-03 13:42:55 +0800

    OctoberPayoutUpdates3

--- app/Console/Commands/UpdateLastPaidFromCSV.php
+++ app/Console/Commands/UpdateLastPaidFromCSV.php
@@ -62,8 +62,8 @@ class UpdateLastPaidFromCSV extends Command {
                     $this->UpdateUserChargesPaid($user);
                     $this->CalculateUserPendingCommissions($user,$paid_amount,$tier);
                     $user->paid_amount = $data[2];
-                    $user->testing_pending_commission_payable = 0;
-                    $user->testing_all_time_commission = $user->all_time_commission + $data[2];
+                    $user->pending_commission_payable = 0;
+                    $user->all_time_commission = $user->all_time_commission + $data[2];
                     $user->save();
                     echo "Updated [$current_email] last pay out date to [$last_pay_out_coms_date]\n";
                     echo "Updated [$current_email] last pay out amount to [$data[2]]\n";
