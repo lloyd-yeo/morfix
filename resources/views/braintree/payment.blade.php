2b4cdb61622a94a6273eddba38bc6edeb729f05f
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-11-07 18:56:10 +0800

    Lazy Commit!

--- resources/views/braintree/payment.blade.php
+++ resources/views/braintree/payment.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
+@section('css')
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="canvas/css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="canvas/style.css" type="text/css" />
+	<link rel="stylesheet" href="canvas/css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="canvas/css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="canvas/css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="canvas/css/magnific-popup.css" type="text/css" />
+@endsection
+
 @section('sidebar')
 	@include('sidebar', ['page' => 'payment'])
 @endsection
