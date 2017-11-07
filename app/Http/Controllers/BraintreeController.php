4ba7001666ae79e5ee70fd0d7053aabd774ea40b
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-11-07 10:36:44 +0800

    Lazy Commit!

--- app/Http/Controllers/BraintreeController.php
+++ app/Http/Controllers/BraintreeController.php
@@ -8,15 +8,11 @@ use Illuminate\Http\Request;
 
 class BraintreeController extends Controller
 {
-	public function __construct()
-	{
+	public function payment(Request $request) {
 		Braintree_Configuration::environment('production');
 		Braintree_Configuration::merchantId('4x5qk4ggmgf9t5vw');
 		Braintree_Configuration::publicKey('vtq3w9x62s57p82y');
 		Braintree_Configuration::privateKey('c578012b2eb171582133ed0372f3a2ae');
-	}
-
-	public function payment(Request $request) {
 		$clientToken = Braintree_ClientToken::generate();
 		dump($clientToken);
 		return view('funnels.ebook');
