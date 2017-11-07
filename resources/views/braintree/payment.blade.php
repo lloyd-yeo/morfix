de5918738b4c0dffb066e3b27a80014e9e4fe729
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-11-07 18:07:01 +0800

    Lazy Commit!

--- resources/views/braintree/payment.blade.php
+++ resources/views/braintree/payment.blade.php
@@ -30,31 +30,31 @@
 			</form>
 
 			{{--<div class="row font-s13">--}}
-				{{--@if (Auth::user()->vip == 1)--}}
-					{{--@include('braintree.table.premium')--}}
-					{{--@include('braintree.table.pro')--}}
-					{{--@include('braintree.table.business')--}}
-					{{--@include('braintree.table.mastermind')--}}
-				{{--@elseif (Auth::user()->tier == 1)--}}
-					{{--@include('braintree.table.premium')--}}
-					{{--@include('braintree.table.pro')--}}
-				{{--@elseif (Auth::user()->tier == 2)--}}
-					{{--@include('braintree.table.pro', ['unit' => 4])--}}
-					{{--@include('braintree.table.business', ['unit' => 4])--}}
-					{{--@include('braintree.table.mastermind', ['unit' => 4])--}}
-				{{--@elseif (Auth::user()->tier == 3)--}}
-					{{--@include('braintree.table.business')--}}
-					{{--@include('braintree.table.mastermind')--}}
-				{{--@elseif (Auth::user()->tier == 12)--}}
-					{{--@include('braintree.table.pro')--}}
-					{{--@include('braintree.table.mastermind')--}}
-				{{--@elseif (Auth::user()->tier == 22)--}}
-					{{--<div class='col-lg-3'></div>--}}
-					{{--@include('braintree.table.pro')--}}
-				{{--@elseif (Auth::user()->tier == 13)--}}
-					{{--<div class='col-lg-3'></div>--}}
-					{{--@include('braintree.table.mastermind')--}}
-				{{--@endif--}}
+			{{--@if (Auth::user()->vip == 1)--}}
+			{{--@include('braintree.table.premium')--}}
+			{{--@include('braintree.table.pro')--}}
+			{{--@include('braintree.table.business')--}}
+			{{--@include('braintree.table.mastermind')--}}
+			{{--@elseif (Auth::user()->tier == 1)--}}
+			{{--@include('braintree.table.premium')--}}
+			{{--@include('braintree.table.pro')--}}
+			{{--@elseif (Auth::user()->tier == 2)--}}
+			{{--@include('braintree.table.pro', ['unit' => 4])--}}
+			{{--@include('braintree.table.business', ['unit' => 4])--}}
+			{{--@include('braintree.table.mastermind', ['unit' => 4])--}}
+			{{--@elseif (Auth::user()->tier == 3)--}}
+			{{--@include('braintree.table.business')--}}
+			{{--@include('braintree.table.mastermind')--}}
+			{{--@elseif (Auth::user()->tier == 12)--}}
+			{{--@include('braintree.table.pro')--}}
+			{{--@include('braintree.table.mastermind')--}}
+			{{--@elseif (Auth::user()->tier == 22)--}}
+			{{--<div class='col-lg-3'></div>--}}
+			{{--@include('braintree.table.pro')--}}
+			{{--@elseif (Auth::user()->tier == 13)--}}
+			{{--<div class='col-lg-3'></div>--}}
+			{{--@include('braintree.table.mastermind')--}}
+			{{--@endif--}}
 			{{--</div>--}}
 		</div>
 	</main>
@@ -70,12 +70,10 @@
             paypal: {
                 flow: 'vault',
                 buttonStyle: {
-                    style: {
-                        size: 'small',
-                        color: 'gold',
-                        shape: 'pill',
-                        label: 'buynow'
-                    },
+                    size: 'small',
+                    color: 'gold',
+                    shape: 'pill',
+                    label: 'buynow'
                 },
             },
             card: {
@@ -87,7 +85,7 @@
             button.addEventListener('click', function () {
                 instance.requestPaymentMethod(function (err, payload) {
                     // Submit payload.nonce to your server
-	                console.log(payload.nonce);
+                    console.log(payload.nonce);
                 });
             });
         });
