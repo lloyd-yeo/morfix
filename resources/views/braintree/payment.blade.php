5d76dda714b9a7eaf20b26d917d84ad071da50f9
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-11-07 13:38:55 +0800

    Lazy Commit!

--- resources/views/braintree/payment.blade.php
+++ resources/views/braintree/payment.blade.php
@@ -19,34 +19,36 @@
 		</div>
 
 		<div class="content content-boxed">
+
 			<div id="dropin-container"></div>
-			<div class="row font-s13">
-				@if (Auth::user()->vip == 1)
-					@include('braintree.table.premium')
-					@include('braintree.table.pro')
-					@include('braintree.table.business')
-					@include('braintree.table.mastermind')
-				@elseif (Auth::user()->tier == 1)
-					@include('braintree.table.premium')
-					@include('braintree.table.pro')
-				@elseif (Auth::user()->tier == 2)
-					@include('braintree.table.pro', ['unit' => 4])
-					@include('braintree.table.business', ['unit' => 4])
-					@include('braintree.table.mastermind', ['unit' => 4])
-				@elseif (Auth::user()->tier == 3)
-					@include('braintree.table.business')
-					@include('braintree.table.mastermind')
-				@elseif (Auth::user()->tier == 12)
-					@include('braintree.table.pro')
-					@include('braintree.table.mastermind')
-				@elseif (Auth::user()->tier == 22)
-					<div class='col-lg-3'></div>
-					@include('braintree.table.pro')
-				@elseif (Auth::user()->tier == 13)
-					<div class='col-lg-3'></div>
-					@include('braintree.table.mastermind')
-				@endif
-			</div>
+
+			{{--<div class="row font-s13">--}}
+				{{--@if (Auth::user()->vip == 1)--}}
+					{{--@include('braintree.table.premium')--}}
+					{{--@include('braintree.table.pro')--}}
+					{{--@include('braintree.table.business')--}}
+					{{--@include('braintree.table.mastermind')--}}
+				{{--@elseif (Auth::user()->tier == 1)--}}
+					{{--@include('braintree.table.premium')--}}
+					{{--@include('braintree.table.pro')--}}
+				{{--@elseif (Auth::user()->tier == 2)--}}
+					{{--@include('braintree.table.pro', ['unit' => 4])--}}
+					{{--@include('braintree.table.business', ['unit' => 4])--}}
+					{{--@include('braintree.table.mastermind', ['unit' => 4])--}}
+				{{--@elseif (Auth::user()->tier == 3)--}}
+					{{--@include('braintree.table.business')--}}
+					{{--@include('braintree.table.mastermind')--}}
+				{{--@elseif (Auth::user()->tier == 12)--}}
+					{{--@include('braintree.table.pro')--}}
+					{{--@include('braintree.table.mastermind')--}}
+				{{--@elseif (Auth::user()->tier == 22)--}}
+					{{--<div class='col-lg-3'></div>--}}
+					{{--@include('braintree.table.pro')--}}
+				{{--@elseif (Auth::user()->tier == 13)--}}
+					{{--<div class='col-lg-3'></div>--}}
+					{{--@include('braintree.table.mastermind')--}}
+				{{--@endif--}}
+			{{--</div>--}}
 		</div>
 	</main>
 @endsection
