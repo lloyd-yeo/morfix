e2aa2dee53eaaf53dd6f32f74e64ba7b3c5dc388
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-11-14 15:24:36 +0800

    Lazy Commit!

--- resources/views/payment/js.blade.php
+++ resources/views/payment/js.blade.php
@@ -10,83 +10,83 @@ $.ajaxSetup({
 });
 var key = "pk_live_WrvnbbOwMxU7FwZzaoTdaUpa";
 
-var premium_handler = StripeCheckout.configure({
-    key: key,
-    image: 'https://morfix.co/app/assets/img/mx-black-crop.png',
-    locale: 'auto',
-    token: function (token) {
-        App.loader('show');
-        
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-        $.post("/upgrade/premium", { stripeToken: token.id, email: "{{ Auth::user()->email }}" },
-            function (data) {
-                if (data.success === true) {
-                    swal('Congratulations!', "You have successfully been upgraded to Premium!", 'success');
-                } else {
-                    swal('Oops...', data.message, 'error');
-                }
-            },"json").always( function(){ App.loader('hide'); } );
-            
-    }
-});
+{{--var premium_handler = StripeCheckout.configure({--}}
+    {{--key: key,--}}
+    {{--image: 'https://morfix.co/app/assets/img/mx-black-crop.png',--}}
+    {{--locale: 'auto',--}}
+    {{--token: function (token) {--}}
+        {{--App.loader('show');--}}
+        {{----}}
+        {{--// You can access the token ID with `token.id`.--}}
+        {{--// Get the token ID to your server-side code for use.--}}
+        {{--$.post("/upgrade/premium", { stripeToken: token.id, email: "{{ Auth::user()->email }}" },--}}
+            {{--function (data) {--}}
+                {{--if (data.success === true) {--}}
+                    {{--swal('Congratulations!', "You have successfully been upgraded to Premium!", 'success');--}}
+                {{--} else {--}}
+                    {{--swal('Oops...', data.message, 'error');--}}
+                {{--}--}}
+            {{--},"json").always( function(){ App.loader('hide'); } );--}}
+            {{----}}
+    {{--}--}}
+{{--});--}}
 
-var pro_handler = StripeCheckout.configure({
-    key: key,
-    image: 'https://morfix.co/app/assets/img/mx-black-crop.png',
-    locale: 'auto',
-    token: function (token) {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-        $.post("/upgrade/pro", { stripeToken: token.id, email: "{{ Auth::user()->email }}" },
-            function (data) {
-                if (data.success === true) {
-                    swal('Congratulations!', "You have successfully been upgraded to Pro!", 'success');
-                } else {
-                    swal('Oops...', data.message, 'error');
-                }
-            },"json");
-    }
-});
+{{--var pro_handler = StripeCheckout.configure({--}}
+    {{--key: key,--}}
+    {{--image: 'https://morfix.co/app/assets/img/mx-black-crop.png',--}}
+    {{--locale: 'auto',--}}
+    {{--token: function (token) {--}}
+        {{--// You can access the token ID with `token.id`.--}}
+        {{--// Get the token ID to your server-side code for use.--}}
+        {{--$.post("/upgrade/pro", { stripeToken: token.id, email: "{{ Auth::user()->email }}" },--}}
+            {{--function (data) {--}}
+                {{--if (data.success === true) {--}}
+                    {{--swal('Congratulations!', "You have successfully been upgraded to Pro!", 'success');--}}
+                {{--} else {--}}
+                    {{--swal('Oops...', data.message, 'error');--}}
+                {{--}--}}
+            {{--},"json");--}}
+    {{--}--}}
+{{--});--}}
 
-var business_handler = StripeCheckout.configure({
-    key: key,
-    image: 'https://morfix.co/app/assets/img/mx-black-crop.png',
-    locale: 'auto',
-    token: function (token) {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-        $.post("/upgrade/business", { stripeToken: token.id, email: "{{ Auth::user()->email }}" },
-            function (data) {
-                if (data.success === true) {
-                    swal('Congratulations!', "You have successfully been upgraded to Business!", 'success');
-                } else {
-                    swal('Oops...', data.message, 'error');
-                }
-            },"json");
-    }
-});
+{{--var business_handler = StripeCheckout.configure({--}}
+    {{--key: key,--}}
+    {{--image: 'https://morfix.co/app/assets/img/mx-black-crop.png',--}}
+    {{--locale: 'auto',--}}
+    {{--token: function (token) {--}}
+        {{--// You can access the token ID with `token.id`.--}}
+        {{--// Get the token ID to your server-side code for use.--}}
+        {{--$.post("/upgrade/business", { stripeToken: token.id, email: "{{ Auth::user()->email }}" },--}}
+            {{--function (data) {--}}
+                {{--if (data.success === true) {--}}
+                    {{--swal('Congratulations!', "You have successfully been upgraded to Business!", 'success');--}}
+                {{--} else {--}}
+                    {{--swal('Oops...', data.message, 'error');--}}
+                {{--}--}}
+            {{--},"json");--}}
+    {{--}--}}
+{{--});--}}
 
-var mastermind_handler = StripeCheckout.configure({
-    key: key,
-    image: 'https://morfix.co/app/assets/img/mx-black-crop.png',
-    locale: 'auto',
-    token: function (token) {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-        $.post("/upgrade/mastermind", { stripeToken: token.id, email: "{{ Auth::user()->email }}" },
-            function (data) {
-                if (data.success === true) {
-                    swal('Congratulations!', "You have successfully been upgraded to Mastermind!", 'success');
-                } else {
-                    swal('Oops...', data.message, 'error');
-                }
-            },"json");
-    }
-});
+{{--var mastermind_handler = StripeCheckout.configure({--}}
+    {{--key: key,--}}
+    {{--image: 'https://morfix.co/app/assets/img/mx-black-crop.png',--}}
+    {{--locale: 'auto',--}}
+    {{--token: function (token) {--}}
+        {{--// You can access the token ID with `token.id`.--}}
+        {{--// Get the token ID to your server-side code for use.--}}
+        {{--$.post("/upgrade/mastermind", { stripeToken: token.id, email: "{{ Auth::user()->email }}" },--}}
+            {{--function (data) {--}}
+                {{--if (data.success === true) {--}}
+                    {{--swal('Congratulations!', "You have successfully been upgraded to Mastermind!", 'success');--}}
+                {{--} else {--}}
+                    {{--swal('Oops...', data.message, 'error');--}}
+                {{--}--}}
+            {{--},"json");--}}
+    {{--}--}}
+{{--});--}}
 
 
-$("#premium-btn").on("click", function(){ 
+//$("#premium-btn").on("click", function(){
     // Open Checkout with further options:
 //    premium_handler.open({
 //        name: 'Morfix.co',
@@ -94,9 +94,9 @@ $("#premium-btn").on("click", function(){
 //        panelLabel: 'Subscribe',
 //        email: "{{ Auth::user()->email }}"
 //    });
-});
+//});
 
-$("#pro-btn").on("click", function(){ 
+//$("#pro-btn").on("click", function(){
     // Open Checkout with further options:
 //    pro_handler.open({
 //        name: 'Morfix.co',
@@ -104,9 +104,9 @@ $("#pro-btn").on("click", function(){
 //        panelLabel: 'Subscribe',
 //        email: "{{ Auth::user()->email }}"
 //    });
-});
+//});
 
-$("#business-btn").on("click", function(){ 
+//$("#business-btn").on("click", function(){
     // Open Checkout with further options:
 //    business_handler.open({
 //        name: 'Morfix.co',
@@ -114,9 +114,9 @@ $("#business-btn").on("click", function(){
 //        panelLabel: 'Subscribe',
 //        email: "{{ Auth::user()->email }}"
 //    });
-});
+//});
 
-$("#mastermind-btn").on("click", function(){ 
+//$("#mastermind-btn").on("click", function(){
     // Open Checkout with further options:
 //    pro_handler.open({
 //        name: 'Morfix.co',
@@ -124,7 +124,7 @@ $("#mastermind-btn").on("click", function(){
 //        panelLabel: 'Subscribe',
 //        email: "{{ Auth::user()->email }}"
 //    });
-});
+//});
 
 jQuery(function () {
     @if (Auth::user()->tier == 1)
