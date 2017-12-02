3408dbbe59644a3978233c40d48df7dd8c38e496
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-12-03 05:04:52 +0800

    Lazy Commit!

--- resources/views/competition/modal/prizes.blade.php
+++ resources/views/competition/modal/prizes.blade.php
@@ -10,22 +10,22 @@
 							<button data-dismiss="modal" type="button"><i class="si si-close"></i></button>
 						</li>
 					</ul>
-					<h3 class="block-title">Terms & Conditions</h3>
+					<h3 class="block-title">Prizes</h3>
 				</div>
 				<div class="block-content">
-					<div class='row'>
-						<div class="col-xs-12 embed-responsive embed-responsive-16by9">
-							<iframe src="https://player.vimeo.com/video/192896299" width="640" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-						</div>
-						<div class='col-xs-12'>
-							<label class="css-input css-checkbox css-checkbox-sm css-checkbox-primary">
-								<input type="checkbox"><span></span> Don't show me this again
-							</label>
-						</div>
-						<div class="modal-footer">
-							<button id="closetutorial-btn" data-dismiss="modal" class="btn blue lighten-1" type="button">Close</button>
+					<div class="content content-narrow">
+						<div class='row'>
+							<h1 class="font-w700 push">Prizes</h1>
+							<img src="{{ asset('assets/img/competition/prizes-1.png') }}" class="img-responsive" />
+							<img src="{{ asset('assets/img/competition/prizes-2.png') }}" class="img-responsive" />
+							<img src="{{ asset('assets/img/competition/prizes-3.png') }}" class="img-responsive" />
+							<img src="{{ asset('assets/img/competition/prizes-4.png') }}" class="img-responsive" />
+							<div class="modal-footer">
+								<button id="closetutorial-btn" data-dismiss="modal" class="btn blue lighten-1" type="button">Close</button>
+							</div>
 						</div>
 					</div>
+
 				</div>
 			</div>
 			<!-- END Apps Block -->
