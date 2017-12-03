7ded2f753c40a098e194e16be2651c97cf4e847b
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-12-04 02:16:05 +0800

    Lazy Commit!

--- resources/views/competition/index.blade.php
+++ resources/views/competition/index.blade.php
@@ -23,9 +23,9 @@
 	    		</span>
 					<span class="timer font-w700" style="margin-top: 15px; font-size: 18px;"></span>
 					<br/>
-					<span class="font-w700" style="width: 100%; float: left; margin-top: 15px; font-size: 18px;">
-						<span id="hourTimer"></span> hours remaining
-						<span id="minuteTimer"></span> minutes remaining
+					<span class="font-w500" style="width: 100%; float: left; margin-top: 15px; font-size: 18px;">
+						<span id="hourTimer"></span> hours remaining,
+						<span id="minuteTimer"></span> minutes remaining,
 						<span id="secondTimer"></span> seconds remaining
 					</span>
 				</div>
