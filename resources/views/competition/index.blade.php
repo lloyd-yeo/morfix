13c5331361b464dd66cffce39eefdee39462f595
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-12-03 12:26:21 +0800

    Lazy Commit!

--- resources/views/competition/index.blade.php
+++ resources/views/competition/index.blade.php
@@ -222,10 +222,10 @@
 					</div>
 					<div class="block-content block-content-full">
 						<div class="row">
-							<div class="col-xs-6 col-sm-4 col-lg-2">
+							<div class="col-xs-6 col-sm-4 col-lg-4">
 								<a class="block block-link-hover2 text-center" href="javascript:void(0)">
 									<div class="block-content block-content-full bg-primary">
-										<i class="si si-message fa-4x text-white"></i>
+										<i class="si si-docs fa-4x text-white"></i>
 										<div class="font-w600 text-white-op push-15-t">Email Swipes</div>
 									</div>
 								</a>
