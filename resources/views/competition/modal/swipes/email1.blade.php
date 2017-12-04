9629da9b6af9b14ac84c3c115fe6cd48ffa66aba
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-12-04 13:57:12 +0800

    Lazy Commit!

---
+++ resources/views/competition/modal/swipes/email1.blade.php
@@ -0,0 +1,78 @@
+<!-- Apps Modal -->
+<div class="modal fade" id="modal-prizes" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-lg modal-dialog modal-dialog-top">
+		<div class="modal-content">
+			<!-- Apps Block -->
+			<div class="block block-themed block-transparent">
+				<div class="block-header bg-modern">
+					<ul class="block-options">
+						<li>
+							<button data-dismiss="modal" type="button"><i class="si si-close"></i></button>
+						</li>
+					</ul>
+					<h3 class="block-title">Prizes</h3>
+				</div>
+				<div class="block-content">
+					<div class="content content-narrow">
+						<div class='row'>
+							<h1 class="font-w700 push">Email #1 Swipe</h1>
+
+							<div class="nice-copy">
+								<span class="font-w700">Email #1</span>
+								<p>
+									Sample Subject Lines:
+
+									(URGENT) want a free Morfix account?
+
+									watch me gain IG Followers in 10 minutes!
+
+									Morfix Demo (video inside)
+
+									(Login Info) your Morfix account
+
+									Morfix is LIVE
+									Morfix - get a FREE account
+
+									Sample Body Copy:
+
+									There's a new software product that's taking over the internet.  It's called Morfix, and I just hooked you up with a FREE account!
+
+									<< Affiliate Link >>
+
+									This new software will help your Instagram account:
+
+									  - Grow Thousands Of Followers A Month on Automation..
+
+									  - Automated Direct Messages...
+
+									  - Schedule Posts...
+
+									  - Trainings on Instagram Income…
+
+									  - Going Viral...
+
+									  - And a WHOLE LOT MORE!
+
+									Want to see a demo of the software in action? Check it out here:
+
+									<< Affiliate Link >>
+
+									Thanks,
+									<< Your Name>>
+
+									P.S. - After you see the demo video, you can get a 7 day free account, and go setup with your IG Account. You'll be blown away with what's possible:
+
+									<< Affiliate Link >>
+
+								</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<!-- END Apps Block -->
+		</div>
+	</div>
+</div>
+<!-- END Apps Modal -->
---
+++ resources/views/competition/modal/swipes/email2.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lloyd
+ * Date: 4/12/17
+ * Time: 13:55
+ */
