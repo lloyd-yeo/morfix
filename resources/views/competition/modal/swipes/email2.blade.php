fc099904b6883942e9fa01a6d3f63bead1c4f32e
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-12-04 13:57:45 +0800

    Lazy Commit!

--- resources/views/competition/modal/swipes/email2.blade.php
+++ resources/views/competition/modal/swipes/email2.blade.php
@@ -1,7 +1,70 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lloyd
- * Date: 4/12/17
- * Time: 13:55
- */
+<!-- Apps Modal -->
+<div class="modal fade" id="modal-prizes" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-lg modal-dialog modal-dialog-top">
+		<div class="modal-content">
+			<!-- Apps Block -->
+			<div class="block block-themed block-transparent">
+				<div class="block-header bg-modern">
+					<ul class="block-options">
+						<li>
+							<button data-dismiss="modal" type="button"><i class="si si-close"></i></button>
+						</li>
+					</ul>
+					<h3 class="block-title">Prizes</h3>
+				</div>
+				<div class="block-content">
+					<div class="content content-narrow">
+						<div class='row'>
+							<h1 class="font-w700 push">Email #2 Swipe</h1>
+
+							<div class="nice-copy">
+								<span class="font-w700">Email #2</span>
+								<p>
+									Sample Subject Lines:
+
+									Smart Followers Growth... Morfix is AMAZING!
+
+									9 "Growth" Niche templates... free!
+
+									What's the catch?  This is TOO good…
+
+									I just hooked you up - FREE Morfix account!
+
+									Did you see what Mike said?
+
+									Sample Body Copy:
+
+									Over the past 12 months, a new tool called Morfix has taken over the internet.  
+
+									Over 3,000 Instagramers and Influencers have setup their Instagram account with it, and I was able to get you a free 7 day trial!
+
+									<< Affiliate Link >>
+
+									Listen to what Mike Benson said after getting his free trial account:
+
+									“...I never knew growing my followers can be so easy. Morfix has done all the heavy lifting for me. All I need to do is focus now is on giving my audience values through my Instagram and Morfix even has a training on that…” 
+
+									<< Affiliate Link >>
+
+									This account will give you access for ONE full week so you can look under the hood and have some fun with Morfix!
+
+									Thanks,
+									<< Your Name>>
+
+									P.S. - After you see the demo video, you can get a free account, and go setup your IG account. You'll be blown away with what's possible:
+
+									<< Affiliate Link >>
+
+
+								</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<!-- END Apps Block -->
+		</div>
+	</div>
+</div>
+<!-- END Apps Modal -->
