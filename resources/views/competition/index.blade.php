962759d0894c361fdf83c2b0ed830b249d09c1ec
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-12-05 13:22:54 +0800

    Lazy Commit!

--- resources/views/competition/index.blade.php
+++ resources/views/competition/index.blade.php
@@ -347,24 +347,22 @@
 					</div>
 					<div class="block-content block-content-full">
 						<div class="row">
-							<div class="col-lg-6">
+							<div class="col-lg-3">
 								<a href="https://clickfunnels.com/?cf_affiliate_id=768373&amp;affiliate_id=768373">
 									<img src="{{ asset('assets/img/banner/banner1.jpeg') }}" width="300" height="250" class="affIMGURL">
 								</a>
 							</div>
-							<div class="col-lg-6">
+							<div class="col-lg-3">
 								<a href="https://clickfunnels.com/?cf_affiliate_id=768373&amp;affiliate_id=768373">
 									<img src="{{ asset('assets/img/banner/banner2.jpeg') }}" width="300" height="250" class="affIMGURL">
 								</a>
 							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-6">
+							<div class="col-lg-3">
 								<a href="https://clickfunnels.com/?cf_affiliate_id=768373&amp;affiliate_id=768373">
 									<img src="{{ asset('assets/img/banner/banner3.jpeg') }}" width="300" height="250" class="affIMGURL">
 								</a>
 							</div>
-							<div class="col-lg-6">
+							<div class="col-lg-3">
 								<a href="https://clickfunnels.com/?cf_affiliate_id=768373&amp;affiliate_id=768373">
 									<img src="{{ asset('assets/img/banner/banner4.jpeg') }}" width="300" height="250" class="affIMGURL">
 								</a>
