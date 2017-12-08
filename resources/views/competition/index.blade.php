b671dd60bdff82b4d19ecccd841b9344509df104
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-12-08 11:56:14 +0800

    Lazy Commit!

--- resources/views/competition/index.blade.php
+++ resources/views/competition/index.blade.php
@@ -371,6 +371,77 @@
 						</div>
 
 						<div class="row">
+							<div class="block block-themed block-transparent">
+								<div class="block-header bg-modern">
+									<ul class="block-options">
+										<li>
+											<button data-dismiss="modal" type="button"><i class="si si-close"></i></button>
+										</li>
+									</ul>
+									<h3 class="block-title">Prizes</h3>
+								</div>
+								<div class="block-content">
+									<div class="content content-narrow">
+										<div class='row'>
+											<h1 class="font-w700 push">Email #1 Swipe</h1>
+
+											<div class="nice-copy">
+												<span class="font-w700">Email #1</span>
+												<p>
+													Sample Subject Lines:
+													<br/><br/>
+													(URGENT) want a free Morfix account?
+													<br/><br/>
+													watch me gain IG Followers in 10 minutes!
+													<br/><br/>
+													Morfix Demo (video inside)<br/><br/>
+
+													(Login Info) your Morfix account<br/><br/>
+
+													Morfix is LIVE<br/>
+													Morfix - get a FREE account<br/><br/>
+
+													Sample Body Copy:<br/><br/>
+
+													There's a new software product that's taking over the internet.  It's called Morfix, and I just hooked you up with a FREE account!<br/><br/>
+
+													<< Affiliate Link >><br/><br/>
+
+													This new software will help your Instagram account:<br/><br/>
+
+													  - Grow Thousands Of Followers A Month on Automation..<br/>
+
+													  - Automated Direct Messages...<br/>
+
+													  - Schedule Posts...<br/>
+
+													  - Trainings on Instagram Income…<br/>
+
+													  - Going Viral...<br/>
+
+													  - And a WHOLE LOT MORE!<br/><br/>
+
+													Want to see a demo of the software in action? Check it out here:<br/><br/>
+
+													<< Affiliate Link >><br/><br/>
+
+													Thanks,<br/>
+													<< Your Name>><br/><br/>
+
+													P.S. - After you see the demo video, you can get a 7 day free account, and go setup with your IG Account. You'll be blown away with what's possible:<br/><br/>
+
+													<< Affiliate Link >>
+
+												</p>
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
 							<div class="col-xs-6 col-sm-4 col-lg-4">
 								<a class="block block-link-hover2 text-center"
 								   href="#!"
