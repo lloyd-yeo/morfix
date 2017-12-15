04f10efc4faba8f794fa14e038f1424d3ff2758b
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-12-15 16:51:03 +0800

    Lazy Commit!

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -106,7 +106,12 @@ class PaymentController extends Controller
 		Braintree_Configuration::publicKey('vtq3w9x62s57p82y');
 		Braintree_Configuration::privateKey('c578012b2eb171582133ed0372f3a2ae');
 
-		$client_token = Braintree_ClientToken::generate();
+		$client_token = NULL;
+		if (Auth::user()->braintree_id != NULL) {
+			$client_token = Braintree_ClientToken::generate(['customerId' => Auth::user()->braintree_id]);
+		} else {
+			$client_token = Braintree_ClientToken::generate();
+		}
 
 		return view('payment.upgrade.mastermind', [ 'client_token' => $client_token ]);
 
