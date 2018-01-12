be9dbb9af677a0bb9e4ee6a7aef8462c4306dfb1
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-01-12 13:02:12 +0800

    Lazy Commit!

--- app/Console/Commands/UpdatePendingCommission.php
+++ app/Console/Commands/UpdatePendingCommission.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\StripeCharge;
 use App\StripeDetail;
+use App\StripeInvoice;
 use App\UserAffiliates;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -10,41 +12,48 @@ use App\User;
 
 class UpdatePendingCommission extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'commission:updatepending';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'commission:updatepending {email?}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Update pending commission of users.';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Update pending commission of users.';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-    	$users = User::where('tier', '>', 2)->where('last_pay_out_date', '2017-12-25 00:00:00')->get();
-    	$this->alert($users->count() . " users");
-    	foreach ($users as $user) {
-    		$pending_comms = 0;
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$start_date = '2017-12-01 00:00:00';
+		$end_date = '2017-12-31 00:00:00';
+		$users = User::where('tier', '>', 2)->where('last_pay_out_date', '2017-12-25 00:00:00');
+		if ($this->argument('email') != NULL) {
+			$users = $users->where('email', $this->argument('email'));
+		}
+		$users = $users->get();
+
+		$this->alert($users->count() . " users");
+		foreach ($users as $user) {
+			$pending_comms = 0;
 			$this->line($user->email);
 
 			$user_affiliates = UserAffiliates::where('referrer', $user->user_id);
@@ -54,57 +63,98 @@ class UpdatePendingCommission extends Command
 				if ($affiliate == NULL) {
 					continue;
 				} else {
-					$stripe_details = StripeDetail::where('email', $affiliate->email)->first();
-					if ($stripe_details != NULL) {
-						
+					$stripe_details = StripeDetail::where('email', $affiliate->email)->get();
+					if ($stripe_details->count() > 0) {
+						foreach ($stripe_details as $stripe_detail) {
+							$stripe_id = $stripe_detail->stripe_id;
+
+							$stripe_invoices = StripeInvoice::where('stripe_id', $stripe_id)
+							                                ->where('invoice_date', '>=', $start_date)
+							                                ->where('invoice_date', '<=', $end_date)
+							                                ->where('paid', 1)
+							                                ->get();
+
+							foreach ($stripe_invoices as $stripe_invoice) {
+								$stripe_charge = StripeCharge::where('charge_id', $stripe_invoice->charge_id)
+								            ->where('invoice_id', $stripe_invoice->invoice_id)
+								            ->where('refunded', 0)
+								            ->first();
+
+								if ($stripe_charge != NULL) {
+									switch ($stripe_invoice->subscription_id) {
+										case '0137':
+											$pending_comms += 20;
+											break;
+										case '0297':
+											$pending_comms += 50;
+											break;
+										case 'MX370':
+											$pending_comms += 200;
+											break;
+										case 'MX297':
+											$pending_comms += 120;
+											break;
+										case '0167':
+											break;
+										default:
+											break;
+									}
+								}
+							}
+						}
 					} else {
 
+						if ($affiliate->braintree_id != NULL) {
+							//retrieve braintree stuff here
+						} else {
+
+						}
 					}
 				}
 			}
-	    }
+		}
 
-//        $path = storage_path('app/may-payout-final.csv');
-//        $file = fopen($path, "r");
-//
-//        $current_email = "";
-//        $current_comms = 0;
-//
-//        while (($data = fgetcsv($file, 0, ",")) !== FALSE) {
-//
-//            if ($data[4] == "Yes") {
-//
-//                if ($current_email != $data[0]) {
-//
-//                    if ($current_email != "") {
-//                        $user = User::where('email', $current_email)->first();
-//                        if ($user !== NULL) {
-//                            $user->pending_commission = $current_comms;
-//                            if($user->save()) {
-//                                echo "Updated [$current_email] pending comms to [$current_comms]\n";
-//                            }
-//                        }
-//                    }
-//
-//                    $current_email = $data[0];
-//                    $current_comms = 0;
-//                }
-//
-//                $referral_charges = DB::select('SELECT * FROM get_referral_charges_of_user WHERE referrer_email = ? AND '
-//                        . 'charge_created >= "2017-06-01 00:00:00" AND charge_created <= "2017-06-31 23:59:59" AND charge_refunded = 0 '
-//                        . 'ORDER BY charge_created DESC;', [$data[0]]);
-//
-//                foreach ($referral_charges as $referral_charge) {
-//
-//                    if ($referral_charge->subscription_id == "0137") {
-//                        $current_comms = $current_comms + 20;
-//                    } else if ($referral_charge->subscription_id == "0297") {
-//                        $current_comms = $current_comms + 50;
-//                    }
-//
-//                    echo $referral_charge->referrer_email . "\t" . $referral_charge->referred_email . "\t"  . $referral_charge->subscription_id . " [$current_comms] \n";
-//                }
-//            }
-//        }
-    }
+		//        $path = storage_path('app/may-payout-final.csv');
+		//        $file = fopen($path, "r");
+		//
+		//        $current_email = "";
+		//        $current_comms = 0;
+		//
+		//        while (($data = fgetcsv($file, 0, ",")) !== FALSE) {
+		//
+		//            if ($data[4] == "Yes") {
+		//
+		//                if ($current_email != $data[0]) {
+		//
+		//                    if ($current_email != "") {
+		//                        $user = User::where('email', $current_email)->first();
+		//                        if ($user !== NULL) {
+		//                            $user->pending_commission = $current_comms;
+		//                            if($user->save()) {
+		//                                echo "Updated [$current_email] pending comms to [$current_comms]\n";
+		//                            }
+		//                        }
+		//                    }
+		//
+		//                    $current_email = $data[0];
+		//                    $current_comms = 0;
+		//                }
+		//
+		//                $referral_charges = DB::select('SELECT * FROM get_referral_charges_of_user WHERE referrer_email = ? AND '
+		//                        . 'charge_created >= "2017-06-01 00:00:00" AND charge_created <= "2017-06-31 23:59:59" AND charge_refunded = 0 '
+		//                        . 'ORDER BY charge_created DESC;', [$data[0]]);
+		//
+		//                foreach ($referral_charges as $referral_charge) {
+		//
+		//                    if ($referral_charge->subscription_id == "0137") {
+		//                        $current_comms = $current_comms + 20;
+		//                    } else if ($referral_charge->subscription_id == "0297") {
+		//                        $current_comms = $current_comms + 50;
+		//                    }
+		//
+		//                    echo $referral_charge->referrer_email . "\t" . $referral_charge->referred_email . "\t"  . $referral_charge->subscription_id . " [$current_comms] \n";
+		//                }
+		//            }
+		//        }
+	}
 }
