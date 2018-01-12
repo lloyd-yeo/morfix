adb7e8b1a2a84a6aee798162606538fe1b19030f
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-01-12 14:14:50 +0800

    Lazy Commit!

--- app/Console/Commands/UpdatePendingCommission.php
+++ app/Console/Commands/UpdatePendingCommission.php
@@ -87,16 +87,29 @@ class UpdatePendingCommission extends Command
 									dump($stripe_charge);
 									switch ($stripe_invoice->subscription_id) {
 										case '0137':
-											$pending_comms += 20;
+											if ($user->tier > 1) {
+												$pending_comms += 20;
+											}
 											break;
 										case '0297':
-											$pending_comms += 50;
+											if ($user->tier > 10) {
+												$pending_comms += 50;
+											}
 											break;
 										case 'MX370':
-											$pending_comms += 200;
+											if ($user->tier > 2 && $user->tier != 12 && $user->tier != 22) {
+												$pending_comms += 200;
+											}
 											break;
 										case 'MX297':
-											$pending_comms += 120;
+											if ($user->tier > 2 && $user->tier != 12 && $user->tier != 22) {
+												$pending_comms += 120;
+											}
+											break;
+										case 'MX970':
+											if ($user->tier > 20) {
+												$pending_comms += 500;
+											}
 											break;
 										case '0167':
 											break;
@@ -120,7 +133,7 @@ class UpdatePendingCommission extends Command
 
 						foreach ($braintree_transactions as $braintree_transaction) {
 							//check if there is refund for transaction
-//							$this->alert($braintree_transaction->sub_id);
+							//							$this->alert($braintree_transaction->sub_id);
 
 							$braintree_transactions_cancelled = BraintreeTransaction::where('sub_id', $braintree_transaction->sub_id)
 							                                                        ->where('created_at', '>=', $start_date)
@@ -134,25 +147,41 @@ class UpdatePendingCommission extends Command
 								                                                        ->where('created_at', '<=', $end_date)
 								                                                        ->where('type', 'sale')
 								                                                        ->first();
+
 								$this->alert($braintree_transactions_completed->sub_id . " " . $braintree_transactions_completed->plan_id);
+
 								switch ($braintree_transactions_completed->plan_id) {
 									case '0137':
-										$pending_comms += 20;
+										if ($user->tier > 1) {
+											$pending_comms += 20;
+										}
 										break;
 									case '0297':
-										$pending_comms += 50;
+										if ($user->tier > 10) {
+											$pending_comms += 50;
+										}
 										break;
 									case 'MX370':
-										$pending_comms += 200;
+										if ($user->tier > 2 && $user->tier != 12 && $user->tier != 22) {
+											$pending_comms += 200;
+										}
 										break;
 									case 'MX297':
-										$pending_comms += 120;
+										if ($user->tier > 2 && $user->tier != 12 && $user->tier != 22) {
+											$pending_comms += 120;
+										}
+										break;
+									case 'MX970':
+										if ($user->tier > 20) {
+											$pending_comms += 500;
+										}
 										break;
 									case '0167':
 										break;
 									default:
 										break;
 								}
+
 							}
 						}
 					} else {
