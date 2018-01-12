8e07adf8a10965d8608b593baa24d0a6c88a8c34
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-01-12 14:00:04 +0800

    Lazy Commit!

--- app/Console/Commands/UpdatePendingCommission.php
+++ app/Console/Commands/UpdatePendingCommission.php
@@ -110,7 +110,8 @@ class UpdatePendingCommission extends Command
 
 					if ($affiliate->braintree_id != NULL) {
 						//retrieve braintree stuff here
-						$braintree_transactions = BraintreeTransaction::where('braintree_id', $affiliate->braintree_id)
+						$braintree_transactions = BraintreeTransaction::select('id', 'status', 'type', 'amount', 'created_at', 'sub_id', 'braintree_id', 'plan_id')
+						                                              ->where('braintree_id', $affiliate->braintree_id)
 						                                              ->where('created_at', '>=', $start_date)
 						                                              ->where('created_at', '<=', $end_date)
 						                                              ->where('status', '!=', 'processor_declined')
