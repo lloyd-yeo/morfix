9d5e5609b6e35ba850250010760262a9d6c64e86
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-01-12 14:21:02 +0800

    Lazy Commit!

--- app/Console/Commands/UpdatePendingCommissionPayable.php
+++ app/Console/Commands/UpdatePendingCommissionPayable.php
@@ -14,14 +14,14 @@ class UpdatePendingCommissionPayable extends Command {
      *
      * @var string
      */
-    protected $signature = 'update:payable';
+    protected $signature = 'commission:updatependingpayable {email?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Update the payable comission for users paid in a certain excel sheet.';
+    protected $description = 'Update the payable comission for users paid.';
 
     /**
      * Create a new command instance.
@@ -38,57 +38,164 @@ class UpdatePendingCommissionPayable extends Command {
      * @return mixed
      */
     public function handle() {
-        $path = storage_path('app/june-updates.csv');
-        $file = fopen($path, "r");
-        $all_data = array();
-        
-        while (($data = fgetcsv($file, 200, ",")) !== FALSE) {
-            
-            if ($data[3] == "Yes") {
-                echo $data[0] . "\t" . $data[3] . "\n";
-                $user = User::where('email', trim($data[0]))->first();
-                if ($user !== NULL) {
-                    
-                    //UPDATE LAST PAID
-                    $user->last_pay_out_date = "2017-07-25 00:00:00";
-                    $user->save();
-                    
-                    //UPDATE ALL_TIME_COMMISSION
-                    $comms = $data[2];
-                    $user->all_time_commission = $user->all_time_commission + $comms;
-                    $user->save();
-                    
-                    $user->pending_commission = $user->pending_commission - $comms;
-                    $user->save();
-                    
-                    $user->pending_commission_payable = $user->pending_commission;
-                    $user->save();
-                }
-                
-            }
-            
-            
-//            echo $data[0] . "\n";
-//            $user = User::where('email', trim($data[0]))->first();
-//            $current_month = \Carbon\Carbon::now()->startOfMonth();
-//            echo $user->user_id . "\t" . $user->name . "\t$current_month\n";
-//            
-//            $referrals = DB::select('SELECT * FROM insta_affiliate.get_referral_charges_of_user WHERE referrer_email = ?', [$user->email]);
-//            
-//            foreach ($referrals as $referral) {
-//                $this->line($referral->charge_id . "\t" . $referral->invoice_id);
-//                $charges = StripeCharge::where('charge_id', $referral->charge_id)->where('charge_created', '<', $current_month)->get();
-//                foreach ($charges as $charge) {
-//                    $charge->commission_given = 1;  
-//                    if ($charge->save()) {
-//                        $this->line("Updated [" . $charge->charge_id . "] for [" . $user->email . "]");
-//                    }
-//                }
-//            }
-            
-//            $user->pending_commission_payable = 0;
-//            $user->save();
-        }
+	    $start_date = '2017-12-01 00:00:00';
+	    $end_date   = '2017-12-31 00:00:00';
+	    $users      = User::where('tier', '>=', 2)->where('last_pay_out_date', '2017-12-25 00:00:00');
+	    if ($this->argument('email') != NULL) {
+		    $users = $users->where('email', $this->argument('email'));
+	    }
+	    $users = $users->get();
+
+	    $this->alert($users->count() . " users");
+
+	    foreach ($users as $user) {
+		    $pending_comms = 0;
+		    $this->line($user->email);
+
+		    $user_affiliates = UserAffiliates::where('referrer', $user->user_id)->get();
+
+		    foreach ($user_affiliates as $user_affiliate) {
+			    $affiliate = User::find($user_affiliate->referred);
+			    if ($affiliate == NULL) {
+				    continue;
+			    } else {
+				    $stripe_details = StripeDetail::where('email', $affiliate->email)->get();
+				    if ($stripe_details->count() > 0) {
+					    foreach ($stripe_details as $stripe_detail) {
+						    $stripe_id = $stripe_detail->stripe_id;
+
+						    $stripe_invoices = StripeInvoice::where('stripe_id', $stripe_id)
+						                                    ->where('invoice_date', '>=', $start_date)
+						                                    ->where('invoice_date', '<=', $end_date)
+						                                    ->where('paid', 1)
+						                                    ->get();
+
+						    foreach ($stripe_invoices as $stripe_invoice) {
+							    $stripe_charge = StripeCharge::where('charge_id', $stripe_invoice->charge_id)
+							                                 ->where('invoice_id', $stripe_invoice->invoice_id)
+							                                 ->where('paid', 1)
+							                                 ->where('refunded', 0)
+							                                 ->first();
+
+							    if ($stripe_charge != NULL) {
+								    switch ($stripe_invoice->subscription_id) {
+									    case '0137':
+										    if ($user->tier > 1) {
+											    $pending_comms += 20;
+											    $this->line("[STRIPE] [COMMS] Added!");
+										    }
+										    break;
+									    case '0297':
+										    if ($user->tier > 10) {
+											    $pending_comms += 50;
+											    $this->line("[STRIPE] [COMMS] Added!");
+										    }
+										    break;
+									    case 'MX370':
+										    if ($user->tier > 2 && $user->tier != 12 && $user->tier != 22) {
+											    $pending_comms += 200;
+											    $this->line("[STRIPE] [COMMS] Added!");
+										    }
+										    break;
+									    case 'MX297':
+										    if ($user->tier > 2 && $user->tier != 12 && $user->tier != 22) {
+											    $pending_comms += 120;
+											    $this->line("[STRIPE] [COMMS] Added!");
+										    }
+										    break;
+									    case 'MX970':
+										    if ($user->tier > 20) {
+											    $pending_comms += 500;
+											    $this->line("[STRIPE] [COMMS] Added!");
+										    }
+										    break;
+									    case '0167':
+										    break;
+									    default:
+										    break;
+								    }
+							    }
+						    }
+					    }
+				    }
+
+				    if ($affiliate->braintree_id != NULL) {
+					    //retrieve braintree stuff here
+					    $braintree_transactions = BraintreeTransaction::select('sub_id')
+					                                                  ->distinct()
+					                                                  ->where('braintree_id', $affiliate->braintree_id)
+					                                                  ->where('created_at', '>=', $start_date)
+					                                                  ->where('created_at', '<=', $end_date)
+					                                                  ->where('status', '!=', 'processor_declined')
+					                                                  ->get();
+
+					    foreach ($braintree_transactions as $braintree_transaction) {
+						    //check if there is refund for transaction
+						    //							$this->alert($braintree_transaction->sub_id);
+
+						    $braintree_transactions_cancelled = BraintreeTransaction::where('sub_id', $braintree_transaction->sub_id)
+						                                                            ->where('created_at', '>=', $start_date)
+						                                                            ->where('created_at', '<=', $end_date)
+						                                                            ->where('type', 'credit')
+						                                                            ->first();
+						    if ($braintree_transactions_cancelled == NULL) {
+
+							    $braintree_transactions_completed = BraintreeTransaction::where('sub_id', $braintree_transaction->sub_id)
+							                                                            ->where('created_at', '>=', $start_date)
+							                                                            ->where('created_at', '<=', $end_date)
+							                                                            ->where('type', 'sale')
+							                                                            ->first();
+
+							    $this->alert($braintree_transactions_completed->sub_id . " " . $braintree_transactions_completed->plan_id);
+
+							    switch ($braintree_transactions_completed->plan_id) {
+								    case '0137':
+									    if ($user->tier > 1) {
+										    $pending_comms += 20;
+										    $this->line("[BRAINTREE] [COMMS] Added!");
+									    }
+									    break;
+								    case '0297':
+									    if ($user->tier > 10) {
+										    $pending_comms += 50;
+										    $this->line("[BRAINTREE] [COMMS] Added!");
+									    }
+									    break;
+								    case 'MX370':
+									    if ($user->tier > 2 && $user->tier != 12 && $user->tier != 22) {
+										    $pending_comms += 200;
+										    $this->line("[BRAINTREE] [COMMS] Added!");
+									    }
+									    break;
+								    case 'MX297':
+									    if ($user->tier > 2 && $user->tier != 12 && $user->tier != 22) {
+										    $pending_comms += 120;
+										    $this->line("[BRAINTREE] [COMMS] Added!");
+									    }
+									    break;
+								    case 'MX970':
+									    if ($user->tier > 20) {
+										    $pending_comms += 500;
+										    $this->line("[BRAINTREE] [COMMS] Added!");
+									    }
+									    break;
+								    case '0167':
+									    break;
+								    default:
+									    break;
+							    }
+
+						    }
+					    }
+				    } else {
+
+				    }
+
+			    }
+		    }
+
+		    $this->alert('[COMMISSIONS] ' . $user->email . ' [' . $pending_comms . ']');
+	    }
     }
 
 }
