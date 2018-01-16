92d4e27bebe0497439f81ba81d40bd16629fc5ea
Junril Pateno <junril090693@gmail.com>
2018-01-16 16:53:05 +0800

    cancelation

--- routes/web.php
+++ routes/web.php
@@ -100,8 +100,8 @@ Route::post('/settings/subscription/cancel/{subscription_id}', 'SettingsControll
 Route::post('/settings/invoice/pay/{invoice_id}', 'SettingsController@attemptInvoice');
 
 Route::post('/settings/cards/update', 'SettingsController@updateCreditCard');
-
-Route::get('/cancel-subscription', 'SettingsController@CancelSubscriptionToDefault');
+Route::post('/settings/address/update', 'SettingsController@updateAddressCard');
+Route::post('/cancel-subscription', 'SettingsController@CancelSubscriptionToDefault');
 
 #Route::get('/faq', 'FaqController@index');
 Route::get('/faq', 'FaqController@topic');
