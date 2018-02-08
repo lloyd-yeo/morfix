1e1399a6719b72036810d64c7094fa31003e1497
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-02-09 01:57:45 +0800

    Lazy Commit!

--- app/Console/Commands/ManualLogin.php
+++ app/Console/Commands/ManualLogin.php
@@ -12,6 +12,7 @@ use App\InstagramProfile;
 use App\CreateInstagramProfileLog;
 use App\Proxy;
 use App\DmJob;
+use App\User;
 use App\InstagramHelper;
 
 class ManualLogin extends Command {
@@ -63,8 +64,61 @@ class ManualLogin extends Command {
         $this->line($ig_username . " " . $ig_password);
 
         try {
-            dump($instagram->login($ig_username, $ig_password));
-            dump($instagram->timeline->getSelfUserFeed());
+	        $profile_log = CreateInstagramProfileLog::where('log_id', $add_profile_request->create_profile_log_id)->first();
+	        $user = User::where('email', $profile_log->email)->first();
+	        $explorer_response = $instagram->login($ig_username, $ig_password);
+
+	        if ($explorer_response !== NULL) {
+		        $profile_log->error_msg = $explorer_response->asJson();
+		        $profile_log->save();
+
+		        $explorer_response_as_assoc_array = $explorer_response->asJson(1);
+		        if ($explorer_response_as_assoc_array["status"] == "fail") {
+			        if (array_key_exists("two_factory_required", $explorer_response_as_assoc_array)) {
+				        if ($explorer_response_as_assoc_array["two_factory_required"]) {
+					        return Response::json([ "success" => FALSE, 'type' => 'endpoint', 'response' => "Account is protected with 2FA, unable to establish connection." ]);
+				        }
+			        }
+		        }
+	        }
+
+	        $morfix_ig_profile                 = new InstagramProfile();
+	        $morfix_ig_profile->user_id        = $user->user_id;
+	        $morfix_ig_profile->email          = $user->email;
+	        $morfix_ig_profile->insta_username = $ig_username;
+	        $morfix_ig_profile->insta_pw       = $ig_password;
+
+	        $user_response  = $instagram->people->getInfoByName($ig_username);
+	        $instagram_user = $user_response->getUser();
+
+	        $profile_log->error_msg = "Profile successfully created.";
+	        $profile_log->save();
+
+	        $user->last_used_proxy = NULL;
+	        $user->save();
+
+	        $morfix_ig_profile->profile_pic_url = $instagram_user->getProfilePicUrl();
+	        $morfix_ig_profile->save();
+
+
+	        DB::update("UPDATE user_insta_profile SET updated_at = NOW(), follower_count = ?, num_posts = ?, insta_user_id = ? WHERE insta_username = ?;",
+		        [ $instagram_user->getFollowerCount(), $instagram_user->getMediaCount(), $instagram_user->getPk(), $ig_username ]);
+
+	        $items = $instagram->timeline->getSelfUserFeed()->getItems();
+
+	        foreach ($items as $item) {
+		        try {
+			        DB::insert("INSERT IGNORE INTO user_insta_profile_media (insta_username, media_id, image_url) 
+				VALUES (?,?,?);", [ $ig_username, $item->getPk(), $item->getImageVersions2()->getCandidates()[0]->getUrl() ]);
+		        }
+		        catch (\ErrorException $e) {
+			        break;
+		        }
+	        }
+
+	        $add_profile_request->working_on = 5;
+	        $add_profile_request->save();
+
         } catch (\InstagramAPI\Exception\ChallengeRequiredException $challenge_required_ex) {
 
         	$challenge_url = $challenge_required_ex->getResponse()->asArray()["challenge"]["url"];
