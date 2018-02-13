342efe3e8e9b8b99ed896822fbea88f5fa55f7ac
Lim Choon Rong <clim046@e.ntu.edu.sg>
2018-02-13 20:39:07 +0800

    Just set the tier in intercom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,39 +115,39 @@
         <!-- END Page Container -->
         @include('layouts.modal')
         @include('payment.modal')
-        <script>
-            var $user_plan = "";
-            switch("{{ Auth::user()->tier }}")
-                case("1")
-                    $user_plan = "Free Trial";
-                    break
-                case(2)
-                    $user_plan = "Premium";
-                    break
-                case(3)
-                    $user_plan = "Pro";
-                    break
-                case(12)
-                    $user_plan = "Business";
-                    break
-                case(13)
-                    $user_plan = "Pro and Business";
-                    break
-                case(22)
-                    $user_plan = "Mastermind and Premium";
-                    break
-                case(23)
-                    $user_plan = "Mastermind and Pro";
-                    break
-            endswitch
-        </script>
+<!--        <script>-->
+<!--            var $user_plan = "";-->
+<!--            switch("{{ Auth::user()->tier }}")-->
+<!--                case("1")-->
+<!--                    $user_plan = "Free Trial";-->
+<!--                    break-->
+<!--                case(2)-->
+<!--                    $user_plan = "Premium";-->
+<!--                    break-->
+<!--                case(3)-->
+<!--                    $user_plan = "Pro";-->
+<!--                    break-->
+<!--                case(12)-->
+<!--                    $user_plan = "Business";-->
+<!--                    break-->
+<!--                case(13)-->
+<!--                    $user_plan = "Pro and Business";-->
+<!--                    break-->
+<!--                case(22)-->
+<!--                    $user_plan = "Mastermind and Premium";-->
+<!--                    break-->
+<!--                case(23)-->
+<!--                    $user_plan = "Mastermind and Pro";-->
+<!--                    break-->
+<!--            endswitch-->
+<!--        </script>-->
 <!--        <script type="text/javascript">$crisp=[];CRISP_WEBSITE_ID="44a88397-d7f6-461d-b8e2-d2133395ac5c";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>-->
         <script>
             window.intercomSettings = {
                 app_id: "z0e5hzb8",
                 name: "{{ Auth::user()->name }}", // Full name
                 email: "{{ Auth::user()->email }}", // Email address
-                user_tier:"{{ $user_plan }}" // User Tier
+                user_tier:"{{ Auth::user()->tier }}" // User Tier
             };
         </script>
         <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/z0e5hzb8';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
