1a5d09f45982592fd40d65f3c7c3a4b8eaa60b68
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-03-13 16:23:23 +0800

    Lazy Commit!

--- app/Http/Controllers/CancellationSubscriptionWebhook.php
+++ app/Http/Controllers/CancellationSubscriptionWebhook.php
@@ -18,6 +18,11 @@ class CancellationSubscriptionWebhook extends Controller
 	 */
 	public function handleDisputeOpened(Request $request)
 	{
+		Braintree_Configuration::environment('production');
+		Braintree_Configuration::merchantId('4x5qk4ggmgf9t5vw');
+		Braintree_Configuration::publicKey('vtq3w9x62s57p82y');
+		Braintree_Configuration::privateKey('c578012b2eb171582133ed0372f3a2ae');
+
 		$notification = WebhookNotification::parse($request->bt_signature, $request->bt_payload);
 
 		Log::info($notification);
