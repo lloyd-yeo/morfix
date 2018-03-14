7fe45cf3b09b2e655b452400e16923fca2b67af1
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-03-14 13:35:34 +0800

    Lazy Commit!

--- resources/views/dashboard/update.blade.php
+++ resources/views/dashboard/update.blade.php
@@ -1,18 +1,19 @@
-{{--<li>--}}
-	{{--<div class="list-timeline-time">{{ Carbon\Carbon::parse($update->created_at)->diffForHumans() }}</div>--}}
+<li>
+{{--	<div class="list-timeline-time">{{ Carbon\Carbon::parse($update->created_at)->diffForHumans() }}</div>--}}
+	<div class="list-timeline-time">{{ $update->created_at }}</div>
 
-	{{--@if ($update->type == "NEW_REFERRAL")--}}
-		{{--<i class="fa fa-thumbs-o-up list-timeline-icon bg-info"></i>--}}
-	{{--@elseif ($update->type == "PRO_OTO_UPSELL")--}}
-		{{--<i class="fa fa-level-up list-timeline-icon bg-info"></i>--}}
-	{{--@elseif ($update->type == "SYSTEM_UPGRADE")--}}
-		{{--<i class="fa fa-cog list-timeline-icon bg-danger"></i>--}}
-	{{--@else--}}
-		{{--<i class="fa fa-coffee list-timeline-icon bg-info"></i>--}}
-	{{--@endif--}}
+	@if ($update->type == "NEW_REFERRAL")
+		<i class="fa fa-thumbs-o-up list-timeline-icon bg-info"></i>
+	@elseif ($update->type == "PRO_OTO_UPSELL")
+		<i class="fa fa-level-up list-timeline-icon bg-info"></i>
+	@elseif ($update->type == "SYSTEM_UPGRADE")
+		<i class="fa fa-cog list-timeline-icon bg-danger"></i>
+	@else
+		<i class="fa fa-coffee list-timeline-icon bg-info"></i>
+	@endif
 
-	{{--<div class="list-timeline-content">--}}
-		{{--<p class="font-w600">{{ $update->title }}</p>--}}
-		{{--<p class="font-s13">{{ $update->content }}</p>--}}
-	{{--</div>--}}
-{{--</li>--}}
+	<div class="list-timeline-content">
+		<p class="font-w600">{{ $update->title }}</p>
+		<p class="font-s13">{{ $update->content }}</p>
+	</div>
+</li>
