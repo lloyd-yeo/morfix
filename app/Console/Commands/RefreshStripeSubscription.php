6ecd2ef5bcf086822e73587b65e002647702965d
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-03-30 22:08:03 +0800

    Lazy Commit!

--- app/Console/Commands/RefreshStripeSubscription.php
+++ app/Console/Commands/RefreshStripeSubscription.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\StripeDetail;
+use App\User;
 use Illuminate\Console\Command;
 use App\StripeActiveSubscription;
 
@@ -39,6 +41,7 @@ class RefreshStripeSubscription extends Command {
         \Stripe\Stripe::setApiKey("sk_live_gnfRoHfQNhreT79YP9b4mIoB");
         $subscriptions = \Stripe\Subscription::all(array('limit' => 100, 'status' => 'all'));
 
+        //Update our DB with all the active Stripe subscription
         foreach ($subscriptions->autoPagingIterator() as $subscription) {
             $active_sub = new StripeActiveSubscription;
             $active_sub->stripe_id = $subscription->customer;
@@ -55,6 +58,106 @@ class RefreshStripeSubscription extends Command {
             }
         }
 
+	    //Get all canceled subscriptions
+        $canceled_subscriptions = StripeActiveSubscription::where('status', 'canceled')->get();
+		foreach ($canceled_subscriptions as $canceled_subscription) {
+			$stripe_id = $canceled_subscription->stripe_id;
+			$user_stripe_detail = StripeDetail::where('stripe_id', $stripe_id)->first();
+			if ($user_stripe_detail != NULL) {
+				$user = User::where('email', $user_stripe_detail->email)->first();
+				if ($user != NULL) {
+					if ($user->paypal = 0 && $user->braintree_id == NULL) {
+						$user->tier = 1;
+						$user->save();
+					}
+				}
+			}
+		}
+
+	    //Get all unpaid subscriptions
+	    $unpaid_subscriptions = StripeActiveSubscription::where('status', 'unpaid')->get();
+	    foreach ($unpaid_subscriptions as $canceled_subscription) {
+		    $stripe_id = $canceled_subscription->stripe_id;
+		    $user_stripe_detail = StripeDetail::where('stripe_id', $stripe_id)->first();
+		    if ($user_stripe_detail != NULL) {
+			    $user = User::where('email', $user_stripe_detail->email)->first();
+			    if ($user != NULL) {
+				    if ($user->paypal = 0 && $user->braintree_id == NULL) {
+					    $user->tier = 1;
+					    $user->save();
+				    }
+			    }
+		    }
+	    }
+
+	    //Get all past_due subscriptions
+	    $past_due_subscriptions = StripeActiveSubscription::where('status', 'past_due')->get();
+	    foreach ($past_due_subscriptions as $canceled_subscription) {
+		    $stripe_id = $canceled_subscription->stripe_id;
+		    $user_stripe_detail = StripeDetail::where('stripe_id', $stripe_id)->first();
+		    if ($user_stripe_detail != NULL) {
+			    $user = User::where('email', $user_stripe_detail->email)->first();
+			    if ($user != NULL) {
+				    if ($user->paypal = 0 && $user->braintree_id == NULL) {
+					    $user->tier = 1;
+					    $user->save();
+				    }
+			    }
+		    }
+	    }
+
+	    //Get all active subscriptions
+	    $active_subscriptions = StripeActiveSubscription::where('status', 'active')->get();
+	    foreach ($active_subscriptions as $active_subscription) {
+		    $stripe_id = $active_subscription->stripe_id;
+		    $user_stripe_detail = StripeDetail::where('stripe_id', $stripe_id)->first();
+		    if ($user_stripe_detail != NULL) {
+			    $user = User::where('email', $user_stripe_detail->email)->first();
+			    if ($user != NULL) {
+//				    if ($user->paypal = 0 && $user->braintree_id == NULL) {
+					    $user->tier = 1;
+					    $user->save();
+//				    }
+			    }
+		    }
+	    }
+
+	    foreach ($active_subscriptions as $active_subscription) {
+		    $stripe_id = $active_subscription->stripe_id;
+		    $user_stripe_detail = StripeDetail::where('stripe_id', $stripe_id)->first();
+		    if ($user_stripe_detail != NULL) {
+			    $user = User::where('email', $user_stripe_detail->email)->first();
+			    if ($user != NULL) {
+			    	switch ($active_subscription->subscription_id) {
+					    case '0137':
+						    $user->tier = $user->tier + 1;
+						    break;
+					    case '0167':
+						    $user->tier = 12;
+						    break;
+					    case '0197':
+						    $user->tier = 12;
+						    break;
+					    case '0297':
+						    $user->tier = $user->tier + 10;
+						    break;
+					    case 'MX370':
+						    $user->tier = $user->tier + 2;
+						    break;
+					    case 'MX970':
+						    $user->tier = $user->tier + 20;
+						    break;
+					    case 'MX297':
+						    $user->tier = $user->tier + 2;
+						    break;
+					    case '0247':
+						    break;
+					    default:
+						    break;
+				    }
+			    }
+		    }
+	    }
     }
 
 }
