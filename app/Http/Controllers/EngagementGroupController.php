7f62b8aea60f8b8f13fc1df10f9c1ce976f21d10
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-04-05 02:34:59 +0800

    Lazy Commit!

--- app/Http/Controllers/EngagementGroupController.php
+++ app/Http/Controllers/EngagementGroupController.php
@@ -12,116 +12,127 @@ use App\InstagramProfile;
 use App\InstagramProfileMedia;
 use Log;
 
-class EngagementGroupController extends Controller {
+class EngagementGroupController extends Controller
+{
 
-    public function __construct() {
-        $this->middleware('auth');
-    }
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index() {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
 
-    	Log::info("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " is accessing engagement group index page. Loading up pictures...");
+		Log::info("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " is accessing engagement group index page. Loading up pictures...");
 
-        $instagram_profiles = InstagramProfile::where('email', Auth::user()->email)
-                ->take(Auth::user()->num_acct)
-                ->get();
+		$instagram_profiles = InstagramProfile::where('email', Auth::user()->email)
+		                                      ->take(Auth::user()->num_acct)
+		                                      ->get();
 
-        $instagram = InstagramHelper::initInstagram();
+		$instagram = InstagramHelper::initInstagram();
 
-        foreach ($instagram_profiles as $ig_profile) {
-            if (InstagramHelper::login($instagram, $ig_profile, 0)) {
+		foreach ($instagram_profiles as $ig_profile) {
+			if (InstagramHelper::login($instagram, $ig_profile, 0)) {
 
-                $items = $instagram->timeline->getSelfUserFeed()->getItems();
+				$items = $instagram->timeline->getSelfUserFeed()->getItems();
 
-	            Log::error("[ENGAGEMENT GROUP INDEX] GET ITEMS " . Auth::user()->email . " " . var_export($items, true));
+				Log::error("[ENGAGEMENT GROUP INDEX] GET ITEMS " . Auth::user()->email . " " . var_export($items, TRUE));
 
-                foreach ($items as $item) {
-                    try {
-                        $image_url = "";
-                        if (is_null($item->getImageVersions2())) {
-                            //is carousel media
-                            $image_url = $item->getCarouselMedia()[0]
-                                    ->getImageVersions2()
-                                    ->getCandidates()[0]
-                                    ->getUrl();
-                        } else {
-                            $image_url = $item->getImageVersions2()
-                                    ->getCandidates()[0]
-                                    ->getUrl();
-                        }
+				foreach ($items as $item) {
+					try {
+						$image_url = "";
+						if (is_null($item->getImageVersions2())) {
+							//is carousel media
+							$image_url = $item->getCarouselMedia()[0]
+								             ->getImageVersions2()
+								             ->getCandidates()[0]
+								->getUrl();
+						} else {
+							$image_url = $item->getImageVersions2()
+							                  ->getCandidates()[0]
+								->getUrl();
+						}
 
-                        try {
-                        	if (InstagramProfileMedia::where('media_id', $item->getPk())->first() == NULL) {
-		                        $new_profile_post = new InstagramProfileMedia;
-		                        $new_profile_post->insta_username = $ig_profile->insta_username;
-		                        $new_profile_post->media_id = $item->getPk();
-		                        $new_profile_post->image_url = $image_url;
-		                        $new_profile_post->code = $item->getCode();
-		                        $new_profile_post->created_at = \Carbon\Carbon::createFromTimestamp($item->getTakenAt());
-		                        $new_profile_post->save();
-	                        }
-                        } catch (\Exception $ex) {
-	                        Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $ex->getMessage());
-	                        Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $ex->getTraceAsString());
-                        }
+						try {
+							if (InstagramProfileMedia::where('media_id', $item->getPk())->first() == NULL) {
+								$new_profile_post                 = new InstagramProfileMedia;
+								$new_profile_post->insta_username = $ig_profile->insta_username;
+								$new_profile_post->media_id       = $item->getPk();
+								$new_profile_post->image_url      = $image_url;
+								$new_profile_post->code           = $item->getCode();
+								$new_profile_post->created_at     = \Carbon\Carbon::createFromTimestamp($item->getTakenAt());
+								$new_profile_post->save();
+							}
+						}
+						catch (\Exception $ex) {
+							Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $ex->getMessage());
+							Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $ex->getTraceAsString());
+						}
+					}
+					catch (\ErrorException $e) {
+						Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $e->getMessage());
+						Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $e->getTraceAsString());
+						$this->profile->error_msg = $e->getMessage();
+						$this->profile->save();
+					}
+					catch (\Exception $ex) {
+						Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $ex->getMessage());
+						Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $ex->getTraceAsString());
+					}
+				}
+			}
+		}
 
-                    } catch (\ErrorException $e) {
-	                    Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $e->getMessage());
-	                    Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $e->getTraceAsString());
-                        $this->profile->error_msg = $e->getMessage();
-                        $this->profile->save();
-                    } catch (\Exception $ex) {
-	                    Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $ex->getMessage());
-	                    Log::error("[ENGAGEMENT GROUP INDEX] " . Auth::user()->email . " " . $ex->getTraceAsString());
-                    }
-                }
-            }
-        }
+		return view('engagement-group.index', [
+			'user_ig_profiles' => $instagram_profiles,
+		]);
+	}
 
-        return view('engagement-group.index', [
-            'user_ig_profiles' => $instagram_profiles,
-        ]);
-    }
+	public function profile(Request $request, $id)
+	{
+		$ig_profile = InstagramProfile::find($id);
 
-    public function profile(Request $request, $id) {
-        $ig_profile = InstagramProfile::find($id);
-        $medias = InstagramProfileMedia::where('insta_username', $ig_profile->insta_username)
-                ->orderBy('created_at', 'desc')
-                ->get();
-        return view('engagement-group.profile', [
-            'ig_profile' => $ig_profile,
-            'medias' => $medias,
-        ]);
-    }
+		$medias = InstagramProfileMedia::where('insta_username', $ig_profile->insta_username)
+		                               ->orderBy('created_at', 'desc')
+		                               ->get();
 
-    public function schedule(Request $request, $media_id) {
-        $user = User::where('email', Auth::user()->email)->first();
-        if ($user->engagement_quota > 0) {
-            $engagement_group_job = EngagementGroupJob::where('media_id', '=', $media_id)->first();
-            if ($engagement_group_job === null) {
-                $engagement_group_job = new EngagementGroupJob;
-                $engagement_group_job->media_id = $media_id;
-                $engagement_group_job->engaged = 0;
-                if ($engagement_group_job->save()) {
-                    $user->engagement_quota = $user->engagement_quota - 1;
-                    $user->save();
-                    $job = new \App\Jobs\EngagementGroup($media_id, $request->input('profile_id'));
-                    $job->onQueue('engagementgroup');
-                    dispatch($job);
-                }
-                return Response::json(array("success" => true, 'message' => "Your photo has been sent for engagement group. Expect a increase in engagement."));
-            } else {
-                $engagement_group_job->engaged = 0;
-                $engagement_group_job->save();
-                return Response::json(array("success" => false, 'message' => "Your image has already been sent for engagement before."));
-            }
-        } else {
-            return Response::json(array("success" => false, 'message' => "You've ran out of engagement credits. Do try again tomorrow."));
-        }
-    }
+		return view('engagement-group.profile', [
+			'ig_profile' => $ig_profile,
+			'medias'     => $medias,
+		]);
+	}
+
+	public function schedule(Request $request, $media_id)
+	{
+		$user = User::where('email', Auth::user()->email)->first();
+		if ($user->engagement_quota > 0) {
+			$engagement_group_job = EngagementGroupJob::where('media_id', '=', $media_id)->first();
+			if ($engagement_group_job === NULL) {
+				$engagement_group_job           = new EngagementGroupJob;
+				$engagement_group_job->media_id = $media_id;
+				$engagement_group_job->engaged  = 0;
+				if ($engagement_group_job->save()) {
+					$user->engagement_quota = $user->engagement_quota - 1;
+					$user->save();
+					$job = new \App\Jobs\EngagementGroup($media_id, $request->input('profile_id'));
+					$job->onQueue('engagementgroup');
+					dispatch($job);
+				}
+
+				return Response::json([ "success" => TRUE, 'message' => "Your photo has been sent for engagement group. Expect a increase in engagement." ]);
+			} else {
+				$engagement_group_job->engaged = 0;
+				$engagement_group_job->save();
+
+				return Response::json([ "success" => FALSE, 'message' => "Your image has already been sent for engagement before." ]);
+			}
+		} else {
+			return Response::json([ "success" => FALSE, 'message' => "You've ran out of engagement credits. Do try again tomorrow." ]);
+		}
+	}
 }
