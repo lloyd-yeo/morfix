652ab0ea0dcefb16640df0e105eda92a85734712
Lim Choon Rong <clim046@e.ntu.edu.sg>
2018-04-16 03:48:24 +0800

    And more checks

--- app/Console/Commands/ExportSettingsToInstagress.php
+++ app/Console/Commands/ExportSettingsToInstagress.php
@@ -76,6 +76,27 @@ class ExportSettingsToInstagress extends Command
                     $gress_username = $this->searchUsernames($gress_id, $target_username);
 
                     if ($gress_username){
+
+                        if (strpos($usernames[0]["full_name"], "?") !== false) {
+                            echo"[RECEIVED USERNAME DETAILS] INVALID CHARACTERS IN USERNAME = ? USING 1st CHECK \n";
+                            continue;
+                        }
+                        if (preg_match('/\?/', $usernames[0]["full_name"])) {
+                            echo"[RECEIVED USERNAME DETAILS] INVALID CHARACTERS IN USERNAME = ? USING 2nd CHECK \n";
+                            continue;
+                        }
+                        if (strstr($usernames[0]["full_name"], '?')) {
+                            echo"[RECEIVED USERNAME DETAILS] INVALID CHARACTERS IN USERNAME = ? USING 3rd CHECK \n";
+                            continue;
+                        }
+                        if (strpos($usernames[0]["username"], '?') !== false) {
+                            echo"[RECEIVED USERNAME DETAILS] INVALID CHARACTERS IN USERNAME= ? \n";
+                            continue;
+                        }
+                        if($usernames[0]["full_name"] == ""){
+                            echo"[RECEIVED USERNAME DETAILS] INVALID CHARACTERS IN FULL NAME = blank \n";
+                            continue;
+                        }
                         $usernames[] =  array("id" => $gress_username["id"], "username" => $gress_username["username"],
                             "full_name" => $gress_username["full_name"], "profile_picture" => $gress_username["profile_picture"]);
                     }
@@ -117,12 +138,16 @@ class ExportSettingsToInstagress extends Command
                 $usernames = $result_json['usernames'];
                 if($usernames[0]["username"] == $target_username){
                     echo"[GET USERNAME DETAILS] FIRST USERNAME IS CORRECT! \n";
-                    if (strpos($usernames[0]["full_name"], '?') !== false) {
-                        echo"[GET USERNAME DETAILS] INVALID CHARACTERS IN USERNAME = ? \n";
+                    if (strpos($usernames[0]["full_name"], "?") !== false) {
+                        echo"[GET USERNAME DETAILS] INVALID CHARACTERS IN USERNAME = ? USING 1st CHECK \n";
                         return false;
                     }
                     if (preg_match('/\?/', $usernames[0]["full_name"])) {
-                        echo"[GET USERNAME DETAILS] INVALID CHARACTERS IN USERNAME = ? USING NEW CHECK \n";
+                        echo"[GET USERNAME DETAILS] INVALID CHARACTERS IN USERNAME = ? USING 2nd CHECK \n";
+                        return false;
+                    }
+                    if (strstr($usernames[0]["full_name"], '?')) {
+                        echo"[GET USERNAME DETAILS] INVALID CHARACTERS IN USERNAME = ? USING 3rd CHECK \n";
                         return false;
                     }
                     if (strpos($usernames[0]["username"], '?') !== false) {
