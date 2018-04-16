a6fafa04f949cec6e41a74b50a93a5339c937239
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-04-16 19:30:42 +0800

    Lazy Commit!

--- app/Console/Commands/EngagementGroup.php
+++ app/Console/Commands/EngagementGroup.php
@@ -1,105 +1,162 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Jobs;
 
-use Illuminate\Console\Command;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\DB;
 use InstagramAPI\Instagram as Instagram;
 use InstagramAPI\SettingsAdapter as SettingsAdapter;
 use InstagramAPI\InstagramException as InstagramException;
+use App\User;
 use App\InstagramProfile;
+use App\InstagramProfileTargetUsername;
+use App\InstagramProfileTargetHashtag;
+use App\EngagementJob;
+use App\BlacklistedUsername;
+use App\InstagramProfileLikeLog;
+use App\LikeLogsArchive;
 use App\CreateInstagramProfileLog;
 use App\Proxy;
 use App\DmJob;
-use App\EngagementJob;
-use App\EngagementGroupJob;
+use App\Niche;
+use App\InstagramProfileMedia;
+use App\InstagramHelper;
 
-class EngagementGroup extends Command {
+class RefreshIgProfile implements ShouldQueue {
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'engagement:add';
+	use Dispatchable,
+		InteractsWithQueue,
+		Queueable,
+		SerializesModels;
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Start liking using the engagement group.';
+	/**
+	 * The number of times the job may be attempted.
+	 *
+	 * @var int
+	 */
+	public $tries = 1;
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct() {
-        parent::__construct();
-    }
+	/**
+	 * The number of seconds the job can run before timing out.
+	 *
+	 * @var int
+	 */
+	public $timeout = 180;
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle() {
-        $outstanding_engagements = EngagementGroupJob::where('engaged', false)->get();
-        
-        foreach ($outstanding_engagements as $outstanding_engagement) {
-            $media_id = $outstanding_engagement->media_id;
-            $comment_count = 100;
+	protected $profile;
+	protected $instagram;
 
-            $outstanding_engagement->engaged = 1;
-            $outstanding_engagement->save();
-            
-            $this->line("UPDATED [$media_id]\n\n");
-            
-            $engagement_group_users = DB::connection('mysql_old')
-                    ->select("SELECT p.insta_username, p.insta_pw, p.proxy, p.auto_like, p.auto_comment, p.id, u.user_tier
-                                FROM user_insta_profile p, user u
-                                WHERE p.user_id = u.user_id
-                                AND p.checkpoint_required = 0
-                                AND p.invalid_user = 0
-                                AND p.incorrect_pw = 0
-                                AND p.feedback_required = 0
-                                AND p.invalid_proxy = 0
-                                AND p.account_disabled = 0
-                                AND (
-                                u.user_tier = 1 
-                                OR (u.user_tier > 1 AND p.auto_interaction = 1 AND (p.auto_like = 1 OR p.auto_comment = 1))
-                                ) ORDER BY RAND()");
+	/**
+	 * Create a new job instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(InstagramProfile $profile) {
+		$this->profile = $profile;
+	}
 
-            foreach ($engagement_group_users as $ig_profile) {
-                $ig_username = $ig_profile->insta_username;
-                try {
-                    
-                    $engagement_job = new EngagementJob;
-                    $engagement_job->media_id = $media_id;
-                    $engagement_job->insta_username = $ig_username;
-                    $engagement_job->action = 0;
-                    $engagement_job->save();
-                    
-//                    DB::connection('mysql_old')
-//                            ->insert("INSERT INTO engagement_job_queue (media_id,insta_username,action) VALUES (?,?,?);", [$media_id, $ig_username, 0]);
-                    
-                    if (($ig_profile->auto_comment == 1 && $comment_count > 0) || $ig_profile->user_tier == 1) {
-                        
-                        $engagement_job = new EngagementJob;
-                        $engagement_job->media_id = $media_id;
-                        $engagement_job->insta_username = $ig_username;
-                        $engagement_job->action = 1;
-                        $engagement_job->save();
-//                        DB::connection('mysql_old')
-//                                ->insert("INSERT INTO engagement_job_queue (media_id,insta_username,action) VALUES (?,?,?);", [$media_id, $ig_username, 1]);
-                        $comment_count = $comment_count - 1;
-                    }
-                } catch (\PDOException $pdo_ex) {
-                    continue;
-                }
-            }
-        }
-    }
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle() {
+		DB::reconnect();
+
+		echo($this->profile->insta_username . "\t" . $this->profile->insta_pw . "\n\n");
+
+		if (!$this->initInstagramAPI($this->profile)){
+			return;
+		}
+
+		try {
+
+			$instagram_user = InstagramHelper::getUserInfo($this->instagram, $this->profile);
+
+			$this->profile->insta_username = $instagram_user->getUsername();
+			$this->profile->profile_full_name = $instagram_user->getFullName();
+			$this->profile->updated_at = \Carbon\Carbon::now();
+			$this->profile->follower_count = $instagram_user->getFollowerCount();
+			$this->profile->num_posts = $instagram_user->getMediaCount();
+			$this->profile->insta_user_id = $instagram_user->getPk();
+			$this->profile->profile_pic_url = $instagram_user->getProfilePicUrl();
+			$this->profile->save();
+
+			$items = $this->instagram->timeline->getSelfUserFeed()->getItems();
+
+			foreach ($items as $item) {
+
+				try {
+					$image_url = "";
+					if (is_null($item->getImageVersions2())) {
+						//is carousel media
+						$image_url = $item->getCarouselMedia()[0]->getImageVersions2()->getCandidates()[0]->getUrl();
+					} else {
+						$image_url = $item->getImageVersions2()->getCandidates()[0]->getUrl();
+					}
+
+					try {
+						$new_profile_post = new InstagramProfileMedia;
+						$new_profile_post->insta_username = $this->profile->insta_username;
+						$new_profile_post->media_id = $item->getPk();
+						$new_profile_post->image_url = $image_url;
+						$new_profile_post->code = $item->getCode();
+						$new_profile_post->created_at = \Carbon\Carbon::createFromTimestamp($item->getTakenAt());
+						$new_profile_post->save();
+					} catch (\Exception $ex) {
+						//                        echo $ex->getMessage();
+					}
+
+				} catch (\ErrorException $e) {
+					$this->profile->error_msg = $e->getMessage();
+					$this->profile->save();
+				}
+			}
+		} catch (\InstagramAPI\Exception\CheckpointRequiredException $checkpoint_ex) {
+			$this->profile->checkpoint_required = 1;
+		} catch (\InstagramAPI\Exception\NetworkException $network_ex) {
+			$this->profile->error_msg = $network_ex->getMessage();
+		} catch (\InstagramAPI\Exception\EndpointException $endpoint_ex) {
+			if (stripos(trim($endpoint_ex->getMessage()), "The username you entered doesn't appear to belong to an account. Please check your username and try again.") !== false) {
+				$this->profile->invalid_user = 1;
+			} else {
+				$this->profile->error_msg = $endpoint_ex->getMessage();
+			}
+		} catch (\InstagramAPI\Exception\IncorrectPasswordException $incorrectpw_ex) {
+			$this->profile->incorrect_pw = 1;
+		} catch (\InstagramAPI\Exception\AccountDisabledException $accountdisabled_ex) {
+			$this->profile->account_disabled = 1;
+		} catch (\InstagramAPI\Exception\RequestException $request_ex) {
+			$this->profile->error_msg = $request_ex->getMessage();
+		}
+		$this->profile->save();
+	}
+
+	public function initInstagramAPI($ig_profile) {
+		$this->instagram = InstagramHelper::initInstagram();
+		$this->instagram = InstagramHelper::setProxy($this->instagram, $ig_profile, 1);
+		if (!InstagramHelper::login($this->instagram, $ig_profile)) {
+			exit();
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * The job failed to process.
+	 *
+	 * @param  Exception $exception
+	 *
+	 * @return void
+	 */
+	public function failed(Exception $exception)
+	{
+		unset($this->instagram);
+	}
 
 }
