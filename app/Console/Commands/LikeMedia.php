9c6ad7568eb0e0a4bed8c30cbdd6deea9b7fc156
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-04-24 15:22:48 +0800

    Lazy Commit!

--- app/Console/Commands/LikeMedia.php
+++ app/Console/Commands/LikeMedia.php
@@ -46,11 +46,80 @@ class LikeMedia extends Command
 			if (InstagramHelper::login($instagram, $profile)) {
 				$media_id = $this->argument('media_id');
 				if ($this->argument('media_id_type') == 'code') {
-					$media_id = InstagramID::fromCode($media_id);
+					$code = $media_id;
+
+					if (!is_string($code) || preg_match('/[^A-Za-z0-9\-_]/', $code)) {
+						throw new \InvalidArgumentException('Input must be a valid Instagram shortcode.');
+					}
+
+					// Convert the base64 shortcode to a base2 binary string.
+					$base2 = '';
+					for ($i = 0, $len = strlen($code); $i < $len; ++$i) {
+						// Find the base64 value of the current character.
+						$base64 = strpos(self::BASE64URL_CHARMAP, $code[$i]);
+
+						// Convert it to 6 binary bits (left-padded if needed).
+						$base2 .= str_pad(decbin($base64), 6, '0', STR_PAD_LEFT);
+					}
+
+					// Now just convert the base2 binary string to a base10 decimal string.
+					$base10 = self::base2to10($base2);
+
+					return $base10;
+
+					$media_id = $base10;
 				}
 
 				dump($media_id);
 			}
         }
     }
+
+	/**
+	 * Converts a binary number of any size into a decimal string.
+	 *
+	 * @param string $base2 The binary bits as a string where each character is
+	 *                      either "1" or "0".
+	 *
+	 * @throws \InvalidArgumentException If the input isn't a binary string.
+	 *
+	 * @return string The decimal number as a string.
+	 */
+	public static function base2to10(
+		$base2)
+	{
+		if (!is_string($base2) || preg_match('/[^01]/', $base2)) {
+			throw new \InvalidArgumentException('Input must be a binary string.');
+		}
+
+		// Pre-build a ~80kb RAM table with all values for bits 1-512. Any
+		// higher bits than that will be generated and cached live instead.
+		if (self::$bitValueTable === null) {
+			self::$bitValueTable = self::buildBinaryLookupTable(512);
+		}
+
+		// Reverse the bit-sequence so that the least significant bit is first,
+		// which is necessary when converting binary via its bit offset powers.
+		$base2rev = strrev($base2);
+
+		// Process each bit individually and reconstruct the base10 number.
+		$base10 = '0';
+		$bits = str_split($base2rev, 1);
+		for ($bitPosition = 0, $len = count($bits); $bitPosition < $len; ++$bitPosition) {
+			if ($bits[$bitPosition] == '1') {
+				// Look up the bit value in the table or generate if missing.
+				if (isset(self::$bitValueTable[$bitPosition])) {
+					$bitValue = self::$bitValueTable[$bitPosition];
+				} else {
+					$bitValue = bcpow('2', (string) $bitPosition, 0);
+					self::$bitValueTable[$bitPosition] = $bitValue;
+				}
+
+				// Now just add the bit's value to the current total.
+				$base10 = bcadd($base10, $bitValue, 0);
+			}
+		}
+
+		return $base10;
+	}
 }
