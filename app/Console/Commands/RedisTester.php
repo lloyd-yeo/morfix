0da21f0866357cef2ed5bc421ccf013dbc09f16d
Lim Choon Rong <clim046@e.ntu.edu.sg>
2018-04-24 17:28:05 +0800

    remove json coding

--- app/Console/Commands/RedisTester.php
+++ app/Console/Commands/RedisTester.php
@@ -41,7 +41,7 @@ class RedisTester extends Command
         $pk = "test:profile:123456";
         $response_array = (array("name" => "abc", "full_name" => "long_name", "is_verified" => "false"));
         echo "This is follower response \n";
-        $response_array = json_encode($response_array, JSON_PRETTY_PRINT);
+//        $response_array = json_encode($response_array, JSON_PRETTY_PRINT);
         echo ($response_array);
 
         Redis::hmset(
