b3dced272a53d27923d4c254a5bf8c64faf90dd5
Lim Choon Rong <clim046@e.ntu.edu.sg>
2018-04-24 17:19:41 +0800

    changed the name

--- app/Console/Commands/RedisTester.php
+++ app/Console/Commands/RedisTester.php
@@ -38,7 +38,7 @@ class RedisTester extends Command
      */
     public function handle()
     {
-        $pk = 123456;
+        $pk = "test:profile:123456";
         $response_array = (array("name" => "abc", "full_name" => "long_name", "is_verified" => "false"));
         echo "This is follower response \n";
         $response_array = json_encode($response_array, JSON_PRETTY_PRINT);
