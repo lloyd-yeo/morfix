2bedf9745082d636e669f189654933afef191c22
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-04-25 15:32:25 +0800

    Lazy Commit!

--- app/Console/Commands/ImportLikeLogsToRedis.php
+++ app/Console/Commands/ImportLikeLogsToRedis.php
@@ -48,14 +48,16 @@ class ImportLikeLogsToRedis extends Command
 		$ig_profiles = InstagramProfile::all();
 		foreach ($ig_profiles as $ig_profile) {
 			$like_logs = InstagramProfileLikeLog::where('insta_username', $ig_profile->insta_username)->get();
+			$this->line("[" . $ig_profile->insta_username . "] like_logs count: " . $like_logs->count());
 			foreach ($like_logs as $like_log) {
 				$media_pk = explode('_', $like_log->target_media)[0];
-				RedisRepository::savePartialMedia($media_pk, $like_log->target_media_code);
-				RedisRepository::savePartialMediaOwner($like_log->target_username, $media_pk);
+//				RedisRepository::savePartialMedia($media_pk, $like_log->target_media_code);
+//				RedisRepository::savePartialMediaOwner($like_log->target_username, $media_pk);
 				RedisRepository::saveUserLikedMediaByUsername($ig_profile->insta_username, $media_pk, $like_log->date_liked);
 				RedisRepository::saveUserLikedMediaByPk($ig_profile->insta_user_id, $media_pk, $like_log->date_liked);
 				RedisRepository::saveUserLikedUsername($ig_profile->insta_user_id, $like_log->target_username);
 			}
+			$this->line("[" . $ig_profile->insta_username . "] saved like_logs to redis.");
 		}
     }
 }
