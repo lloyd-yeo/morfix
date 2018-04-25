0f29618a780c3be41bc16f1d0b95fd88f679c4a9
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-04-25 13:09:06 +0800

    Lazy Commit!

--- app/RedisRepository.php
+++ app/RedisRepository.php
@@ -3,11 +3,19 @@
 namespace App;
 
 use Log;
+use Illuminate\Support\Facades\Redis;
 
 class RedisRepository
 {
 	public static function saveUsers($users)
 	{
-
+//		Redis::hmset(
+//			$payload->id(), [
+//				'status' => 'reserved',
+//				'payload' => $payload->value,
+//				'updated_at' => time(),
+//				'reserved_at' => time(),
+//			]
+//		);
 	}
 }
