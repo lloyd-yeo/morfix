158dd419336fa0a1e8ca3098e2de27fd8a676a77
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-05-16 16:19:40 +0700

    Lazy Commit!

--- app/InstagramProfileFollowerAnalysis.php
+++ app/InstagramProfileFollowerAnalysis.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class InstagramProfileFollowerAnalysis extends Model {
 
-    protected $table = 'morfix_instagram_profile_follower_analysis';
+    protected $table = 'instagram_profile_follower_analysis';
 
 }
